53be7bd1c75a3c0d55a398d25815f1717a8a8e7b
张屈 <joneyphair@qq.com>
2016-09-20 13:19:02 +0800

    chore: 推送文件

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -2,5 +2,5 @@
 
 npm run build
 
-# rsync -rvltOD ./dist/* ali-krspace-web-01:/data/work/admin.krspace.cn/
-# rsync -rvltOD ./dist/* ali-krspace-web-02:/data/work/admin.krspace.cn/
+rsync -rvltOD ./build/* ali-krspace-web-01:/data/work/admin.krspace.cn/
+rsync -rvltOD ./build/* ali-krspace-web-02:/data/work/admin.krspace.cn/
