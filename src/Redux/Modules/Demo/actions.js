6dbdfdb2db8bd1b2c8604d8660a60838b9a56401
张屈 <joneyphair@qq.com>
2016-09-20 13:16:32 +0800

    fix: git ignorecase false

---
+++ src/Redux/Modules/Demo/actions.js
@@ -0,0 +1,132 @@
+import fetch from 'node-fetch';
+import param from 'jquery-param';
+
+
+
+
+export function switchSidebarNav(value){
+	return{
+		type:'SWITCH_SIDEBAR_NAV',
+		data:value
+	}
+}
+
+export function switchBottomNav(value){
+
+	return{
+		type:'SWITCH_BOTTOM_NAV',
+		data:value
+	}
+
+}
+
+export function setCompanysData(companys){
+
+	return {
+		type:'SET_COMPANYS_DATA',
+		data:companys
+	}
+
+}
+
+export function setCompanysStatus(status){
+
+	var obj = {
+		type:'SET_COMPANYS_STATUS_LOADING',
+		status:'loading'
+	};
+
+	switch(status){
+
+	case 'loading':{
+		obj.type = 'SET_COMPANYS_STATUS_LOADING'
+		obj.status = 'loading';
+		break;
+	}
+
+	case 'failure':{
+		obj.type = 'SET_COMPANYS_STATUS_FAILURE'
+		obj.status = 'failure';
+		break;
+	}
+
+	case 'success':{
+		obj.type = 'SET_COMPANYS_STATUS_SUCCESS'
+		obj.status = 'success';
+		break;
+	}
+
+	case 'error':{
+		obj.type = 'SET_COMPANYS_STATUS_ERROR'
+		obj.status = 'error';
+		break;
+	}
+	default:{
+		obj.type = 'SET_COMPANYS_STATUS_LOADING'
+		obj.status = 'loading';
+	}
+
+
+	}
+
+	return obj;
+
+}
+
+
+export function fetchCompanysStatusCount(){
+
+	return dispatch=>{
+
+		fetch('http://rong.36kr.com/api/company').then(function(response){
+			return response.json();
+		}).then(function(response){
+			dispatch(setCompanysData(response.data.page.data));
+			dispatch(setCompanysStatus('success'));
+		}).catch(function(err){
+			dispatch(setCompanysStatus('error'));
+		});
+	}
+}
+
+export function fetchCompanys(request){
+
+	return dispatch =>{
+
+		dispatch(setCompanysStatus('loading'));
+
+		fetch('http://rong.36kr.com/api/company?='+param(request)).then(function(response){
+			return response.json();
+		}).then(function(response){
+			dispatch(setCompanysData(response.data.page.data));
+			dispatch(setCompanysStatus('success'));
+		}).catch(function(err){
+			dispatch(setCompanysStatus('error'));
+		});
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Redux/Modules/Demo/reducers.js
@@ -0,0 +1,82 @@
+import fetch from 'node-fetch';
+
+export function sidebar_nav (state={},action){
+
+	switch(action.type){
+
+		case 'SWITCH_SIDEBAR_NAV':{
+			return Object.assign({},state,{switch_value:action.data});
+		}
+
+		default:{
+			return state;
+		}
+	}
+}
+
+export function bottom_nav (state={},action){
+
+	switch(action.type){
+
+		case 'SWITCH_BOTTOM_NAV':{
+			return Object.assign({},state,{...action.data});
+		}
+
+		default:{
+			return state;
+		}
+	}
+}
+
+export function companys_filter(state = {},action){
+
+	switch(action.type){
+
+		case 'SET_COMPANYS_FILTER_ALL':{
+
+			return state;
+		}
+
+	default:{
+		return state;
+	}
+	}
+
+}
+
+export function companys_fetch(state={},action){
+
+	switch(action.type){
+
+	case 'SET_COMPANYS_STATUS_LOADING':{
+		return Object.assign({},state,{status:action.status});
+	}
+
+	case 'SET_COMPANYS_STATUS_SUCCESS':{
+		return Object.assign({},state,{status:action.status});
+	}
+
+	case 'SET_COMPANYS_STATUS_ERROR':{
+		return Object.assign({},state,{status:action.status});
+	}
+
+	case 'SET_COMPANYS_STATUS_FAILURE':{
+		return Object.assign({},state,{status:action.status});
+	}
+
+	default:{
+		return state;
+	}
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
