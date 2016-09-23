88d8437be82380f1ee8d5bf3fb28300a95f92e46
张屈 <joneyphair@qq.com>
2016-09-23 15:33:55 +0800

    chore: deploy

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -2,5 +2,5 @@
 
 npm run build
 
-rsync -rvltOD ./build/* ali-krspace-web-01:/data/work/admin.krspace.cn/
-rsync -rvltOD ./build/* ali-krspace-web-02:/data/work/admin.krspace.cn/
+rsync -rvltOD ./build/* ali-krspace-web-01:/data/work/kr-node-proxy/static/
+rsync -rvltOD ./build/* ali-krspace-web-02:/data/work/kr-node-proxy/static/
