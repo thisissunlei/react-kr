94a3d6f57e818873bd7efd21895a560bdfa4f99e
张屈 <joneyphair@qq.com>
2016-09-26 10:22:45 +0800

    feat:合同详情

--- src/Containers/Operation/Agreement/Join/Detail/index.js
+++ src/Containers/Operation/Agreement/Join/Detail/index.js
@@ -26,182 +26,14 @@ import {
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn,TableFooter} from 'kr-ui/Table';
 
 
-var JoinForm = (props) => {
-
-  const { error, handleSubmit, pristine, reset, submitting,submit} = props;
-
-  return (
-
-    <form onSubmit={handleSubmit(submit)}>
-
-
-				<Grid style={{marginTop:30}}>
-
-					<Row>
-						<Col md={5}>
-						  <LabelText name="order_type" label="出租方"/>
-						 </Col>
-						<Col md={5} mdOffset="1"> <LabelText name="username" type="text" label="地址" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="联系人" /> </Col>
-						<Col md={5} mdOffset="1"> <KrField name="username" type="text" label="电话" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="承租方" /> </Col>
-						<Col md={5} mdOffset="1"> <KrField name="username" type="text" label="地址" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="联系人" /> </Col>
-						<Col md={5} mdOffset="1"> <KrField name="username" type="text" label="电话" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="所属社区" /> </Col>
-						<Col md={5} mdOffset="1"> 
-<KrField name="order_type" component="select" label="所在楼层">
-							 <option>请选择订单类型</option>
-						            <option value="11">Red</option>
-						            <option value="00ff00">Green</option>
-						            <option value="0000ff">Blue</option>
-						 </KrField>
-						</Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="地址" /> </Col>
-						<Col md={5} mdOffset="1"> <KrField name="username" type="text" label="合同编号" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="租赁期限"  requireLabel={true} /> </Col>
-						<Col md={5} mdOffset="1"> 
-						<KrField name="order_type" component="select" label="付款方式">
-							 <option>请选择订单类型</option>
-						            <option value="11">Red</option>
-						            <option value="00ff00">Green</option>
-						            <option value="0000ff">Blue</option>
-						 </KrField> 
-						 </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="order_type" component="select" label="支付方式">
-							 <option>请选择订单类型</option>
-						            <option value="11">Red</option>
-						            <option value="00ff00">Green</option>
-						            <option value="0000ff">Blue</option>
-						 </KrField>  </Col>
-						<Col md={5} mdOffset="1"> <KrField name="username" type="text" label="签署时间" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="首付款时间" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="工位" /> </Col>
-						<Col md={5} mdOffset="1"> <KrField name="username" type="text" label="会议室" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={11}> <KrField name="username" type="text" label="租赁用途" placeholder="办公使用" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="username" type="text" label="租金总额" placeholder="" /> </Col>
-						<Col md={5} mdOffset="1"> <KrField name="username" type="text" label="押金总额" /> </Col>
-					</Row>
-
-					<Row>
-						<Col md={5}> <KrField name="uname" type="file" label="合同附件" /> </Col>
-					</Row>
-
-				</Grid>
-
-
-				<Grid style={{marginTop:30}}>
-
-					<Row>
-
-						<Col md={10}></Col>
-						<Col md={2}> <Button  label="确定" type="submit" primary={true} /> </Col>
-
-					</Row>
-
-				</Grid>
-
-
-		  {/*
-			<Button label="重置" primary={true} onTouchTap={reset} disabled={pristine || submitting} />
-		  */}
-
-    </form>
-
-  )
-}
-
-JoinForm = reduxForm({
-  form: 'joinForm'  
-})(JoinForm);
+import {
+	BreadCrumbs,
+	Loading,
+	Notify
+} from 'kr-ui';
 
 
 
-
-var PlanForm = (props) => {
-
-  const { error, handleSubmit, pristine, reset, submitting,submit,cancel } = props;
-
-  return (
-
-    <form onSubmit={handleSubmit(submit)}>
-
-       
-				<Grid style={{marginTop:30}}>
-					<Row>
-						<Col md={2}>
-						  <LabelText label="社区名称" text="氪空间"/>
-						</Col>
-						<Col md={2}>
-						  <LabelText label="所属楼层" text="200个"/>
-						</Col>
-						<Col md={2}>
-						  <LabelText label="可租赁工位" text="200个"/>
-						</Col>
-						<Col md={1}>
-						  <LabelText label="选择工位" text=""/>
-						</Col>
-						<Col md={1}>
-						 <KrField name="username" type="text" />
-						</Col>
-						<Col md={1}>
-						       <KrField name="name" type="text" />
-						</Col>
-					</Row>
-
-					<Row>
-						<Col md={8}></Col>
-						<Col md={2}> <Button  label="确定" type="submit" primary={true} /> </Col>
-						<Col md={2}> <Button label="取消"  onTouchTap={cancel} /> </Col>
-
-					</Row>
-				</Grid>
-
-		  {/*
-			<Button label="重置" primary={true} onTouchTap={reset} disabled={pristine || submitting} />
-		  */}
-
-    </form>
-
-  )
-}
-
-PlanForm = reduxForm({
-  form: 'submitValidation'  
-})(PlanForm);
 
 
 export default class JoinEdit extends Component {
@@ -241,84 +73,141 @@ export default class JoinEdit extends Component {
 	}
 
 	handleClick(){
-		submitForm('PlanForm');
 		console.log('----');
 	}
 
   render() {
 
-			 const actions = [
-				  <Button
-					label="Cancel"
-					primary={true}
-					onTouchTap={this.handleClose}
-				  />,
-				  <Button
-					label="Submit"
-					primary={true}
-					onTouchTap={this.handleClose}
-				  />,
-				];
+	 const orderBaseInfo = {};
+	  const contractList = [];
 
     return (
 
-      <div>
-			<Section title="创建入驻协议书" description=""> 
-
-				<JoinForm  submit={this.confirmJoinSubmit}/>
+		  <div>
 
 
+			<BreadCrumbs children={['系统运营','财务管理']}/>
 
-				<Section title="租赁明细" description="" rightMenu = {
-								<Menu>
-									  <MenuItem primaryText="删除" />
-									  <MenuItem primaryText="分配"  onTouchTap={this.handleOpen} />
-								</Menu>
-				}> 
+<Section title="客户订单详情" description=""> 
+				
+				<Grid style={{marginTop:30}}>
+
+				
+					<Row>
+						<Col md={6} ><LabelText label="出租方" text=""/></Col>
+						<Col md={6} ><LabelText label="地址" text=""/></Col>
+					</Row>
+
+				
+					<Row>
+						<Col md={6} ><LabelText label="联系人" text=""/></Col>
+						<Col md={6} ><LabelText label="电话" text=""/></Col>
+					</Row>
+
+				
+					<Row>
+						<Col md={6} ><LabelText label="承租方" text=""/></Col>
+						<Col md={6} ><LabelText label="地址" text=""/></Col>
+					</Row>
+
+				
+					<Row>
+						<Col md={6} ><LabelText label="联系人" text=""/></Col>
+						<Col md={6} ><LabelText label="电话" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="所属社区" text=""/></Col>
+						<Col md={6} ><LabelText label="所属楼层" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="地址" text=""/></Col>
+						<Col md={6} ><LabelText label="合同编号" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="支付方式" text=""/></Col>
+						<Col md={6} ><LabelText label="租赁期限" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="首付款时间" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="签署日期" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="租赁项目" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="租赁用途" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="租赁总额" text=""/></Col>
+						<Col md={6} ><LabelText label="押金总额" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="备注" text=""/></Col>
+					</Row>
+
+					<Row>
+						<Col md={6} ><LabelText label="上传附件" text=""/></Col>
+					</Row>
+
+				</Grid>
 
 
-			<Table  displayCheckbox={true}>
+			       <Section title="租赁明细" description=""> 
+
+	<Table>
 					<TableHeader>
 							<TableHeaderColumn>类别</TableHeaderColumn>
 							<TableHeaderColumn>编号／名称</TableHeaderColumn>
-							<TableHeaderColumn>租赁开始时间</TableHeaderColumn>
-							<TableHeaderColumn>租赁结束时间</TableHeaderColumn>
+							<TableHeaderColumn>单价（元／月）</TableHeaderColumn>
+							<TableHeaderColumn>起始日期</TableHeaderColumn>
+							<TableHeaderColumn>结束日期</TableHeaderColumn>
 					</TableHeader>
 					<TableBody>
-						 <TableRow>
-							<TableRowColumn>1</TableRowColumn>
-							<TableRowColumn>John Smith</TableRowColumn>
-							<TableRowColumn>Employed</TableRowColumn>
-								<TableRowColumn>John Smith</TableRowColumn>
-						</TableRow>
 
-						 <TableRow>
-							<TableRowColumn>1</TableRowColumn>
-							<TableRowColumn>John Smith</TableRowColumn>
-							<TableRowColumn>Employed</TableRowColumn>
-								<TableRowColumn>John Smith</TableRowColumn>
-						</TableRow>
+					{contractList.map((item,index)=>{
+						return (
+							 <TableRow key={index}>
+							<TableRowColumn>{item.contractcode}</TableRowColumn>
+							<TableRowColumn>
 
+									{item.contracttype == 1 && '意向书'}
+									{item.contracttype == 2 && '入住协议'}
+									{item.contracttype == 3 && ':增续租协议'}
+									{item.contracttype == 4 && ':减租协议'}
+									{item.contracttype == 5 && ':退租协议'}
+									{item.contracttype == 6 && ':增值服务合同'}
+
+							</TableRowColumn>
+							<TableRowColumn><Date.Format value={item.contractTotalamount}/></TableRowColumn>
+							<TableRowColumn><Date.Format value={item.leaseBegindate}/></TableRowColumn>
+							<TableRowColumn> <Date.Format value={item.leaseEnddate}/></TableRowColumn>
+							<TableRowColumn><Button  type="link" label="查看" href={`/#/operation/customerManage/${item.customerid}/order/${item.mainbillid}/detail`} />
+							<Button type="link" label="编辑"  href={`/#/operation/customerManage/${item.customerid}/order/${item.mainbillid}/edit`} /></TableRowColumn>
+						   </TableRow>
+							);
+					})}
+						
 				   </TableBody>
-			 </Table>
+			 </Table>		
+
+      </Section>
+
+	
+
 
 
 			</Section>
-
-			</Section>
-
-
-			<Dialog
-				title="平面图"
-				modal={true}
-				 contentStyle={{width: '90%',maxWidth: 'none'}}
-				open={this.state.open}>
-
-				<PlanForm ref="formdata" submit={this.confirmSubmit} cancel={this.handleClose}/>
-
-			</Dialog>
-
-			<Button  label="确定"  onTouchTap={this.handleClick} />
 
 
 
