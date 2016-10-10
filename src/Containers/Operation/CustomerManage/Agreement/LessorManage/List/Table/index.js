8e1d27ac3addd6b8914c005fdfa598338a2ff0e7
张屈 <joneyphair@qq.com>
2016-10-10 15:18:02 +0800

    feat:查看出租方管理

--- src/Containers/Operation/CustomerManage/Agreement/LessorManage/List/Table/index.js
+++ src/Containers/Operation/CustomerManage/Agreement/LessorManage/List/Table/index.js
@@ -4,8 +4,25 @@ import {KrField,LabelText} from 'kr-ui/Form';
 
 import {Button} from 'kr-ui/Button';
 
+import {Dialog,Snackbar} from 'material-ui';
+
 
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn,TableFooter} from 'kr-ui/Table';
+
+
+const ViewHtml = (props)=>{
+
+	return (
+		<div>
+				<KrField component="labelText" label="出租方名称" value={props.item.corporationName} /> 
+				<KrField name="enableflag" component="labelText"  label="是否启用" value={props.item.enableflag?'是':'否'} /> 
+				<KrField name="corporationName" component="labelText"  label="出租方名称" value={props.item.corporationName} /> 
+				<KrField name="corporationAddress" component="labelText" type="text" label="详细地址"  value={props.item.corporationAddress}/> 
+				<KrField name="corporationDesc" component="labelText" label="备注"  placeholder="备注信息" value={props.item.corporationDesc}/> 
+
+	</div>
+	);
+}
 
 
 export default class RenderTable extends Component {
@@ -13,9 +30,37 @@ export default class RenderTable extends Component {
   constructor(props,context){
     super(props, context);
 
+
+	  this.state = {
+		  openView:false,
+		  item:{}
+	  }
+
   }
 
+  openViewDialog(index){
+
+	  const list = this.props.items;
+	  console.log('item',list[index]);
+
+		this.setState({
+			item:list[index],
+			openView:!this.state.openView
+		});
+
+  }
+
+
 	render() {
+
+		 const actions = [
+			  <Button
+				label="关闭"
+				primary={true}
+				 style={{marginLeft:10}}
+				onTouchTap={this.openViewDialog.bind(this)}
+			  />,
+			];
 
 		if(!this.props.items.length){
 			return (
@@ -76,8 +121,8 @@ export default class RenderTable extends Component {
 							<TableRowColumn>{item.createdate}</TableRowColumn>
 							<TableRowColumn>Steve Brown</TableRowColumn>
 							<TableRowColumn>
-								  <Button label="查看"  type="link"/>
-								  <Button label="编辑"  type="link"/>
+								  <Button label="查看"  type="link" onClick={this.openViewDialog.bind(this,index)}/>
+								  <Button label="编辑"  type="link" onTouchTap={this.openViewDialog} />
 							 </TableRowColumn>
 						 </TableRow>
 
@@ -87,6 +132,16 @@ export default class RenderTable extends Component {
 				<TableFooter></TableFooter>
 
 				</Table>
+
+
+				<Dialog
+			title="查看"
+			modal={true}
+			actions={actions}
+			open={this.state.openView}
+				>
+				<ViewHtml item={this.state.item}/>
+			  </Dialog>
    </div>
   );
   }
