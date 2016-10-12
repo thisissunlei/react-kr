3ac2e7e8a160282ae585c1086ad54ffdf6a75db3
张屈 <joneyphair@qq.com>
2016-10-12 12:16:23 +0800

    feat:material ui ‘

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,30 +1,53 @@
+//Components
 import Calendar from './Calendar';
 import Section from './Section';
 import Form from './Form/index';
 import BreadCrumbs from './BreadCrumbs';
-
 import Table from './Table/index';
-
 import Date from './Date';
 import Loading from './Loading';
 import Pagination from './Pagination';
-
 import Button from './Button';
 import MyForm from './MyForm';
-import Dialog from './Dialog';
-
 import Notify from './Notify';
 
+//materal-ui
+import {
+	Menu,
+	MenuItem,
+	DropDownMenu,
+	IconMenu,
+	Divider,
+	FontIcon,
+	DatePicker,
+	Paper,
+	Avatar,
+	Dialog
+} from 'material-ui';
+
 module.exports = {
+
+	//Components
 	Calendar,
 	Section,
 	BreadCrumbs,
 	Date,
 	Loading,
 	Pagination,
-	Dialog,
 	Notify,
 	...Form,
 	...Table,
 	...Button,
+
+	//materal-ui
+	Menu,
+	MenuItem,
+	DropDownMenu,
+	IconMenu,
+	Divider,
+	FontIcon,
+	DatePicker,
+	Paper,
+	Avatar,
+	Dialog
 }
