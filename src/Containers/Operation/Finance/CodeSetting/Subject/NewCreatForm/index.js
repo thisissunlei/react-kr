a791c5159895fc79d04d4c72051a30240d40d9ab
dongfanai <dongfanai@krspace.cn>
2016-10-15 12:15:32 +0800

    科目配置

---
+++ src/Containers/Operation/Finance/CodeSetting/Subject/NewCreatForm/index.js
@@ -0,0 +1,28 @@
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'kr/Redux';
+import {reduxForm,formValueSelector} from 'redux-form';
+import {Dialog,Snackbar} from 'material-ui';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import {
+  Table, 
+  TableBody,
+  TableHeader, 
+  TableHeaderColumn, 
+  TableRow, 
+  TableRowColumn,
+  TableFooter,
+  Grid,
+  Row,
+  Col,
+  BreadCrumbs,
+  Section,
+  KrField,
+  LabelText,
+  Button,
+}from 'kr-ui';
+
+class NewCreateForm extends Component{
+
+}
+
