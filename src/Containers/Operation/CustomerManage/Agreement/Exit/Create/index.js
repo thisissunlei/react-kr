345a52fee6aed002053fc348860c5b919ad2bcaa
dongfanai <dongfanai@krspace.cn>
2016-10-16 15:36:00 +0800

    新建退租协议书

--- src/Containers/Operation/CustomerManage/Agreement/Exit/Create/index.js
+++ src/Containers/Operation/CustomerManage/Agreement/Exit/Create/index.js
@@ -101,7 +101,7 @@ export default  class JoinCreate extends Component {
     return (
 
 		 <div>
-		 	<BreadCrumbs children={['系统运营','客户管理','入驻协议']}/>
+		 	<BreadCrumbs children={['系统运营','客户管理','退租协议']}/>
 			<Section title="创建入驻协议书" description=""> 
 					<NewCreateForm onSubmit={this.onSubmit} fnaCorporation={fnaCorporation} paymentList={payment} payTypeList={payType} floorList={customer.floor} billList={this.state.billList} customer={customer} initialValues={initialValues}/>
 			</Section>
