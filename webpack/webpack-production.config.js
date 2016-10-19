57788bb9da9053f96905bee2b3ab5c7acaf784de
张屈 <joneyphair@qq.com>
2016-10-19 14:29:09 +0800

    chore:优化打包文件

--- webpack/webpack-production.config.js
+++ webpack/webpack-production.config.js
@@ -6,107 +6,106 @@ const TransferWebpackPlugin = require('transfer-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
-  entry: [
-	  path.join(process.cwd(), '/node_modules/babel-polyfill/lib/index.js'),
-	  path.join(process.cwd(), '/src/app.js'),
-  ],
-  resolve: {
-    extensions: ['', '.js', '.md'], // 加载这些类型的文件时不用加后缀
-    alias: {
-      'kr-ui': path.join(process.cwd(), '/src/Components'), 
-      'kr': path.join(process.cwd(), '/src'), 
-	 // 'material-ui': path.resolve(__dirname, '../meterial-ui'),
-    },
-  },
-  // 出口文件配置
-  output: {
-    path: buildPath,
-    filename: 'app.js',
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-	  /*
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      output: {
-        comments: false,
-      },
-    }),
-	  */
-	new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-	new ExtractTextPlugin('app.css'),
-	new HtmlWebpackPlugin({
-		publicPath: '/',
-		title: 'production',
-		filename: 'index.html',
-		template: './src/index.template.html',
-		inject:'body'
-	}),
-	new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, 'node-noop'),
-	new webpack.NoErrorsPlugin(),
-	/*
+	entry: [
+		path.join(process.cwd(), '/node_modules/babel-polyfill/lib/index.js'),
+		path.join(process.cwd(), '/src/app.js'),
+	],
+	resolve: {
+		extensions: ['', '.js', '.md'], // 加载这些类型的文件时不用加后缀
+		alias: {
+			'kr-ui': path.join(process.cwd(), '/src/Components'), 
+			'kr': path.join(process.cwd(), '/src'), 
+			// 'material-ui': path.resolve(__dirname, '../meterial-ui'),
+		},
+	},
+	// 出口文件配置
+	output: {
+		path: buildPath,
+		filename: 'app.js',
+	},
+	plugins: [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+			},
+			output: {
+				comments: false,
+			},
+		}),
+		new webpack.optimize.CommonsChunkPlugin({name:'common', filename:'common.js'}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production'),
+			},
+		}),
+		new ExtractTextPlugin({ filename: 'app.css', disable: false, allChunks: true }),
+		new HtmlWebpackPlugin({
+			publicPath: '/',
+			title: 'production',
+			filename: 'index.html',
+			template: './src/index.template.html',
+			inject:'body'
+		}),
+		new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, 'node-noop'),
+		new webpack.NoErrorsPlugin(),
+		/*
 	new TransferWebpackPlugin([
 		{from: 'root/css', to: 'css'},
 		{from: 'root/images', to: 'images'},
 	], path.resolve(__dirname, 'src')),
 	*/
-  ],
-  module: {
-	  loaders: [
-		  {
-			  test: /\.js$/,
-			  loader: 'babel-loader',
-			  exclude: /node_modules/,
-		  },
-		  {
-			  test: /\.json$/,
-			  loader: 'json-loader',
-		  },
-		  {
-			  test: /\.md$/,
-			  loader: 'raw-loader',
-		  },
-		  {
-			  test: /\.less$/,
-			  loader: "style-loader!css-loader!less-loader"
-		  },
-		  {
-			  test: /\.css$/, 
-			  loader: ExtractTextPlugin.extract('style-loader','css-loader')
-		  },
-		  {
-			  test: /\.(png|jpg|gif)$/,
-			  loader: 'file?name=[name].[ext]?[hash]'
-		  },
-		  {
-			  test: /\.eot/,
-			  loader : 'file?prefix=font/'
-		  },
-		  {
-			  test: /\.woff/,
-			  loader : 'file?prefix=font/&limit=10000&mimetype=application/font-woff'
-		  },
-		  {
-			  test: /\.ttf/,
-			  loader : 'file?prefix=font/'
-		  }, 
-		  {
-			  test: /\.svg/,
-			  loader : 'file?prefix=font/'
-		  }
-	  ],
-  },
-  eslint: {
-	  configFile: '../.eslintrc',
-  },
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.json$/,
+				loader: 'json-loader',
+			},
+			{
+				test: /\.md$/,
+				loader: 'raw-loader',
+			},
+			{
+				test: /\.less$/,
+				loader: "style-loader!css-loader!less-loader"
+			},
+			{
+				test: /\.css$/, 
+				loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader' })
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				loader: 'file?name=[name].[ext]?[hash]'
+			},
+			{
+				test: /\.eot/,
+				loader : 'file?prefix=font/'
+			},
+			{
+				test: /\.woff/,
+				loader : 'file?prefix=font/&limit=10000&mimetype=application/font-woff'
+			},
+			{
+				test: /\.ttf/,
+				loader : 'file?prefix=font/'
+			}, 
+			{
+				test: /\.svg/,
+				loader : 'file?prefix=font/'
+			}
+		],
+	},
+	eslint: {
+		configFile: '../.eslintrc',
+	},
 };
 
 module.exports = config;
--- webpack/webpack-test.config.js
+++ webpack/webpack-test.config.js
@@ -4,22 +4,21 @@ const buildPath = path.join(process.cwd(), '/static');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var env = process.env.NODE_ENV;
+var env = process.env.NODE_ENV || 'development';
 
 const config = {
-  entry:[
-	  path.join(process.cwd(), '/node_modules/babel-polyfill/lib/index.js'),
-	  path.join(process.cwd(), '/src/app.js'),
-  ],
-  resolve: {
-    extensions: ['', '.js', '.md','.css'], 
-    alias: {
-      'kr-ui': path.join(process.cwd(), '/src/Components'), 
-      'kr': path.join(process.cwd(), '/src'), 
-	 // 'material-ui': path.resolve(__dirname, '../meterial-ui'),
-    },
-  },
-  /*
+	entry:[
+		path.join(process.cwd(), '/node_modules/babel-polyfill/lib/index.js'),
+		path.join(process.cwd(), '/src/app.js'),
+	],
+	resolve: {
+		extensions: ['', '.js', '.md','.css'], 
+		alias: {
+			'kr-ui': path.join(process.cwd(), '/src/Components'), 
+			'kr': path.join(process.cwd(), '/src'), 
+		},
+	},
+	/*
   devServer: {
 	  contentBase: "./static",
 	  port: 8001,
@@ -29,80 +28,88 @@ const config = {
 	  stats: 'normal',
   },
   */
-  // devtool: 'eval',
-  output: {
-	  path: buildPath,
-	  filename: 'bundle.js',
-	  publicPath:"http://127.0.0.1:8001/"
-  },
-  plugins: [
-
-	  new webpack.optimize.OccurenceOrderPlugin(),
-	  new webpack.HotModuleReplacementPlugin(),
-	  new webpack.NoErrorsPlugin(),
-
-	  new webpack.DefinePlugin({
-		  'process.env.NODE_ENV': JSON.stringify(env)
-	  }),
-	  new ExtractTextPlugin('app.css', { allChunks: true }),
-	  new HtmlWebpackPlugin({
-		  title: '36kr融资',
-		  filename: 'index.html',
-		  template: './src/index.template.html',
-		  inject:'body'
-	  }),
-	  new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, 'node-noop')
-  ],
-  module: {
-	  exprContextRegExp: /$^/,
-	  exprContextCritical: false,
-	  loaders: [
-		  {
-			  test: /\.jsx?$/,
-			  loaders: [
-				  'react-hot',
-				  'babel-loader',
-			  ],
-			  exclude: /(node_modules|bower_components)/
-		  },
-		  {
-			  test: /\.json$/,
-			  loader: 'json-loader',
-		  },
-		  {
-			  test: /\.md$/,
-			  loader: 'raw-loader',
-		  },
-		  {
-			  test: /\.css$/,
-			  loader: 'style-loader!css-loader',
-		  },
-		  {
-			  test: /\.less$/,
-			  loader: "style-loader!css-loader!less-loader"
-		  },
-		  {
-			  test: /\.(png|jpg|gif)$/,
-			  loader: 'file?name=[name].[ext]?[hash]'
-		  },
-		  {
-			  test: /\.eot/,
-			  loader : 'file?prefix=font/'
-		  },
-		  {
-			  test: /\.woff/,
-			  loader : 'file?prefix=font/&limit=10000&mimetype=application/font-woff'
-		  },
-		  {
-			  test: /\.ttf/,
-			  loader : 'file?prefix=font/'
-		  }, 
-		  {
-			  test: /\.svg/,
-			  loader : 'file?prefix=font/'
-		  }
-	  ],
-  }
+	devtool: 'eval-source-map',
+	output: {
+		path: buildPath,
+		filename: 'bundle.js',
+		publicPath:"/"
+	},
+	plugins: [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+			},
+			output: {
+				comments: false,
+			},
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(env)
+		}),
+		new ExtractTextPlugin({ filename: 'app.css', disable: false, allChunks: true }),
+		new HtmlWebpackPlugin({
+			title: '36kr融资',
+			filename: 'index.html',
+			template: './src/index.template.html',
+			inject:'body'
+		}),
+		new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, 'node-noop')
+	],
+	module: {
+		exprContextRegExp: /$^/,
+		exprContextCritical: false,
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loaders: [
+					'babel-loader',
+				],
+				exclude: /(node_modules|bower_components)/
+			},
+			{
+				test: /\.json$/,
+				loader: 'json-loader',
+			},
+			{
+				test: /\.md$/,
+				loader: 'raw-loader',
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader',
+			},
+			{
+				test: /\.less$/,
+				loader: "style-loader!css-loader!less-loader"
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				loader: 'file?name=[name].[ext]?[hash]'
+			},
+			{
+				test: /\.eot/,
+				loader : 'file?prefix=font/'
+			},
+			{
+				test: /\.woff/,
+				loader : 'file?prefix=font/&limit=10000&mimetype=application/font-woff'
+			},
+			{
+				test: /\.ttf/,
+				loader : 'file?prefix=font/'
+			}, 
+			{
+				test: /\.svg/,
+				loader : 'file?prefix=font/'
+			}
+		],
+	},
+	eslint: {
+		configFile: '../.eslintrc',
+	},
 };
 
 module.exports = config;
