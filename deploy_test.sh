dfeb165d5abf80f8f06440a689170a79a8bbe12c
张屈 <joneyphair@qq.com>
2016-10-19 14:58:32 +0800

    fix

--- deploy_test.sh
+++ deploy_test.sh
@@ -4,5 +4,5 @@ npm run build
 
 target_site=www@10.1.60.201
 
-rsync -cza --delete-before  -e 'ssh -p 9830'  ./build/* ${target_site}:/data/work/frontend/kr-admin/ >/dev/null
+rsync -cza --delete-before  -e 'ssh -p 9830'  ./build/* ${target_site}:/data/work/frontend/kr-node-proxy/static/ >/dev/null
 
