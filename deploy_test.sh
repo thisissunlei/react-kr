859e2a54a9a0417f29cba9db534a9987108b527a
张屈 <joneyphair@qq.com>
2016-10-19 13:47:58 +0800

    chore:增加测试环境脚本

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-target=${1:7}
 
-npm run  build:$target
+npm run build
 
-if [ $target = 'test' ] || [ $target = 'test4' ]; then
-  server='03'
-elif [ $target = 'test2' ] || [ $target = 'test3' ]; then
-  server='02'
-elif [ $target = 'dev' ]; then
-  server='04'
-fi
+rsync -rvltOD ./build/* krspace01:/data/work/kr-admin
 
-# rsync -rvltOD ./dist/* "dev$server:/data/work/frontend/$target/36kr/space/dist"
