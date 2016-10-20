c9fd4d5be58be825452aeb36f0dac8b5190a610b
张屈 <joneyphair@qq.com>
2016-10-20 14:20:02 +0800

    fix:dev

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-npm run  test 
+npm run  build 
 
 target_site=www@10.1.60.201
 
-rsync -cza --delete-before  -e 'ssh -p 9830'  ./test/* ${target_site}:/data/work/frontend/kr-node-proxy/static/
+rsync -cza --delete-before  -e 'ssh -p 9830'  ./build/* ${target_site}:/data/work/frontend/kr-node-proxy/static/
 
