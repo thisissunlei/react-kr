9e2ae2125e04daf8de098fb07ebb396f688c606c
张屈 <joneyphair@qq.com>
2016-10-22 13:31:30 +0800

    feat:增加表单组件

--- src/Containers/Undefined/index.js
+++ src/Containers/Undefined/index.js
@@ -5,6 +5,8 @@ import {bindActionCreators} from 'redux';
 import * as actionCreators from 'kr-ui/../Redux/Actions';
 
 import {
+	Form,
+	KrField,
 	Table,
  	TableBody,
 	TableHeader,
@@ -35,18 +37,41 @@ export default class Undefined extends Component{
 
 	constructor(props,context){
 		super(props, context);
+
+		this.onSubmit = this.onSubmit.bind(this);
 	}
 
 	componentDidMount() {
 
 	}
+	onSubmit(values){
+		console.log('--values',values);
+
+	}
 
 	render(){
+
+		let initialValues = {
+			lessorContacttel:'haa',
+			age:1,
+		}
 
 		return(
 
 			<div>
 					<Section title="出错了" description="" >
+
+						<Form name="jyayayoinForm" initialValues={initialValues} onSubmit={this.onSubmit}>
+								<KrField grid={1/2}  name="age" type="text" component="input" label="age" /> 
+								<KrField grid={1/2}  name="lessorContacttel" type="text" component="input" label="电话" /> 
+								<KrField grid={1/2}  name="lessorContacttel" type="text" component="input" label="电话" /> 
+								<KrField grid={1/2}  name="lessorContacttel" type="text" component="input" label="电话" /> 
+								<KrField grid={1/2}  name="lessorContacttel" type="text" component="input" label="电话" /> 
+								<KrField grid={1/2}  name="lessorContacttel" type="text" component="input" label="电话" /> 
+
+								 <Button  label="确定" type="submit" primary={true} /> 
+
+						</Form>
 
 					</Section>
 			</div>		
