b2af1fcfb0373ec4bacbce0f8e560642fe7d8c6e
张屈 <joneyphair@qq.com>
2016-10-22 15:46:59 +0800

    feat:form component 增加校验字段

--- src/Containers/Undefined/index.js
+++ src/Containers/Undefined/index.js
@@ -44,9 +44,9 @@ export default class Undefined extends Component{
 	componentDidMount() {
 
 	}
+
 	onSubmit(values){
 		console.log('--values',values);
-
 	}
 
 	render(){
@@ -56,12 +56,32 @@ export default class Undefined extends Component{
 			age:1,
 		}
 
+		let validations = {
+			age:{
+				minLength:{
+					value:5,
+					message:'最小长度'
+				},
+			},
+			lessorContacttel:{
+				minLength:{
+					value:1,
+					message:'最小长度'
+				},
+				maxLength:{
+					value:2,
+					message:'最大长度为2'
+				}
+			}
+
+	   	};
+
 		return(
 
 			<div>
 					<Section title="出错了" description="" >
 
-						<Form name="jyayayoinForm" initialValues={initialValues} onSubmit={this.onSubmit}>
+						<Form name="jyayayoinForm" initialValues={initialValues} onSubmit={this.onSubmit} validations={validations}>
 								<KrField grid={1/2}  name="age" type="text" component="input" label="age" /> 
 								<KrField grid={1/2}  name="lessorContacttel" type="text" component="input" label="电话" /> 
 								<KrField grid={1/2}  name="lessorContacttel" type="text" component="input" label="电话" /> 
