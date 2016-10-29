1bb574b9bdff1a93fc03057b815189d74a242f31
马超越 <machaoyue@36kr.com>
2016-10-29 17:53:12 +0800

    平面图

--- src/Containers/Operation/CommunityManage/Detail/FloorPlan/index.js
+++ src/Containers/Operation/CommunityManage/Detail/FloorPlan/index.js
@@ -10,6 +10,7 @@ import {
 	Grid,
 	Notify,
 	BreadCrumbs,
+	IframeContent,
 } from 'kr-ui';
 
 
@@ -17,6 +18,7 @@ export default  class FloorPlan extends Component {
 
 	constructor(props,context){
 		super(props, context);
+		this.getStationUrl = this.getStationUrl.bind(this);
 
 	}
 
@@ -24,12 +26,43 @@ export default  class FloorPlan extends Component {
 
 	 }
 
+	 getStationUrl(){
+
+	     let url = "http://optest.krspace.cn/krspace_operate_web/commnuity/communityFloorPlan/toCommunityFloorPlanList?communityId={communityId}&wherefloor={wherefloor}&date={date}&dateend={dateend}";
+
+		// stationVos = stationVos.map(function(item){
+		// 	var obj = {};
+		// 	obj.id = item.stationId;
+		// 	obj.type = item.stationType;
+		// 	return obj;
+		// });
+
+		let params = {
+			communityId:1,
+			wherefloor:3,
+			date:'2016-10-10',
+			dateend:'2016-10-10',
+		};
+
+		if(Object.keys(params).length){
+			for (let item in params) {
+				if (params.hasOwnProperty(item)) {
+					url = url.replace('{' + item + '}', params[item]);
+					delete params[item];
+				}
+			}
+		}
+
+		return url ;
+	}
+
 
   render() {
 
     return (
 
 		 <div>
+							<IframeContent src={this.getStationUrl()} onClose={this.onIframeClose}/>
 
 		</div>
 	);
