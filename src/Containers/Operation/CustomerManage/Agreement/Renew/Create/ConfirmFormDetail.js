12b8a39aff7db4375335a736eccf25af337d2b74
张屈 <joneyphair@qq.com>
2016-10-31 18:17:35 +0800

    fix:续租时间格式的问题

--- src/Containers/Operation/CustomerManage/Agreement/Renew/Create/ConfirmFormDetail.js
+++ src/Containers/Operation/CustomerManage/Agreement/Renew/Create/ConfirmFormDetail.js
@@ -56,8 +56,11 @@ export default class ConfirmFormDetail  extends Component{
 	render(){
 
 		let {detail, optionValues} = this.props;
-        console.log("----------",detail, optionValues);
+
+		detail = Object.assign({},detail);
+
         var leasorName ;
+
         optionValues.fnaCorporationList.map((item)=>{
         	if(item.value === detail.leaseId){
         		return leasorName = item.corporationAddress;
