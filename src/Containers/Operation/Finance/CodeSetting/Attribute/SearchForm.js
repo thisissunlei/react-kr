e2f20e22022dfab7dc72ab4eca47caad3e9bbcb6
dongfanai <dongfanai@krspace.cn>
2016-11-02 22:35:41 +0800

    属性配置，科目配置搜索修改

--- src/Containers/Operation/Finance/CodeSetting/Attribute/SearchForm.js
+++ src/Containers/Operation/Finance/CodeSetting/Attribute/SearchForm.js
@@ -1,8 +1,19 @@
-import React, {Component, PropTypes} from 'react';
-import {connect} from 'kr/Redux';
+import React, {
+	Component,
+	PropTypes
+} from 'react';
+import {
+	connect
+} from 'kr/Redux';
 
-import {reduxForm,formValueSelector} from 'redux-form';
-import {Actions,Store} from 'kr/Redux';
+import {
+	reduxForm,
+	formValueSelector
+} from 'redux-form';
+import {
+	Actions,
+	Store
+} from 'kr/Redux';
 
 import {
 	KrField,
@@ -10,58 +21,67 @@ import {
 	Row,
 	Col,
 	Button,
+	ListGroup,
+	ListGroupItem
 } from 'kr-ui';
 
 
- class SearchForm extends Component{
+class SearchForm extends Component {
 
-	 static PropTypes = {
-		 onSubmit:React.PropTypes.func,
-		 onCancel:React.PropTypes.func,
-	 }
+	static PropTypes = {
+		onSubmit: React.PropTypes.func,
+		onCancel: React.PropTypes.func,
+	}
 
-	constructor(props){
+	constructor(props) {
 		super(props);
 
 		this.onSubmit = this.onSubmit.bind(this);
 		this.onCancel = this.onCancel.bind(this);
 	}
 
-	 onSubmit(form){
+	onSubmit(form) {
 
-	 	form.pageSize = 20;
-	 	form.page = 1;
+		form.pageSize = 20;
+		form.page = 1;
 
-	 	console.log('--------',form)
-		const {onSubmit} = this.props;
-	    onSubmit && onSubmit(form);
-	 }
+		console.log('--------', form)
+		const {
+			onSubmit
+		} = this.props;
+		onSubmit && onSubmit(form);
+	}
 
-	 onCancel(){
-		 const {onCancel} = this.props;
-		 onCancel && onCancel();
-	 }
+	onCancel() {
+		const {
+			onCancel
+		} = this.props;
+		onCancel && onCancel();
+	}
 
-	render(){
+	render() {
 
-		const { error, handleSubmit, pristine, reset} = this.props;
+		const {
+			error,
+			handleSubmit,
+			pristine,
+			reset
+		} = this.props;
 
 		return (
 
 			<form onSubmit={handleSubmit(this.onSubmit)}>
-
-
-              <Row>
-				<Col md={8}><KrField name="searchParam" type="text"  placeholder='请输入属性名称'/></Col>
-				<Col md={4}><Button  label="查询" type="submit" primary={true} /></Col>
-              </Row>
-				
-			   
+				<ListGroup>
+					<ListGroupItem> <KrField name="searchParam" type="text"  placeholder='请输入属性名称' simple={true}/></ListGroupItem>
+					<ListGroupItem> <Button  label="查询" type="submit" primary={true} /></ListGroupItem>
+				</ListGroup>
 					
-				</form>
+			</form>
 		);
 	}
 }
 
 
-export default reduxForm({ form: 'searchForm'})(SearchForm);
+export default reduxForm({
+	form: 'searchForm'
+})(SearchForm);
