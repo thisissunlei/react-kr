8f823073ad8e22c9f4f29b96faf4e381a48be231
马超越 <machaoyue@36kr.com>
2016-11-03 12:24:31 +0800

    修改标签

--- src/Containers/Operation/CustomerManage/Agreement/Reduce/Detail/index.js
+++ src/Containers/Operation/CustomerManage/Agreement/Reduce/Detail/index.js
@@ -172,6 +172,7 @@ function getOrderUrl(){
 						  
 
 											  </Section>
+											  </div>
 				  </div>
 		  );
 
@@ -193,7 +194,6 @@ function getOrderUrl(){
 				  </Row>
 			  </Grid>
 			</Section>
-</div>
       </div>
 
     );
