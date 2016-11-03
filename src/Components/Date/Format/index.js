ffb04d8345b3c87d1a37053fffaba4212c60c927
张屈 <joneyphair@qq.com>
2016-11-03 19:10:53 +0800

    fix:修改时间组件

--- src/Components/Date/Format/index.js
+++ src/Components/Date/Format/index.js
@@ -18,20 +18,10 @@ export default class Format extends React.Component {
 
 	render() {
 
-		let {className,value,format,time} = this.props;
-
-
-		let result = '';
-
-		try{
-			result =  dateFormat(value,format);
-		}catch(err){
-			let time=new Date(value*1)
-			result = dateFormat(time,"yyyy-mm-dd");
-		}
+		let {className,value,format} = this.props;
 
 		return (
-			<span>{result}</span>
+			<span>{dateFormat(value,format)}</span>
 		);
 
 
