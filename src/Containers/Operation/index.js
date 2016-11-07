d3583da67ac073050eb04c0120b884b5d37d6317
dongfanai <dongfanai@krspace.cn>
2016-11-07 11:45:14 +0800

    up

--- src/Containers/Operation/index.js
+++ src/Containers/Operation/index.js
@@ -1,29 +1,21 @@
-import { default as Home } from './Home';
-import { default as Finance } from './Finance';
-import { default as JoinOrder} from './JoinOrder';
-import { default as CustomerManage} from './CustomerManage';
-import { default as CommunityManage} from './CommunityManage';
+import {
+	default as Home
+} from './Home';
+
+import {
+	default as JoinOrder
+} from './JoinOrder';
+import {
+	default as CustomerManage
+} from './CustomerManage';
+import {
+	default as CommunityManage
+} from './CommunityManage';
 
 
 module.exports = {
 	Home,
-	Finance,
 	JoinOrder,
 	CustomerManage,
 	CommunityManage,
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
