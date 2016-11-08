1020af226f64f466e9fe7173871298fe66ba0f64
张屈 <joneyphair@qq.com>
2016-11-08 18:53:03 +0800

    fix:prod test build

--- deploy_test.sh
+++ deploy_test.sh
@@ -4,5 +4,5 @@ npm run  test
 
 target_site=www@10.1.60.201
 
-rsync -cza --delete-before  -e 'ssh -p 9830'  ./test/* ${target_site}:/data/work/frontend/kr-node-proxy/static/
+rsync -cza --delete-before  -e 'ssh -p 9830'  ./webpack/dist/* ${target_site}:/data/work/frontend/kr-node-proxy/static/
 
