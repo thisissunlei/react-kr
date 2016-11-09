f9a9d234c0ea88398c5840b29bd697e3899520e8
dongfanai <dongfanai@krspace.cn>
2016-11-09 22:07:38 +0800

    备注组件修改

--- src/Components/Form/Field/TextareaComponent/index.js
+++ src/Components/Form/Field/TextareaComponent/index.js
@@ -45,7 +45,7 @@ export default class TextareaComponent extends React.Component {
 		return (
 			<WrapComponent label={label} wrapStyle={style} requireLabel={requireLabel} inline={inline}>
 				<textarea {...input} placeholder={placeholder|| label} disabled={disabled} col={col} row={row}  ></textarea>
-				<div style={{width:40,height:30,lineHeight:"30px",color:'#cccccc',float:'right',fontSize:'14px'}}><span className="len">{len}</span>/<span className="size">{maxSize}</span></div>
+				<div style={{width:40,height:30,lineHeight:"30px",color:'#cccccc',float:'right',fontSize:'14px'}}><span className="len">{len}</span>/<span className="size">{maxSize?maxSize:200}</span></div>
 				{touched && error && <div className="error-wrap"> <span>{error}</span></div> }
 			</WrapComponent>
 		);
