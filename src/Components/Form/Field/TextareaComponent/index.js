4a5ebb63ac0eb7e77e569886d94a936c7b5f2d32
dongfanai <dongfanai@krspace.cn>
2016-11-09 21:41:24 +0800

    备注组件修改

--- src/Components/Form/Field/TextareaComponent/index.js
+++ src/Components/Form/Field/TextareaComponent/index.js
@@ -2,23 +2,39 @@ import React from 'react';
 
 import WrapComponent from '../WrapComponent';
 
-export default class TextareaComponent extends React.Component{
+export default class TextareaComponent extends React.Component {
 
 	static PropTypes = {
-		onChange:React.PropTypes.func
+		onChange: React.PropTypes.func
 	}
-	
-	constructor(props){
+
+	constructor(props) {
 		super(props)
 	}
 
-	render(){
+	render() {
 
-         let { input, label, type, meta: { touched, error } ,requireLabel,disabled,placeholder,col,row,style,inline} = this.props;
+		let {
+			input,
+			label,
+			type,
+			meta: {
+				touched,
+				error
+			},
+			requireLabel,
+			disabled,
+			placeholder,
+			col,
+			row,
+			style,
+			inline
+		} = this.props;
 
 		return (
 			<WrapComponent label={label} wrapStyle={style} requireLabel={requireLabel} inline={inline}>
 				<textarea {...input} placeholder={placeholder|| label} disabled={disabled} col={col} row={row}></textarea>
+				<div style={{width:36,height:30,lineHeight:"30px",color:'#cccccc',float:'right'}}><span className="len">1</span>/<span className="size">100</span></div>
 				{touched && error && <div className="error-wrap"> <span>{error}</span></div> }
 			</WrapComponent>
 		);
@@ -26,4 +42,3 @@ export default class TextareaComponent extends React.Component{
 	}
 
 }
-
