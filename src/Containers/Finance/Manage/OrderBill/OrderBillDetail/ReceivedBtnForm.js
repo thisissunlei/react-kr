0f8ffd55511760e61ac27e5e2f21a641d18673ef
dongfanai <dongfanai@krspace.cn>
2016-11-17 20:40:23 +0800

    添加回款样式

--- src/Containers/Finance/Manage/OrderBill/OrderBillDetail/ReceivedBtnForm.js
+++ src/Containers/Finance/Manage/OrderBill/OrderBillDetail/ReceivedBtnForm.js
@@ -68,12 +68,12 @@ class ReceivedBtnForm extends Component {
 		this.state = {
 
 		}
-    
+
 		//Store.dispatch(reset('ReceivedBtnForm'));
 	}
 
 	componentDidMount() {
-        
+
 
 		let initialValues = {
 			sumSign: '1',
@@ -116,11 +116,11 @@ class ReceivedBtnForm extends Component {
 			changeValues,
 			typeList
 		} = this.props;
-        
-        let heightStyle={
-        	width:'546',
-        	height:'72'
-        }
+
+		let heightStyle = {
+			width: '546',
+			height: '72'
+		}
 
 
 		return (
@@ -129,24 +129,31 @@ class ReceivedBtnForm extends Component {
                  
 					      <form onSubmit={handleSubmit(this.onSubmit)} style={{marginTop:45,marginLeft:'10px'}}>
                             <KrField  name="mainbillid" type="hidden" component="input"/>
-		                    <KrField  label="代码名称"  style={{width:262,marginRight:'30px'}} name="accountId" type="select" options={optionList} requireLabel={true}/>
-						     <KrField name="sumSign"  style={{width:262,marginRight:'30px'}} component="group" label="金额正负" requireLabel={true}>
-				                <KrField name="sumSign" style={{width:112,marginRight:'30px'}} label="正" component="radio" type="radio" value="0"/>
-				                <KrField name="sumSign"style={{width:112,marginRight:'30px'}} label="负" component="radio" type="radio" value="1"/>
+		                    <KrField  label="代码名称" grid={1/2} right={20} name="accountId" type="select" options={optionList} requireLabel={true}/>
+						     <KrField name="sumSign" grid={1/2} left={20} component="group" label="金额正负" requireLabel={true}>
+				                <KrField name="sumSign" grid={1/2} right={20} label="正" component="radio" type="radio" value="0"/>
+				                <KrField name="sumSign"  grid={1/2} left={20}label="负" component="radio" type="radio" value="1"/>
 			                </KrField>
                             
-						    <KrField component="date"  style={{width:262,marginRight:'30px'}} label="回款日期" name="receiveDate" requireLabel={true}/>
-						     <KrField label="上传附件" style={{width:262,marginRight:'30px'}} name="fileids" component="file" />
-                             <KrField label="交易编号" placeholder='请输入交易编号' style={{width:262,marginRight:'30px'}}  name="dealCode"  component="input" type="text" requireLabel={true}/>
-                             <KrField label="是否自动拆分" style={{width:262,marginRight:'30px'}} name="autoSplit" component="select" options={
+						    <KrField component="date" grid={1/2} right={20} label="回款日期" name="receiveDate" requireLabel={true}/>
+						     <KrField label="上传附件" grid={1/2} left={20}  name="fileids" component="file" />
+                             <KrField label="交易编号" grid={1/2} right={20} placeholder='请输入交易编号'   name="dealCode"  component="input" type="text" requireLabel={true}/>
+                             <KrField label="是否自动拆分" grid={1/2} left={20} name="autoSplit" component="select" options={
 						    	[{label:"是",value:"1"},{label:"否",value:"0"}]
 						    } requireLabel={true}/>
 
 						    {parseInt(changeValues.autoSplit)?<div>
-						    	 <KrField label="金额（元） "  style={{width:262,marginRight:'30px'}} name="sum" component="input" type="text" requireLabel={true} placeholder='请输入金额'/>
+						    	 <KrField label="金额（元） " grid={1/2}  name="sum" component="input" type="text" requireLabel={true} placeholder='请输入金额'/>
 						    </div>:<div>
-						      {typeList.map((item,index)=>						
-						         <KrField key={index} style={{width:262,marginRight:'30px'}} label={item.label} component="input" name={item.value} type="text"/>						 
+						      {typeList.map((item,index)=>{
+						      	if(index%2==0){
+									return <KrField key={index} grid={1/2} right={20} label={item.label} component="input" name={item.value} type="text"/>	
+						      	}else{
+						      		return <KrField key={index} grid={1/2} left={20} label={item.label} component="input" name={item.value} type="text"/>	
+						      	}
+						      	
+						      }						
+						         					 
 						       )}                           
 						    </div>}
 						    
@@ -189,15 +196,15 @@ const validate = values => {
 
 
 	if (!values.accountId) {
-		errors.accountId ='请填写代码名称';
+		errors.accountId = '请填写代码名称';
 	}
 
 	if (!values.receiveDate) {
-		errors.receiveDate ='请填写回款日期';
+		errors.receiveDate = '请填写回款日期';
 	}
 
 	if (!values.dealCode) {
-		errors.dealCode ='请填写交易编号';
+		errors.dealCode = '请填写交易编号';
 	}
 	if (!values.sum) {
 		errors.sum = '请填写金额';
