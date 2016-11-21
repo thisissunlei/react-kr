dff7f8370557c4721840f7a6d3922a5bddf964b9
dongfanai <dongfanai@krspace.cn>
2016-11-21 18:30:22 +0800

    计划表bug修复

--- src/Containers/Operation/CommunityManage/Detail/Schedule/BasicTable.js
+++ src/Containers/Operation/CommunityManage/Detail/Schedule/BasicTable.js
@@ -43,13 +43,12 @@ import DismantlingForm from './DismantlingForm';
 class SearchForm extends Component {
 	static defaultProps = {
 		tab: '',
+		Ids: React.PropTypes.string,
 	}
-	static defaultProps = {
-		communityids: React.PropTypes.string,
-	}
+
 	constructor(props) {
 		super(props);
-		console.log('communityids', this.props.communityIds)
+
 		this.onSubmit = this.onSubmit.bind(this);
 		this.state = {
 			currentYear: '2016',
@@ -61,12 +60,13 @@ class SearchForm extends Component {
 			page: 1,
 			pageSize: 15,
 			type: 'BILL',
-			communityids: this.props.communityIds,
+			ids: '',
 
 		};
 		this.getcommunity = this.getcommunity.bind(this);
 		this.selectCommunity = this.selectCommunity.bind(this);
 		this.getcommunity();
+
 
 	}
 
@@ -74,26 +74,21 @@ class SearchForm extends Component {
 	onSubmit(form) {
 
 		let {
-			communityids
-		} = this.state
-		let {
 			page,
 			pageSize,
-		} = this.state
-		let {
+			communityids,
+			ids,
 			formValues
-		} = this.state;
+		} = this.state
 
 		formValues = {
 			type: form.filter,
 			value: form.content,
-			communityids: communityids,
+			communityids: communityids || this.props.Id,
 			page: page,
 			pageSize: pageSize
 
 		}
-
-
 		const {
 			onSubmit
 		} = this.props;
@@ -521,6 +516,7 @@ export default class BasicTable extends Component {
 			_this.setState({
 				communityIds: communityIds
 			})
+
 			Store.dispatch(Actions.callAPI('getInstallmentplan', {
 				communityids: content.toString(),
 				value: '',
@@ -716,7 +712,7 @@ export default class BasicTable extends Component {
 		 				</p>
 		 			</div>
 		 		</div>
-		 		<SearchForm  onSubmit={this.onSubmit} communityIds={communityIds} onChange={this.onChange}/>
+		 		<SearchForm  onSubmit={this.onSubmit} Id={communityIds} onChange={this.onChange}/>
 		 		
 		 	</div>
 		 	
