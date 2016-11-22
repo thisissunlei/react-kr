39b767210baa92702bd114346f61d9600b0dd6a4
刘毅豪 <liuyihao@krspace.cn>
2016-11-22 19:47:01 +0800

    style

--- src/Containers/Operation/CustomerManage/Agreement/LessorManage/List/index.js
+++ src/Containers/Operation/CustomerManage/Agreement/LessorManage/List/index.js
@@ -150,7 +150,7 @@ export default class LessorManageList extends Component {
 
 		return (
 
-			<div>
+			<div className="hireFn">
 					<BreadCrumbs children={['系统运营','合同信息','出租方管理']}/>
 					<Section title="出租方管理" description="" style={{marginBottom:-5,minHeight:910}}>
 
