6345521775cf4f0757c35a61393c20b3b15077d5
张屈 <joneyphair@qq.com>
2016-11-24 19:10:34 +0800

    fix:添加测试环境部署脚本

--- deploy_test.sh
+++ deploy_test.sh
@@ -5,4 +5,17 @@ npm run  test
 
 target_site=www@10.1.60.201
 
+if [$1 == "test"]
+then
+target_site=www@10.1.60.201
+elif [$1 == "test01"]
+then
+target_site=www@114.215.78.9
+elif [$1 == "test02"]
+then
+target_site=www@114.215.78.48
+else
+target_site=www@10.1.60.201
+fi
+
 rsync -cza --delete-before  -e 'ssh -p 9830'  ./webpack/dist/* ${target_site}:/data/work/frontend/kr-node-proxy/static/
