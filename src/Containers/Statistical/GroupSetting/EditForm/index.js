fca79a223dde5f2158aec5a4226007b6f6ce985a
张屈 <joneyphair@qq.com>
2016-11-25 12:28:58 +0800

    feat:统计分组

---
+++ src/Containers/Statistical/GroupSetting/EditForm/index.js
@@ -0,0 +1,46 @@
+import React,{Component} from 'react';
+import { connect } from 'react-redux';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {Actions,Store} from 'kr/Redux';
+import {
+	KrField,
+	Table,
+	TableBody,
+	TableHeader,
+	TableHeaderColumn,
+	TableRow,
+	TableRowColumn,
+	TableFooter,
+	Button,
+	Section,
+	Grid,
+	Row,
+	Col,
+	Dialog,
+
+} from 'kr-ui';
+
+export default class Initialize  extends Component{
+
+	constructor(props,context){
+		super(props, context);
+
+		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+	}
+
+
+
+	render(){
+
+		return(
+
+			<div>
+					<Section title="分组配置" description="" >
+
+
+					</Section>
+			</div>
+		);
+	}
+
+}
