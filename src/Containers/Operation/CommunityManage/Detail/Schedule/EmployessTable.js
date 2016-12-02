2166f00170b297dee3e9c4432be47532521825ca
dongfanai <dongfanai@krspace.cn>
2016-12-02 11:00:51 +0800

    fix:新增员工

--- src/Containers/Operation/CommunityManage/Detail/Schedule/EmployessTable.js
+++ src/Containers/Operation/CommunityManage/Detail/Schedule/EmployessTable.js
@@ -529,10 +529,10 @@ export default class EmployessTable extends Component {
 				modal={true}
 				open={this.state.openNewmeber}
 				onClose={this.onClose}
-				contentStyle={{width:465,height:600}}
+				contentStyle={{width:620,height:600}}
 			>
 
-				<IframeContent  width={450} height={600} src={this.getStationUrl()}  onClose={this.onIframeClose}  />
+				<IframeContent  width={500} height={600} src={this.getStationUrl()}  onClose={this.onIframeClose}  />
 			</Dialog>
 
 		</div>
