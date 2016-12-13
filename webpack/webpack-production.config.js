8fddda3f6c3550d3562887106ae89cb08944b1b6
张屈 <joneyphair@qq.com>
2016-12-13 13:50:03 +0800

    fix:console

--- webpack/webpack-production.config.js
+++ webpack/webpack-production.config.js
@@ -39,24 +39,8 @@ const config = {
     new webpack.optimize.UglifyJsPlugin({
 			compress: {
 				warnings: false,
-        properties: true,
-        sequences: true,
-        dead_code: true,
-        conditionals: true,
-        comparisons: true,
-        evaluate: true,
-        booleans: true,
-        unused: true,
-        loops: true,
-        hoist_funs: true,
-        cascade: true,
-        if_return: true,
-        join_vars: true,
         drop_console: true,
         drop_debugger: true,
-        unsafe: true,
-        hoist_vars: true,
-        negate_iife: true
 			},
 			output: {
 				comments: false,
