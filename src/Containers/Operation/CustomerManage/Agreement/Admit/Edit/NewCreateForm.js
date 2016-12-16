46f722b940d1624006c4afc66056d2e0f1aebfa1
张屈 <joneyphair@qq.com>
2016-12-16 14:39:40 +0800

    fix:入驻合同会议室显示

--- src/Containers/Operation/CustomerManage/Agreement/Admit/Edit/NewCreateForm.js
+++ src/Containers/Operation/CustomerManage/Agreement/Admit/Edit/NewCreateForm.js
@@ -515,8 +515,8 @@ class NewCreateForm extends Component {
 				<KrField right={60} grid={1}  name="fileIdList" component="file" label="上传附件" defaultValue={optionValues.contractFileList} requireLabel={true}/>
 
                <KrField right={60} grid={1/1} component="group" label="租赁项目" requireLabel={true}>
-								<KrField grid={1/2}  name="stationnum" type="text" component="labelText" label="工位" value={changeValues.stationnum} />
-								<KrField grid={1/2}  name="boardroomnum" type="text" component="labelText" label="会议室" value={changeValues.boardroomnum==0?'0':changeValues.boardroomnum} />
+								<KrField grid={1/2}  name="stationnum" type="text" component="labelText" label="工位" value={changeValues.stationnum} defaultValue={0} />
+								<KrField grid={1/2}  name="boardroomnum" type="text" component="labelText" label="会议室" value={changeValues.boardroomnum} defaultValue={0} />
 				</KrField>
                 <DotTitle title='租赁明细'>
 
