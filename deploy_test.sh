91ebbe720ac665b5c967ca17833886d5096d137a
张屈 <joneyphair@qq.com>
2016-12-16 12:39:55 +0800

    fix:webpack

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 npm run dll
+npm run test
 
 target_site=www@10.1.60.201
 target_site_port=9830
@@ -8,15 +9,12 @@ target_site_port=9830
 if [ $1 = 'test01' ]; then
   target_site=www@114.215.78.9
   target_site_port=22
-  npm run test01
 elif [ $1 = 'test02' ]; then
     target_site=www@114.215.78.48
     target_site_port=22
-    npm run test02
 elif [ $1 = 'test' ]; then
     target_site=www@10.1.60.201
     target_site_port=9830
-    npm run test
 fi
 
 echo $target_site
