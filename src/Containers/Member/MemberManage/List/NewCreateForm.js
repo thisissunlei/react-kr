b0462a7e08dfbe375cfe8a53034918e2eabcb708
谭琳琳 <tanlinlin@krspace.cn>
2016-12-21 13:29:25 +0800

    feature:增加会员卡号校验

--- src/Containers/Member/MemberManage/List/NewCreateForm.js
+++ src/Containers/Member/MemberManage/List/NewCreateForm.js
@@ -234,7 +234,7 @@ import imgLine from './images/line.png'
 						<KrField name="sendMsg" grid={1/2} label="是" type="radio" value="1"/>
 						<KrField name="sendMsg" grid={1/2} label="否" type="radio" value="0" />
               </KrField>
-        <KrField grid={1/2} name="foreignCode" type="input" label="会员卡号" requireLabel={true} onBlur={this.foreignCodeBlur} style={{width:'252px'}}/>
+        <KrField grid={1/2} name="foreignCode" type="input" label="会员卡号" requireLabel={true} onBlur={this.foreignCodeBlur} style={{width:'252px'}} requiredValue={true} pattern={/^\w{8}(,\w{8})*$/} errors={{requiredValue:'会员卡号为必填项',pattern:'会员卡号应由8位数字或字母组成'}}/>
 				<Grid style={{marginTop:30,marginLeft:'-40px'}}>
 					<Row>
 						<Col md={12} align="center">
