9e8b79193c0971ce0871b583578225051971aa4c
张弛 <zhangchi@krspace.cn>
2016-12-22 18:11:06 +0800

    feature电子合同Station

--- src/PureComponents/Agreement/Print/Footer/index.js
+++ src/PureComponents/Agreement/Print/Footer/index.js
@@ -35,7 +35,7 @@ export default class Initialize extends Component {
 
 			<div className="print-Footer">
 
-						<p>双方证实上述资料属实，并已阅读及同意本页和背页所载之协议</p>
+						<p>双方证实上述内容属实，并已阅读及同意本页和背页所载之协议</p>
 						<p className="print-Bottom">氪空间&ensp;<span className="dott"></span>&ensp;让办公更简单<span className="tel">Tel：400-807-3636</span></p>
 
 			</div>
