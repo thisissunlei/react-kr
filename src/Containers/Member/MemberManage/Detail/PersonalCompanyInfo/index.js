26f55969db675906b24c521b95924dc3b2494c7c
谭琳琳 <tanlinlin@krspace.cn>
2016-12-22 15:44:13 +0800

    fix

--- src/Containers/Member/MemberManage/Detail/PersonalCompanyInfo/index.js
+++ src/Containers/Member/MemberManage/Detail/PersonalCompanyInfo/index.js
@@ -54,17 +54,17 @@ export default class  CompanyInfo extends Component {
 		return (
 
 			<div className='ui-detail-order'>
-			           <KrField grid={1/3} alignRight={true} label="公司名称:" component="labelText" value={companyInfo.customerCompany} defaultValue="无"/>
+			           <KrField grid={1/3} alignRight={true} label="公司名称:" component="labelText" value={companyInfo.customerCompany} defaultValue="无" style={{marginRight:'20px'}}/>
 
 			           <KrField grid={1/3}  alignRight={true} component="labelText"  label="投资轮次:" value={companyInfo.round} defaultValue="无" />
 
-								 <KrField grid={1/3}  alignRight={true} component="labelText"  label="项目名称:" value={companyInfo.projectCategoryName} defaultValue="无" />
+								 <KrField grid={1/3}  alignRight={true} component="labelText"  label="项目名称:" value={companyInfo.projectCategoryName} defaultValue="无" style={{marginLeft:'-20px'}}/>
 
-                <KrField grid={1/3} alignRight={true} label="办公城市:" component="labelText"  value={companyInfo.cityName} defaultValue="无"/>
+                <KrField grid={1/3} alignRight={true} label="办公城市:" component="labelText"  value={companyInfo.cityName} defaultValue="无" style={{marginRight:'20px'}}/>
 
 			           <KrField grid={1/3} alignRight={true} label="详细地址:" component="labelText"  value={companyInfo.detailAddress} defaultValue="无"/>
 
-			           <KrField grid={1/3} alignRight={true} label="公司网址:" component="labelText" type="date" value={companyInfo.website} defaultValue="无"/>
+			           <KrField grid={1/3} alignRight={true} label="公司网址:" component="labelText" type="date" value={companyInfo.website} defaultValue="无" style={{marginLeft:'-20px'}}/>
 			</div>
 
 		)
