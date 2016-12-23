690a659e4e33347f872517f09fd2cd7bd7f9c1c3
马超越 <machaoyue@36kr.com>
2016-12-23 10:05:00 +0800

    fix:解决合并冲突

--- src/Containers/Member/MemberManage/index.js
+++ src/Containers/Member/MemberManage/index.js
@@ -2,9 +2,12 @@
 
 import { default as List} from './List';
 import { default as Setting} from './Setting';
-
+import { default as Card} from './Card';
+import { default as Detail} from './Detail';
 module.exports = {
 	List,
-  Setting
+  Setting,
+  Detail,
+  Card
 
 }
