7fd103c4e64068b5f13ed027cf576759b0fbd138
dongfanai <dongfanai@krspace.cn>
2016-12-28 15:01:11 +0800

    style：header

---
+++ src/Containers/Operation/CustomerManage/Agreement/Increase/Print/State.js
@@ -0,0 +1,65 @@
+import mobx, {
+	observable,
+	action,
+	asMap,
+	computed,
+	extendObservable
+} from 'mobx';
+import {
+	Actions,
+	Store
+} from 'kr/Redux';
+
+let State = observable({
+	name: 'dd',
+	values: {},
+	fields: {},
+	initialValues: {},
+	syncErrors: {},
+	validations: {},
+	data: [],
+	installmentPlans: [],
+	Baseinfo: {},
+	stationVOs: [],
+	installmentPlansList: []
+});
+
+//action
+State.getBasicInfo = action(function(params) {
+	var _this = this;
+	Store.dispatch(Actions.callAPI('checkinagreement-print-info', {
+		contractId: params.id
+	})).then(function(response) {
+		_this.Baseinfo = response;
+		if (response.stationVOs.length >= 7) {
+			_this.stationVOs = response.stationVOs;
+		} else {
+			var stationVOs = response.stationVOs;
+			for (var i = 0, len = 7 - stationVOs.length; i < len; i++) {
+				var obj = {
+					leaseDate: " ",
+					lineTotal: " ",
+					num: " ",
+					stationName: " ",
+					stationTypeName: " ",
+					unitPrice: " "
+				}
+				stationVOs.push(obj)
+			}
+			_this.stationVOs = stationVOs;
+
+		}
+
+		_this.installmentPlans = response.installmentPlans;
+
+
+
+	}).catch(function(err) {
+
+	});
+
+
+
+});
+
+module.exports = State;
