85034b5a2a8c0e43c0075bd26812d2ed08aa149c
张弛 <zhangchi@krspace.cn>
2016-12-29 10:29:49 +0800

    feat合同打印

--- src/Containers/Operation/CustomerManage/Agreement/Admit/Print/index.js
+++ src/Containers/Operation/CustomerManage/Agreement/Admit/Print/index.js
@@ -46,7 +46,10 @@ export default class JoinPrint extends Component {
 	}
 	componentDidMount() {
 		Store.dispatch(Actions.switchSidebarNav(false));
-
+		setTimeout(function() {
+			window.print();
+			window.close();
+		}, 1000)
 	}
 
 	render() {
