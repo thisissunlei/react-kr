42192ba34b7be05e5c614ebb9fb29e7faeba9ecd
张屈 <joneyphair@qq.com>
2016-12-30 19:56:11 +0800

    fix

--- webpack/webpack-production.config.js
+++ webpack/webpack-production.config.js
@@ -56,8 +56,8 @@ const config = {
 		new webpack.optimize.MinChunkSizePlugin({
    			 compress: {
      			 warnings: false,
-    			drop_debugger: true,
-    			drop_console: true
+    			//drop_debugger: true,
+    			//drop_console: true
     		},
     		minChunkSize: 10000
   		}),
