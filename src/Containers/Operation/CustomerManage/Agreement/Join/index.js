09e8a447b26b8a278343fdfa4650125b2c92ddbe
dongfanai <dongfanai@krspace.cn>
2016-12-30 13:57:45 +0800

    fix:删除

--- src/Containers/Operation/CustomerManage/Agreement/Join/index.js
+++ src/Containers/Operation/CustomerManage/Agreement/Join/index.js
@@ -2,12 +2,10 @@ import Create from './Create';
 import Edit from './Edit';
 import Detail from './Detail';
 import Print from './Print';
-import Intention from './Intention';
 
 module.exports = {
 	Detail,
 	Edit,
 	Create,
-	Print,
-	Intention
+	Print
 }
