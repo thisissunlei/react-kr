5175c4450ed6e3f399ea2f504b5b63515070b9e2
dongfanai <dongfanai@krspace.cn>
2017-01-03 18:20:11 +0800

    style:

--- src/PureComponents/Agreement/Print/BaseInfo/index.js
+++ src/PureComponents/Agreement/Print/BaseInfo/index.js
@@ -46,17 +46,37 @@
  			<div className="ui-print-baseinfo">
 				<div className="baseinfo">基础信息</div>
 				<div className="baseinfo-content">
-					<div className="infolist" style={{minWidth:"50%"}}>
-						<div>服务提供方（甲方）：{baseInfo.lessorName}</div>
-						<div>地址：{baseInfo.lessorAddress}</div>
-						<div>联系人：{baseInfo.lessorContact}</div>
-						<div>联系电话：{baseInfo.lessorContacttel}</div>
+					<div className="infolist" >
+						<div >
+							<div className="username long">服务提供方（甲方）：</div>
+							<div className="namelist">
+							{baseInfo.lessorName}
+							</div>
+						</div>
+						<div >
+							<div className="username long">地址：</div>
+							<div className="namelist">
+							{baseInfo.lessorAddress}
+							</div>
+						</div>
+						<div  className="infolist-div ">联系人：{baseInfo.lessorContact}</div>
+						<div  className="infolist-div ">联系电话：{baseInfo.lessorContacttel}</div>
 					</div>
 					<div className="infolist">
-						<div>服务接收方（乙方）：{baseInfo.leaseName}</div>
-						<div>地址：{baseInfo.leaseAddress}</div>
-						<div>联系人：{baseInfo.leaseContact}</div>
-						<div>联系电话：{baseInfo.leaseContacttel}</div>
+						<div>
+							<div className="username long">服务接收方（乙方）：</div>
+							<div className="namelist">
+							{baseInfo.leaseName}
+							</div>
+						</div>
+						<div>
+							<div className="username long">地址：</div>
+								<div className="namelist">
+									{baseInfo.leaseAddress}
+								</div>
+							</div>
+						<div  className="infolist-div">联系人：{baseInfo.leaseContact}</div>
+						<div  className="infolist-div">联系电话：{baseInfo.leaseContacttel}</div>
 					</div>
 				</div>
 			</div>
