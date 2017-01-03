f5061d35275745674c3c423b3723dfaeaf3e99a0
马超越 <machaoyue@36kr.com>
2017-01-03 15:15:55 +0800

    fix:

--- src/Configs/apis.js
+++ src/Configs/apis.js
@@ -507,7 +507,6 @@ const APIS = {
     url:'/api/krspace-finance-web/member/member-list?value={value}&type={type}&startTime={startTime}&endTime={endTime}&registerSourceId={registerSourceId}&jobId={jobId}&pageSize={pageSize}&page={page}&companyId={companyId}&cityId={cityId}',
     method:'get'
   },
-
   //获取指定公司会员列表
   'getCompanyMemberList':{
    url:'/api/krspace-finance-web/member/company-team?companyId={companyId}&page={page}&pageSize={pageSize}',
