42f34b7d45447f0c568ea2349a6b9c6dbb849907
刘毅豪 <liuyihao@krspace.cn>
2017-01-09 17:52:59 +0800

    feature:新建三级联动

--- src/Containers/Operation/CustomerManage/CustomerList/NewCustomerList/index.js
+++ src/Containers/Operation/CustomerManage/CustomerList/NewCustomerList/index.js
@@ -35,15 +35,20 @@ import './index.less'
 
 
 
-	 onSubmit(values){
-		 const {onSubmit} = this.props;
+	onSubmit(values){
+		const {onSubmit} = this.props;
 		onSubmit && onSubmit(values);
-	 }
+	}
 
-	 onCancel(){
-		 const {onCancel} = this.props;
-		 onCancel && onCancel();
-	 }
+	onCancel(){
+		const {onCancel} = this.props;
+		onCancel && onCancel();
+	}
+
+	click = (params) =>{
+		console.log(params.value,"33")
+	}
+
 
 	render(){
 
@@ -69,20 +74,24 @@ import './index.less'
 										<span>个</span>
 									</KrField>
 									<KrField grid={1/2} label="联系人姓名" name="propcode" style={{width:252,marginLeft:15}} component="input" requireLabel={true}/>
-									<KrField grid={1/2} label="意向工位类型" name="proptype" component="select" style={{width:252,marginLeft:15}} options={[
-											 {value:'PAYMENT',label:'回款'},
-										   {value:'INCOME',label:'收入'},]}
-											 requireLabel={true}
+									<KrField grid={1/2} label="意向工位类型" name="proptype" component="select" style={{width:252,marginLeft:15}} 
+											options={[
+												{value:'PAYMENT',label:'回款'},
+											   	{value:'INCOME',label:'收入'},
+										   	]}
+											requireLabel={true}
 									/>
 									<KrField grid={1/2} label="联系人电话" name="propcode" style={{width:252,marginLeft:15}} component="input"  requireLabel={true}/>
 									<KrField grid={1/2} label="意向工位价格" name="propcode" style={{width:252,marginLeft:15}} component="input"  requireLabel={true}>
 										<span>元/个/月</span>
 									</KrField>
 									<KrField grid={1/2} label="联系人邮箱"  name="propcode" style={{width:252,marginLeft:15}} component="input" requireLabel={false}/>
-									<KrField grid={1/2} label="意向入驻社区" name="proptype" component="select" style={{width:252,marginLeft:15}} options={[
-										   {value:'PAYMENT',label:'回款'},
-										   {value:'INCOME',label:'收入'},]}
-											 requireLabel={true}
+									<KrField grid={1/2} label="意向入驻社区" name="proptype" component="select" style={{width:252,marginLeft:15}} 
+											options={[
+										    	{value:'PAYMENT',label:'回款'},
+										    	{value:'INCOME',label:'收入'},
+											]}
+											requireLabel={true}
 									/>
 									<KrField grid={1/2} label="联系人微信" name="propcode" style={{width:252,marginLeft:15}} component="input" requireLabel={false}/>
 									<KrField grid={1/2} label="预计入驻时间" name="receiveDate" style={{width:252,marginLeft:15}} component="date"    requireLabel={true}/>
@@ -91,7 +100,7 @@ import './index.less'
 
 						<div className="titleBar"><span className="order-number">2</span><span className="wire"></span><label className="small-title">公司信息</label></div>
 						<div className="small-cheek">
-								<KrField grid={1/2} label="公司信息" name="propcode" style={{width:252,marginLeft:15}} component="input" requireLabel={false}/>
+								<KrField grid={1/2} label="公司名称" name="propcode" component="input" style={{width:252,marginLeft:15}}  requireLabel={true}/>
 								<KrField grid={1/2} label="投资轮次" name="proptype" component="select" style={{width:252,marginLeft:15}} options={[
 										 {value:'PAYMENT',label:'回款'},
 										 {value:'INCOME',label:'收入'},]}
@@ -100,15 +109,13 @@ import './index.less'
 								<KrField grid={1/2} label="公司规模" name="propcode" style={{width:252,marginLeft:15}} component="input" requireLabel={true}/>
 								<KrField grid={1/2} label="融资金额" name="propcode" style={{width:252,marginLeft:15}} component="input" requireLabel={false}/>
 								<KrField grid={1/2} label="是否已有办公室" name="enableflag" style={{width:252,marginLeft:15}} component="group" requireLabel={true}>
-					              	<KrField name="enableflag1" label="是" type="radio" value="ENABLE" checked={true}/>
-					             	<KrField name="enableflag1" label="否" type="radio" value="DISENABLE" />
+					              	<KrField name="enableflag1" label="是" type="radio" value="ENABLE" onClick={this.click}/>
+					             	<KrField name="enableflag1" label="否" type="radio" value="DISENABLE" onClick={this.click}/>
 					            </KrField>
 								<KrField grid={1/2} label="项目名称" name="propcode" style={{width:252,marginLeft:15}} component="input" requireLabel={true}/>
-								<KrField grid={1/2} label="所属地区" name="proptype" component="select" style={{width:252,marginLeft:15}} options={[
-										 {value:'PAYMENT',label:'回款'},
-										 {value:'INCOME',label:'收入'},]}
-										 requireLabel={true}
-								/>
+
+								<KrField grid={1/2} label="工作地点" name="work"  style={{width:252,marginLeft:15}} component="city" onSubmit={this.city}/>
+
 								<KrField grid={1/2} label="项目类型" name="proptype" component="select" style={{width:252,marginLeft:15}} options={[
 										 {value:'PAYMENT',label:'回款'},
 										 {value:'INCOME',label:'收入'},]}
