47dca1367d0e022038dde7ab30dae62c392b3da7
刘毅豪 <liuyihao@krspace.cn>
2017-02-20 11:48:52 +0800

    feature:新建页目录搭建

---
+++ src/PureComponents/Agreement/Exit/index.js
@@ -0,0 +1,9 @@
+import Create from './Create';
+import Edit from './Edit';
+import Detail from './Detail';
+
+module.exports = {
+	Detail,
+	Edit,
+	Create,
+}
--- src/PureComponents/Agreement/index.js
+++ src/PureComponents/Agreement/index.js
@@ -1,7 +1,11 @@
 import { default as JoinDetail } from './JoinDetail';
 import { default as Print } from './Print';
+import { default as Admit } from "./Admit";
+import { default as Exit } from "./Exit";
 
 module.exports = {
 	JoinDetail,
-	Print
+	Print,
+	Admit,
+	Exit
 }
