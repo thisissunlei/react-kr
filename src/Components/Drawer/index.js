04f64308c9e00062b13f9885bf4637d74df04641
张屈 <joneyphair@qq.com>
2017-02-23 15:23:07 +0800

    fix

--- src/Components/Drawer/index.js
+++ src/Components/Drawer/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Drawer from 'material-ui/Drawer';
+
+
+import DrawerBody from './DrawerBody';
+import DrawerModal  from './DrawerModal';
+
+import './index.less';
 
 export default class DrawerSimpleExample extends React.Component {
 
+
+  static defaultProps = {
+    width:1000,
+  }
   static propTypes = {
-    open:React.PropTypes.bool
+    open:React.PropTypes.bool,
+    width:React.PropTypes.number,
+    contentStyle:React.PropTypes.object,
   }
 
   constructor(props) {
@@ -12,15 +23,25 @@ export default class DrawerSimpleExample extends React.Component {
   }
 
   render() {
-    let {children,open} = this.props;
-    let content="";
+
+    let {children,open,contentStyle,width} = this.props;
+
     if(!open){
-     return null;
+      return null;
     }
+
+
+    contentStyle = Object.assign({},contentStyle);
+
+    if(width){
+      contentStyle = Object.assign({},contentStyle,{width:width});
+    }
+
     return (
-        <Drawer {...this.props}>
-          {children}
-        </Drawer>
+      <div className="ui-drawer">
+        <DrawerModal />
+        <DrawerBody style={contentStyle}> {children} </DrawerBody>
+      </div>
     );
   }
 }
