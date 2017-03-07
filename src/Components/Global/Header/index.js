72aff1f64efb577c7aa3b465ca813837ff6dd0b8
刘毅豪 <liuyihao@krspace.cn>
2017-03-07 15:35:29 +0800

    fix:header ie9 三点按钮

--- src/Components/Global/Header/index.js
+++ src/Components/Global/Header/index.js
@@ -255,7 +255,7 @@ class Header extends Component {
 				}
 
 				iconElementRight = {
-					<div style={{minWidth:70,textAlign:'right'}}>
+					<div style={{minWidth:70,textAlign:'right',float:"right"}}>
 					<div style={{display:showInfoLogo,position:'relative',marginRight:10,cursor: 'pointer'}} onClick={this.showInfo}>
 						<span className="icon-info information-logo"  ></span>
 						<span className="ui-un-read-count" style={{visibility:hasUnRead>0?'visible':'hidden'}}>{hasUnRead}</span>
