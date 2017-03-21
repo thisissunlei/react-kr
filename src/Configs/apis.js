09222fae811757a3687974eedb69de076b2770dd
张弛 <zhangchi@krspace.cn>
2017-03-21 10:45:52 +0800

    0321

--- src/Configs/apis.js
+++ src/Configs/apis.js
@@ -1116,6 +1116,16 @@ const APIS = {
         url: '/api/krspace-finance-web/finaVerify/opt/edit-verify-status',
         method: 'post'
     },
+    //财务--审核--批量审核
+    'batch-edit-verify-status': {
+        url: '/api/krspace-finance-web/finaVerify/opt/batch-edit-verify-status',
+        method: 'post'
+    },
+    //财务--审核--导出
+    'auditExport': {
+        url: '/api/krspace-finance-web/finaVerify/data/export-excel?idList={idList}',
+        method: 'get'
+    },
 }
 
 module.exports = APIS;
