784281a6936102d19bf4ae6ebb08fcf6beaf7f82
张屈 <joneyphair@qq.com>
2017-03-22 17:38:13 +0800

    fix

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -2,10 +2,12 @@ import * as DateFormat from './DateFormat';
 import * as ClassNames from './ClassNames';
 import * as ShallowEqual from './ShallowEqual';
 import * as IsPhoneNumber from './IsPhoneNumber';
+import * as Http from './Http';
 
 module.exports = {
 	...DateFormat,
 	...ClassNames,
 	...ShallowEqual,
 	...IsPhoneNumber,
+	...Http
 }
