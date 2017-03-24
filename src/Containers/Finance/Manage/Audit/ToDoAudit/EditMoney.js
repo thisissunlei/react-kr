7c2cbf485f7cac313facb4389fb989a2b520dec1
dongfanai <dongfanai@krspace.cn>
2017-03-24 17:16:56 +0800

    fix:校验数子正则

--- src/Containers/Finance/Manage/Audit/ToDoAudit/EditMoney.js
+++ src/Containers/Finance/Manage/Audit/ToDoAudit/EditMoney.js
@@ -207,7 +207,7 @@ class EditMoney extends Component {
 			Message.error('金额不能大于未回款额');
 			return
 		}
-		if (/[^0-9]+/.test(value)) {
+		if (/[^0-9]+.[^0-9]+/.test(value)) {
 			Message.error('金额只能为数字');
 			return;
 		}
