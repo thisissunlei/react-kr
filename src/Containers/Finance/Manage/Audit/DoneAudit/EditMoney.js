6e44885cbd82dabf458758efbf2ab42183a9e574
dongfanai <dongfanai@krspace.cn>
2017-03-26 14:49:14 +0800

    fix：编辑上传附件

--- src/Containers/Finance/Manage/Audit/DoneAudit/EditMoney.js
+++ src/Containers/Finance/Manage/Audit/DoneAudit/EditMoney.js
@@ -81,7 +81,7 @@ class EditMoney extends Component {
 	componentDidMount() {
 		var _this = this;
 		setTimeout(function() {
-			_this.getDetailInfo();
+			_this.getPayInfo();
 		}, 0)
 	}
 
@@ -768,20 +768,12 @@ class EditMoney extends Component {
 								maxSize={100}
 						/>
 						<KrField  
-							 	name="contractFileList" 
-							 	component="input" 
-							 	type="hidden" 
-							 	label="合同附件"
-						/>
-						<KrField  
 							style={{width:548}}  
 							name="uploadFileIds" 
 							component="file" 
 							label="上传附件" 
-							defaultValue={[]} 
-							onChange={(files)=>{
-								Store.dispatch(change('AddMoney','contractFileList',files));
-							}} 
+							defaultValue={infoList.uploadFileIds} 
+							
 						/>
 					</CircleStyleTwo>
 					<CircleStyleTwo num="2" info="付款明细" circle="bottom">
