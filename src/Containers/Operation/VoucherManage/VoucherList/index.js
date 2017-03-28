85c6d97f29630d7b1476968108b19a15b0c0825b
张弛 <zhangchi@krspace.cn>
2017-03-28 13:47:31 +0800

    audit

--- src/Containers/Operation/VoucherManage/VoucherList/index.js
+++ src/Containers/Operation/VoucherManage/VoucherList/index.js
@@ -1,67 +1,150 @@
 import React, {
-	Component
+  Component
 } from 'react';
 import {
-	connect
+  connect
 } from 'react-redux';
 import {
-	bindActionCreators
+  bindActionCreators
 } from 'redux';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-
-import * as actionCreators from 'kr-ui/../Redux/Actions';
-
 import {
-	Actions,
-	Store
+  Actions,
+  Store
 } from 'kr/Redux';
 
+
 import {
-	Table,
-	TableBody,
-	TableHeader,
-	TableHeaderColumn,
-	TableRow,
-	TableRowColumn,
-	TableFooter,
-	Button,
-	Section,
-	Grid,
-	Row,
-	Col,
-	Dialog,
-	Notify,
-	BreadCrumbs,
-	ListGroup,
-	Title,
+  Form,
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+  TableFooter,
+  Button,
+  Section,
+  Grid,
+  Row,
+  Col,
+  Dialog,
+  Tabs,
+  Tab,
+  KrField,
+  Title
 } from 'kr-ui';
 
-import './index.less';
-export default class VoucherList extends Component {
+// import ToDoAudit from './ToDoAudit';
+// import DoAudit from './DoAudit';
+// import DoneAudit from './DoneAudit';
 
-	constructor(props, context) {
-		super(props, context);
+export default class AuditList extends Component {
 
-		this.state = {
-			itemDetail: {},
-		}
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      tab: 'table',
+      countList: " "
+    }
+  }
 
-	}
+  componentDidMount() {}
 
-	componentDidMount() {
+  merchants = () => {
+    let {
+      tab,
+      initSearch
+    } = this.state;
+    tab = 'merchants';
+    this.setState({
+      tab,
+      initSearch: 'm'
+    });
+  }
 
-	}
+  personal = () => {
+    let {
+      tab,
+      initSearch
+    } = this.state;
+    tab = 'personal';
+    this.setState({
+      tab,
+      initSearch: 'p'
+    });
+  }
+  signedClient = () => {
+    let {
+      tab,
+      initSearch
+    } = this.state;
+    tab = 'signedClient';
+
+    this.setState({
+      tab,
+      initSearch: 's'
+    });
+  }
 
 
-	render() {
-		return (
+  render() {
+    let {
+      tab,
+      initSearch,
+      countList
+    } = this.state;
+    const activeTab = {
+      color: '#2b8dcd',
+      borderBottom: "1px solid #eee",
+      fontSize: '16px'
+    }
+    const commenTab = {
+      color: '#666',
+      borderBottom: "1px solid #eee",
+      fontSize: '16px'
+    }
 
-			<div>
-				asdfdsaf
-			</div>
 
-		);
+    let merchantsStyle = (tab == 'merchants' || tab == 'table') ? activeTab : commenTab;
+    let personalStyle = (tab == 'personal') ? activeTab : commenTab;
+    let signedClientStyle = (tab == 'signedClient') ? activeTab : commenTab;
 
-	}
+  //   if (countList != " ") {
+  //     return (
+	//
+  //       <div>
+  //         <Title value="审核列表"/>
+  //         <Tabs className="tabs">
+  //           <Tab label="待处理凭证" onActive={this.merchants} style={merchantsStyle}>
+  //               <ToDoAudit
+  //                     tab={initSearch}
+  //               />
+  //           </Tab>
+  //           <Tab label="已处理凭证" onActive={this.personal} style={personalStyle}>
+  //              <DoAudit
+  //                      tab={initSearch}
+  //               />
+  //           </Tab>
+	//
+  //           <Tab label="已删除凭证" onActive={this.signedClient} style={signedClientStyle}>
+  //               <DoneAudit
+  //                      tab={initSearch}
+  //               />
+  //           </Tab>
+  //       </Tabs>
+  //     </div>
+	//
+  //     );
+  //   } else {
+  //     return (
+  //       <div></div>
+  //     )
+  //   }
+
+	return(
+		<div></div>
+	)
+  }
 
 }
