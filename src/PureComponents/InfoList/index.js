6080dba3fe904cc0c7352d407ec4c0bd05e0cb0c
刘毅豪 <liuyihao@krspace.cn>
2017-04-06 20:16:04 +0800

    fix:文字居左

--- src/PureComponents/InfoList/index.js
+++ src/PureComponents/InfoList/index.js
@@ -263,7 +263,7 @@ export default class AppointmentVisit extends Component {
 												color="#333333";
 											}
 											return (
-												<div className="appointment-visit-time" style={{color:color,top:9}} onClick={this.columnClick.bind(this,itemData)}> <KrDate value={value} format="yyyy-mm-dd HH:MM:ss"/></div>
+												<div className="appointment-visit-time-info" style={{color:color,top:9}} onClick={this.columnClick.bind(this,itemData)}> <KrDate value={value} format="yyyy-mm-dd HH:MM:ss"/></div>
 											);
 										}
 									}
