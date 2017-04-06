6a5ea37b3c37b0903ccb368c722a4e83f6d3a1ec
张屈 <joneyphair@qq.com>
2017-04-06 11:46:13 +0800

    fix

--- webpack/webpack-test.config.js
+++ webpack/webpack-test.config.js
@@ -1,11 +1,10 @@
 const webpack = require('webpack');
 const path = require('path');
-const buildPath = path.join(process.cwd(), 'dist');
+const buildPath = path.join(process.cwd(), '/dist');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const node_modules_dir = path.join(process.cwd(),'node_modules');
 const HappyPack = require('happypack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var env = process.env.NODE_ENV || 'production';
 
@@ -86,10 +85,7 @@ const config = {
 			showErrors:true,
 			chunksSortMode:'none'
 		}),
-		new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, 'node-noop'),
-		new CopyWebpackPlugin([
-			{from:path.join(process.cwd(),'public','vendors'),to:path.join(process.cwd(),'dist','vendors')}
-		])
+		new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, 'node-noop')
 	],
 	module: {
 		exprContextRegExp: /$^/,
