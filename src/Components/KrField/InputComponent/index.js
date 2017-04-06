379567681dfba6984ee3ebb616594ddc2854712d
张屈 <joneyphair@qq.com>
2017-04-06 17:09:20 +0800

    fix:优化input 组件 krfield

--- src/Components/KrField/InputComponent/index.js
+++ src/Components/KrField/InputComponent/index.js
@@ -9,17 +9,11 @@ import './index.less';
 
 export default class InputComponent extends React.Component{
 
-
-
-	static contextTypes =  {
-	    _reduxForm: React.PropTypes.object.isRequired,
-	}
 	static propTypes = {
 		inline:React.PropTypes.bool,
 		simple:React.PropTypes.bool,
 		heightStyle:React.PropTypes.object,
 		maxLength:React.PropTypes.number,
-		//自动获取焦点
 		autoFocus:React.PropTypes.bool,
 	}
 
@@ -27,28 +21,21 @@ export default class InputComponent extends React.Component{
 		super(props,context)
 	}
 
-	componentDidMount(){
-	}
-
 	onChange = (value)=>{
-
 
 		let {input} = this.props;
 		input.onChange(value);
 		const {onChange} = this.props;
-
 		onChange && onChange(value,input)
-
-
-
 	}
+
 	onBlur=(value)=>{
-		// console.log("####",value)
 		let {input} = this.props;
 		input.onBlur(value);
 		const {onBlur} = this.props;
 		onBlur && onBlur(value)
 	}
+
 	onFocus=(value)=>{
 		let {input} = this.props;
 		input.onFocus(value);
@@ -56,18 +43,6 @@ export default class InputComponent extends React.Component{
 		onFocus && onFocus(value)
 	}
 
-	onError = (message)=>{
-		let {meta,input} = this.props;
-		const {onError} = this.props;
-		const {_reduxForm} = this.context;
-		let errors = {};
-		errors[input.name] = message;
-		meta.dispatch(stopAsyncValidation(_reduxForm.form,errors));
-
-		_reduxForm.blur();
-
-		onError && onError();
-	}
 
 	render(){
 
@@ -98,7 +73,7 @@ export default class InputComponent extends React.Component{
 
 			return (
 				<WrapComponent {...wrapProps}>
-					<Input {...input} placeholder={placeholder|| label} type={type} disabled={disabled} className={className} style={heightStyle} onChange={this.onChange} onBlur={this.onBlur} onFocus={this.onFocus} {...other} onError={this.onError} autoFocus={autoFocus}/>
+					<Input {...input} placeholder={placeholder|| label} type={type} disabled={disabled} className={className} style={heightStyle} onChange={this.onChange} onBlur={this.onBlur} onFocus={this.onFocus} {...other} autoFocus={autoFocus}/>
 					{touched && error && <div className="error-wrap"> <span>{error}</span> </div> }
 				</WrapComponent>
 		);
