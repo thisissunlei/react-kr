641c6dad84e8c99757d17acf646267679ac30f50
张屈 <joneyphair@qq.com>
2017-04-11 09:24:54 +0800

    fix

--- src/Configs/Apis/AgreementApi/index.js
+++ src/Configs/Apis/AgreementApi/index.js
@@ -25,4 +25,69 @@ module.exports = {
           url: '/api/krspace-finance-web/checkinagreement/checkin-agreement/actions/show?id={id}',
           method: 'get'
       },
+      //增租协议-创建-编辑
+      'addOrEditIncreaseContract': {
+          url: '/api/krspace-finance-web/checkinagreement/addOrEditIncreaseContract',
+          method: 'post'
+      },
+
+      //入驻协议-新增-编辑
+      'addOrEditEnterContract': {
+          url: '/api/krspace-finance-web/checkinagreement/addOrEditEnterContract',
+          method: 'post'
+      },
+      //创建合同时初始化数据
+      'fina-contract-intention': {
+          url: '/api/krspace-finance-web/finacontractdetail/fina-contract-intention?customerId={customerId}&mainBillId={mainBillId}&type={type}',
+          method: 'get'
+      },
+      //合同－退租合同－查看
+      'getFnaContractWithdrawalById': {
+          url: '/api/krspace-finance-web/fnaContractWithdrawalController/getFnaContractWithdrawalById?id={id}',
+          method: 'get'
+      },
+
+      //合同－承租合同－查看
+      'show-fina-contract-intentletter': {
+          url: '/api/3/krspace-finance-web/finacontractdetail/fina-contract-intentletter/acitions/show',
+          method: 'get'
+      },
+
+      //合同－承租合同－查看
+      'showFinaContractIntentletter': {
+          url: '/api/krspace-finance-web/finacontractdetail/fina-contract-intentletter/acitions/show?id={id}',
+          method: 'get'
+      },
+
+      //合同－减租合同－查看
+      'showFnaContractRentController': {
+          url: '/api/krspace-finance-web/fnaContractRentController/getFnaContractRentById?id={id}',
+          method: 'get'
+      },
+      //合同－减租合同－新建或编辑
+      'getFnaContractRentController': {
+          url: '/api/krspace-finance-web/fnaContractRentController/saveFnaContractRent',
+          method: 'post'
+      },
+      //合同－(入驻合同、增租、续租)－查看
+      'show-checkin-agreement': {
+          url: '/api/krspace-finance-web/checkinagreement/checkin-agreement/actions/show?id={id}',
+          method: 'get'
+      },
+
+      //合同－承租合同－新建
+      'addFinaContractIntentletter': {
+          url: '/api/krspace-finance-web/finacontractdetail/fina-contract-intentletter/actions/save',
+          method: 'post'
+      },
+      //合同－承租合同－编辑
+      'updateFinaContractIntentletter': {
+          url: '/api/krspace-finance-web/finacontractdetail/fina-contract-intentletter/actions/update',
+          method: 'put'
+      },
+      //合同－创建基础数据
+      'finaContractIntention': {
+          url: '/mockjsdata/3/krspace-finance-web/finacontractdetail/fina-contract-intention?communityId={communityId}&customerId={customerId}',
+          method: 'get'
+      },
 }
