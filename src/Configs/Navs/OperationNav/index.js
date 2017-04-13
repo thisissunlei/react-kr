d71a429731e56d69e95849431cf078633c7fce7f
张屈 <joneyphair@qq.com>
2017-04-13 15:44:16 +0800

    fix:社区凭证管理

--- src/Configs/Navs/OperationNav/index.js
+++ src/Configs/Navs/OperationNav/index.js
@@ -29,7 +29,7 @@ module.exports = [
 						menuCode: 'marketList',
 					},
 				]
-			}, 
+			},
 			{
 				primaryText: "社区配置",
 				iconName: 'icon-community',
@@ -44,28 +44,28 @@ module.exports = [
 						primaryText: '社区列表',
 						menuCode: 'communityBaseList',
 						router: '/operation/communityAllocation/communityList'
-					}, 
+					},
 					{
 						primaryText: '会议室列表',
 						menuCode: 'communityBoardroomList',
 						originUrl: '/krspace_operate_web/commnuity/communityBase/toCityCommunityList?jumpType=toCommunityBoardroom&mid=66'
-					}, 
+					},
 					{
 						primaryText: '设备列表',
 						menuCode: 'communityDeviceList',
 						originUrl: '/krspace_operate_web/community/device/toDeviceList?mid=75'
 
-					}, 
+					},
 					{
 						primaryText: '工位列表',
 						menuCode: 'communityStationList',
 						originUrl: '/krspace_operate_web/commnuity/communityBase/toCityCommunityList?jumpType=toCommunityStation&mid=83'
-					}, 
+					},
 					{
 						primaryText: '平面图配置',
 						menuCode: 'communityFloorPlanList',
 						originUrl: '/krspace_operate_web/commnuity/communityBase/toCityCommunityList?jumpType=toCommunityFloorPlan&mid=90'
-					}, 
+					},
 					{
 						primaryText: '访客记录',
 						menuCode: 'sysVisitRecordList',
@@ -74,7 +74,8 @@ module.exports = [
 
 
 				]
-			}, 
+			},
+
 			{
 				primaryText: "基础配置",
 				iconName: 'icon-basis',
@@ -86,28 +87,28 @@ module.exports = [
 						primaryText: "参数配置",
 						menuCode: 'operate_sysparamadmin',
 						originUrl: '/krspace_operate_web/sys/sysParam/toSysParamList?mid=60',
-					}, 
+					},
 					{
 						primaryText: "合同配置",
 						menuCode: 'agreement_setting',
 						router: '/operation/customerManage/agreement/setting/list',
-					}, 
+					},
 					{
 						primaryText: '代码分类',
 						menuCode: 'codeCategoryList',
 						originUrl: '/krspace_operate_web/codecategory/toCodeCategoryList?mid=85'
-					}, 
+					},
 
 					{
 						primaryText: '商圈列表',
 						menuCode: 'businessAreaList',
 						originUrl: '/krspace_operate_web/businessArea/toBusinessAreaList?mid=87'
-					}, 
+					},
 					{
 						primaryText: "出租方管理",
 						menuCode: 'lessorManage',
 						router: '/operation/customerManage/agreement/lessorManage/list',
-					}, 
+					},
 					{
 						primaryText: '设备定义',
 						menuCode: 'sysDeviceDefinitionList',
@@ -122,7 +123,19 @@ module.exports = [
 						router: '/operation/groupSetting'
 					}
 				]
-			}, 
+			},
+			{
+				primaryText: "凭证管理",
+				iconName: 'icon-wendang',
+				iconColor: '#79859a',
+				router: 'voucherManage',
+				menuCode: 'communityStationList',
+				menuItems: [{
+					primaryText: '凭证列表',
+					router: '/operation/voucherManage/voucherList',
+					menuCode: 'coustomerInfoList',
+				}]
+			},
 		]
 	}
 
