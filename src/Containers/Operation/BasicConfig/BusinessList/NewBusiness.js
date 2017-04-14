541db949006efd44b30bec7385148648bb3d5a3c
刘毅豪 <liuyihao@krspace.cn>
2017-04-14 10:47:24 +0800

    feature/新建表单编写

--- src/Containers/Operation/BasicConfig/BusinessList/NewBusiness.js
+++ src/Containers/Operation/BasicConfig/BusinessList/NewBusiness.js
@@ -72,11 +72,13 @@ import './index.less';
 
 
 						
-						<KrField grid={1/2}  name="companyId" style={{width:262,marginLeft:28}} component='input'  label="商圈代码" inline={false}  placeholder='请输入代码名称' requireLabel={true}/>
-						<KrField grid={1/2}  name="companyId" style={{width:262,marginLeft:28}} component='companyName'  label="区县" inline={false}  placeholder='请选择区县' requireLabel={true}/>
-						<KrField grid={1/2}  name="companyId" style={{width:262,marginLeft:28}} component='input'  label="商圈名称" inline={false}  placeholder='请输入商圈名称' requireLabel={true}/>
+						<KrField grid={1/2}  name="no" style={{width:262,marginLeft:28}} component='input'  label="商圈代码" inline={false}  placeholder='请输入代码名称' requireLabel={true}/>
+						<KrField grid={1/2}  name="name" style={{width:262,marginLeft:28}} component='input'  label="商圈名称" inline={false}  placeholder='请输入商圈名称' requireLabel={true}/>
+
+						<KrField grid={1/2} label="区县" name="distinctId"  style={{width:262,marginLeft:28}} component="city"  requireLabel={false} />
+
 						<KrField grid={1/2}  name="companyId" style={{width:262,marginLeft:28}} component='input'  label="客户名称" inline={false}  placeholder='请输入客户名称' requireLabel={true}/>
-						<KrField grid={1/2}  name="enable" style={{width:262,marginLeft:28}} component="group" label="启用状态" requireLabel={true}>
+						<KrField grid={1/2}  name="enable" style={{width:262,marginLeft:28}} component="group" label="启用状态" requireLabel={false}>
 							 <KrField name="enable" label="是" type="radio" value="ENABLE" checked={true}/>
 							 <KrField name="enable" label="否" type="radio" value="DISABLE" />
 						</KrField>
