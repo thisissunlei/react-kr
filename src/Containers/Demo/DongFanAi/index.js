85c7ca7c6e7c1bf5d32f953d0f0da402ec06b997
dongfanai <dongfanai@krspace.cn>
2017-04-17 16:24:06 +0800

    feature:demo liuyihao

--- src/Containers/Demo/DongFanAi/index.js
+++ src/Containers/Demo/DongFanAi/index.js
@@ -1,52 +1,21 @@
 import React from 'react';
-
-import {
-	PlanMap,
-	Dialog,
-} from 'kr-ui';
+import  './index.less';
 
 export default class DongFanAi extends React.Component {
 
 	constructor(props, context) {
 		super(props, context);
 
-		this.state = {
-			open:true,
-			checkedStations:[],
-		}
+		
 
 	}
 
-	close = ()=>{
-		this.setState({
-			open:!this.state.open
-		})
-	}
 
-	confirm = ()=>{
-		this.close();
-	}
 
-	onCheckedStation =(clickStation,checkedStations)=>{
-		this.setState({
-			checkedStations
-		});
-	}
-
-	componentDidMount() {}
 
 	render() {
 		return (
 			<div>
-					<Dialog
-						title="平面图"
-						contentStyle={{width:1000}}
-						actions={<Button label="确定" onTouchTap={this.confirm}/>}
-						onClose={this.close}
-						bodyStyle={{paddingLeft:0,paddingRight:0}}
-						open={this.state.open} >
-								<PlanMap onCheckedStation={this.onCheckedStation} />
-				</Dialog>
 			</div>
 
 		);
