5260d592b67d6fa3daff74773428d66e70f68f5d
dongfanai <dongfanai@krspace.cn>
2017-04-17 17:58:51 +0800

    feat:demo wushulin

--- src/Containers/Demo/WSL/index.js
+++ src/Containers/Demo/WSL/index.js
@@ -1,43 +1,24 @@
-import React,{Component} from 'react';
-import { connect } from 'react-redux';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-import {Actions,Store} from 'kr/Redux';
-import {
-	KrField,
-	Table,
-	TableBody,
-	TableHeader,
-	TableHeaderColumn,
-	TableRow,
-	TableRowColumn,
-	TableFooter,
-	Button,
-	Section,
-	Grid,
-	Row,
-	Col,
-	Paper,
-	Dialog,
-} from 'kr-ui';
+import React from 'react';
+import {KrField} from 'kr-ui';
 import './index.less';
 
-export default class Initialize  extends Component{
+export default class WSL  extends React.Component{
 
 	constructor(props,context){
 		super(props, context);
-		
+
 	}
-     
-     
+
+
 	render(){
-        
+
 		return(
              <div>
-			       <KrField name="uploadImageList" 
-								component="uploadImageList" 
-								style={{marginTop:10}} 
-								photoSize={'212*136'} 
-								pictureFormat={'JPG'} 
+			       <KrField name="uploadImageList"
+								component="uploadImageList"
+								style={{marginTop:10}}
+								photoSize={'212*136'}
+								pictureFormat={'JPG'}
 								pictureMemory={'32K'}
 								//requestURI = {this.state.requestURI}
 					/>
