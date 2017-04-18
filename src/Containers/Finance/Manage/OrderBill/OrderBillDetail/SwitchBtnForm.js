622d5f421e076c83782128d618d96aca38bdbd60
dongfanai <dongfanai@krspace.cn>
2017-04-18 14:52:11 +0800

    finance

--- src/Containers/Finance/Manage/OrderBill/OrderBillDetail/SwitchBtnForm.js
+++ src/Containers/Finance/Manage/OrderBill/OrderBillDetail/SwitchBtnForm.js
@@ -1,35 +1,16 @@
-import React,{Component} from 'react';
-import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React from 'react';
 import {Actions,Store} from 'kr/Redux';
-import * as actionCreators from 'kr-ui/../Redux/Actions';
-import {reduxForm,formValueSelector,initialize} from 'redux-form';
+import {reduxForm,initialize} from 'redux-form';
 import {
-	Table,
- 	TableBody,
-	TableHeader,
-	TableHeaderColumn, 
-	TableRow,
-	TableRowColumn,
-	TableFooter,
 	Button,
-	Section,
 	Grid,
 	Row,
 	Col,
-	Notify,
-	List,
- 	ListItem,
-	LabelText,
-	Dialog,
 	KrField,
 	ButtonGroup
 } from 'kr-ui';
 
-
-
-
-class SwitchBtnForm extends Component{
+class SwitchBtnForm extends React.Component{
 
 	static PropTypes = {
 		onSubmit:React.PropTypes.func,
@@ -44,7 +25,7 @@ class SwitchBtnForm extends Component{
         this.onCancel = this.onCancel.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
 		  this.state = {
-			
+
 	     }
    }
 
@@ -54,10 +35,10 @@ class SwitchBtnForm extends Component{
        	 contractcodeId:'',
        }
 	   Store.dispatch(initialize('SwitchBtnForm',initialValues));
-		
+
 	}
-    
-   
+
+
     onSubmit(values){
 		 const {onSubmit} = this.props;
 		 onSubmit && onSubmit(values);
@@ -65,17 +46,17 @@ class SwitchBtnForm extends Component{
 	 }
 
 	 onCancel(){
-		 const {onCancel} = this.props;	
-		 onCancel && onCancel();		 
+		 const {onCancel} = this.props;
+		 onCancel && onCancel();
 	 }
-	
-    
-	  
 
-    
+
+
+
+
 
 	render(){
-      
+
      let heightStyle={
        	 width:'546',
        	 height:'72',
@@ -86,33 +67,33 @@ class SwitchBtnForm extends Component{
        }
 
         const { error, handleSubmit, pristine, reset,optionList,fiMoney} = this.props;
-		
-   
-        
- 
+
+
+
+
 		return(
 
-			    <div className='ui-switch-wrap'>                 
-					      <form onSubmit={handleSubmit(this.onSubmit)}>                           
+			    <div className='ui-switch-wrap'>
+					      <form onSubmit={handleSubmit(this.onSubmit)}>
 						    <KrField  name="id" type="hidden"/>
-						    <KrField grid={1/2} label="可操作金额" right={29}  component="labelText" value={fiMoney} defaultValue="0" inline={false} requireLabel={true}/>        
+						    <KrField grid={1/2} label="可操作金额" right={29}  component="labelText" value={fiMoney} defaultValue="0" inline={false} requireLabel={true}/>
                             <KrField grid={1/2} label="上传附件" style={{marginLeft:-5}} name="fileids" component="file" style={{marginTop:-1}} defaultValue={[]}/>
                              <KrField grid={1/2} label="合同编号" right={43} name="contractcodeId" type="select" options={optionList} requireLabel={true} style={{marginTop:-12}}/>
                             <KrField label="备注" style={style} name="finaflowdesc" heightStyle={heightStyle} component="textarea" type="text" placeholder='请输入备注,文字不能超过100字' maxSize={100} lengthClass='ui-length-textarea'/>
-                            
+
 
 						   <Grid style={{marginBottom:5,marginLeft:-30}}>
 						<Row>
 							<Col md={12} align="center">
 								<ButtonGroup>
-									<div  className='ui-btn-center'><Button  label="确定" type="submit" joinEditForm /></div> 
+									<div  className='ui-btn-center'><Button  label="确定" type="submit" joinEditForm /></div>
 									<Button  label="取消" type="button"  cancle={true} onTouchTap={this.onCancel} />
 								</ButtonGroup>
 							</Col>
 						</Row>
 					</Grid>
                </form>
-			</div>		
+			</div>
 
 		);
 
@@ -127,13 +108,9 @@ const validate = values =>{
 		if(!values.contractcodeId){
 			errors.contractcodeId = '请填写合同编号';
 		}
-	
+
 		return errors
 	}
 
 
 export default reduxForm({form:'SwitchBtnForm',validate,enableReinitialize:true,keepDirtyOnReinitialize:true})(SwitchBtnForm);
-
-
-
-
