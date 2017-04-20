ddba681f29427ccbb55b117061f003439e28d917
谭琳琳 <805148199@qq.com>
2017-04-20 19:22:40 +0800

    修改OA

--- src/Configs/Router/OARouter/index.js
+++ src/Configs/Router/OARouter/index.js
@@ -13,13 +13,18 @@ import {
 	Store
 } from 'kr/Redux';
 
-import OA from 'kr/Containers/OA';
 import Basic from 'kr/Containers/Basic';
+
+const OA_Home = (location, callback) => {
+  require.ensure([], require => {
+    callback(null, require('kr/Containers/OA/Home').default)
+  }, 'OA_Home')
+}
 
 module.exports =()=>{
 	return (
         <Route path="oa" component={Basic}>
-            <Route path="index" component={OA.Home}/>
+            <Route path="index" getComponent={OA_Home}/>
         </Route>
 	);
 };
