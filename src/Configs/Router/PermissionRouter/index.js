03c5e3c07935e62afb070f198e7983e883942ed1
谭琳琳 <805148199@qq.com>
2017-04-21 11:31:21 +0800

    修改permmision的路由router

--- src/Configs/Router/PermissionRouter/index.js
+++ src/Configs/Router/PermissionRouter/index.js
@@ -15,6 +15,12 @@ import {
 
 import Basic from 'kr/Containers/Basic';
 
+const Basic = (location, callback) => {
+  require.ensure([], require => {
+    callback(null, require('kr/Containers/Basic').default)
+  }, 'Basic')
+}
+
 const Permission_PersonalCenter = (location, callback) => {
   require.ensure([], require => {
     callback(null, require('kr/Containers/Permission/PersonalCenter').default)
@@ -24,7 +30,7 @@ const Permission_PersonalCenter = (location, callback) => {
 module.exports =()=>{
 
 	return (
-		<Route path="permission" component={Basic}>
+		<Route path="permission" getComponent={Basic}>
 			{/*
 	<Route path="index" component={Permission.Home}/>
 	<Route path="notify" component={Permission.Notify}/>
