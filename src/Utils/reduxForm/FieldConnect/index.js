0874b7a660715d40ba12cfa896557db24a3d733a
张屈 <joneyphair@qq.com>
2017-04-21 14:15:26 +0800

    fix:radio mobxform

---
+++ src/Utils/reduxForm/FieldConnect/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+export default class FieldConnect extends React.Component{
+
+  constructor(props,context){
+    super(props,context);
+
+  }
+
+  render(){
+
+  }
+
+}
