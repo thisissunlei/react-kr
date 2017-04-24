d2a3e293c55ebac43b0d00bc063cb8d366f0b369
张屈 <joneyphair@qq.com>
2017-04-24 13:55:27 +0800

    fix:删除无用嗲吗

--- src/Containers/Welcome/index.js
+++ src/Containers/Welcome/index.js
@@ -1,18 +1,11 @@
-
 import React from 'react';
-import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as actionCreators from 'kr-ui/../Redux/Actions';
+
 import './index.less';
 
-class Help extends React.Component{
+export default class Help extends React.Component{
 
 	constructor(props,context){
 		super(props, context);
-	}
-
-	componentDidMount() {
-
 	}
 
 	render(){
@@ -29,21 +22,3 @@ class Help extends React.Component{
 	}
 
 }
-
-
-
-
-function mapStateToProps(state){
-	return {
-		notify:state.notify,
-		items:state.notify.items
-	};
-}
-
-function mapDispatchToProps(dispatch){
-	return {
-		actions:bindActionCreators(Object.assign({},actionCreators),dispatch)
-	};
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(Help);
