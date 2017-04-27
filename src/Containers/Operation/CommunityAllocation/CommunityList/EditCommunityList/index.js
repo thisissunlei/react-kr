538a062d0f10bbd9d6ac1b0010c047157f679219
wusuling <wusuling@krspace.cn>
2017-04-27 16:56:18 +0800

    feat:社区编辑获取信息大改动放到父级文件里面

--- src/Containers/Operation/CommunityAllocation/CommunityList/EditCommunityList/index.js
+++ src/Containers/Operation/CommunityAllocation/CommunityList/EditCommunityList/index.js
@@ -255,7 +255,6 @@ const renderStation = ({ fields, meta: { touched, error }}) => {
             photoD:[],
             communityId:'',
 		}
-
 	}
 
 	onSubmit = (values) => {
@@ -378,107 +377,8 @@ const renderStation = ({ fields, meta: { touched, error }}) => {
       Store.dispatch(change('editCommunityList','longitude',yLocation));
     }
 
-    componentWillMount(){
-        let {id}=this.props;
-        var _this=this;
-				Http.request('communityGetEdit',{id:id}).then(function(response) {
 
-          response.openDate=DateFormat(response.openDate,"yyyy-mm-dd hh:MM:ss");
-          response.signStartDate=DateFormat(response.signStartDate,"yyyy-mm-dd hh:MM:ss");
-          response.signEndDate=DateFormat(response.signEndDate,"yyyy-mm-dd hh:MM:ss");
-
-          Store.dispatch(initialize('editCommunityList',response));
-
-
-          Store.dispatch(change('editCommunityList','local',response.latitude+','+response.longitude));
-
-          State.cityData=`${response.provinceName}/${response.cityName}/${response.countyName}`
-					var bright_basic=[];
-					var bright_service=[];
-					var bright_special=[];
-					var bright_bright=[];
-
-					var photo_First=[];
-					var photo_List=[];
-					var photo_Detail=[];
-					response.photoVOs.map((item,index)=>{
-						if(item.type=='THEFIRST'){
-							item.src=item.photoUrl;
-							delete item.photoUrl;
-							photo_First.push(item);
-						}
-						if(item.type=='LIST'){
-							item.src=item.photoUrl;
-							delete item.photoUrl;
-							photo_List.push(item);
-						}
-						if(item.type=='DETAILS'){
-							item.src=item.photoUrl;
-							delete item.photoUrl;
-							photo_Detail.push(item);
-						}
-					})
-
-
-					_this.setState({
-						timeStart:response.businessBegin,
-						timeEnd:response.businessEnd,
-						cityId:response.cityId,
-						photoF:photo_First,
-						photoL:photo_List,
-						photoD:photo_Detail,
-            communityId:response.id,
-            communityName:response.name
-					})
-
-					response.brights.map((item,index)=>{
-						if(item.type=="BRIGHTPOINTS"){
-							bright_bright.push(item);
-						}
-						if(item.type=="INFRASTRUCTURE"){
-							bright_basic.push(item);
-						}
-						if(item.type=="SPECIALSERVICE"){
-							bright_special.push(item);
-						}
-						if(item.type=="BASICSERVICE"){
-							bright_service.push(item);
-						}
-						if(item.type=="TRANSPORTATION"){
-							Store.dispatch(change('editCommunityList','brightPorts.brightPoints',item.brightPoints));
-						}
-						if(item.type=="PERIMETER"){
-							Store.dispatch(change('editCommunityList','brightRound.brightPoints',item.brightPoints));
-						}
-					})
-
-          Store.dispatch(change('editCommunityList','porTypes',response.porTypes.length?response.porTypes:[{}]));
-					Store.dispatch(change('editCommunityList','bright_bright',bright_bright.length?bright_bright:[{type:'BRIGHTPOINTS'}]));
-					Store.dispatch(change('editCommunityList','bright_special',bright_special.length?bright_special:[{type:'SPECIALSERVICE'}]));
-					Store.dispatch(change('editCommunityList','bright_service',bright_service.length?bright_service:[{type:'INFRASTRUCTURE'}]));
-					Store.dispatch(change('editCommunityList','bright_basic',bright_basic.length?bright_basic:[{type:'BASICSERVICE'}]));
-
-					if(response.opened==true){
-						Store.dispatch(change('editCommunityList','opened','1'));
-					}
-					if(response.opened==false){
-						Store.dispatch(change('editCommunityList','opened','0'));
-					}
-
-
-					if(response.portalShow==true){
-						Store.dispatch(change('editCommunityList','portalShow','1'));
-					}
-					if(response.portalShow==false){
-						Store.dispatch(change('editCommunityList','portalShow','0'));
-					}
-
-				}).catch(function(err) {
-					Message.error(err.message);
-				});
-
-
-			}
+    
 
 			//展开
 			flagOpen=()=>{
@@ -512,10 +412,7 @@ const renderStation = ({ fields, meta: { touched, error }}) => {
 				}
 
 
-
-
-
-				const { error, handleSubmit, pristine, reset,dataReady,open} = this.props;
+				const { error, handleSubmit, pristine, reset,dataReady,open,cityData} = this.props;
 
 				return (
 					<div>
@@ -542,7 +439,7 @@ const renderStation = ({ fields, meta: { touched, error }}) => {
 							options={toJS(State.searchData)}
 							/>
 
-						<KrField grid={1/2} label="所属区县" name="countyId"  style={{width:262,marginLeft:16,position:'relative',zIndex:5}} component="city" onSubmit={this.cityValue} requireLabel={true} cityName={State.cityData}/>
+						<KrField grid={1/2} label="所属区县" name="countyId"  style={{width:262,marginLeft:16,position:'relative',zIndex:5}} component="city" onSubmit={this.cityValue} requireLabel={true} cityName={cityData}/>
 
 						<KrField grid={1/2} label="详细地址" name="address" style={{width:262,marginLeft:28}} component="input" requireLabel={true}/>
 
