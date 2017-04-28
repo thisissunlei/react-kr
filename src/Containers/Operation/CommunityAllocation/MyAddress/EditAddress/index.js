523a2304c2580f77f4d3fbbb0a82aba434f98b06
谭琳琳 <805148199@qq.com>
2017-04-28 10:52:18 +0800

    保存

---
+++ src/Containers/Operation/CommunityAllocation/MyAddress/EditAddress/index.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import {
+	toJS
+} from 'mobx';
+import {DateFormat} from 'kr/Utils';
+import {reduxForm,initialize,change,FieldArray} from 'redux-form';
+import {Actions,Store} from 'kr/Redux';
+import {
+	observer,
+	mobx
+} from 'mobx-react';
+import {
+	KrField,
+	Grid,
+	Row,
+	Col,
+	Button,
+	ButtonGroup,
+	Message,
+  Tabs,
+  Tab,
+} from 'kr-ui';
+import './index.less';
+import State from '../State';
+@observer
+ class NewCommunityList extends React.Component{
+
+	static PropTypes = {
+
+	}
+
+	constructor(props){
+		super(props);
+		this.state={
+			cityId:'',
+			openDown:true,
+      openUp:false,
+      communityName:'',
+      codeName:''
+		}
+	}
+  componentDidMount(){
+      
+  }
+
+	onSubmit = (values) => {
+  
+		const {onSubmit} = this.props;
+		onSubmit && onSubmit(values);
+  }
+
+	onCancel = () => {
+		const {onCancel} = this.props;
+		onCancel && onCancel();
+	}
+
+	closeEdit=()=>{
+		State.switchEditAddress();
+	}
+   
+	componentWillReceiveProps(nextProps) {
+	}
+
+
+
+
+
+
+	render(){
+
+		return (
+	      <div className="new-my-address">
+	        <div className="close-new-div">
+	          <img src={require('../images/closeIcon.svg')} className="close-new-img" onClick={this.closeEdit}/>
+	        </div>
+	        <Tabs className="new-my-address-tabs" inkBarStyle={{background:"#499df1",top:0}} initialSelectedIndex={-1} tabTemplateStyle={{color:"#333"}} style={{width:100}}>
+	          <Tab label="基本信息">
+
+	          </Tab>
+	          <Tab label="社区指南">
+	            <div className="community-guide-box">
+	              <Button  label="添加指南" type="button"  onTouchTap={this.onCancel}/>
+
+	            </div>
+	          </Tab>
+	        </Tabs>
+	      </div>
+		);
+	}
+}
+const validate = values =>{
+
+		const errors = {};
+
+
+      // if(!values.area){
+      //   errors.area='请输入社区面积';
+      // }
+
+		return errors
+	}
+export default reduxForm({ form: 'NewCommunityList',validate,enableReinitialize:true,keepDirtyOnReinitialize:true})(NewCommunityList);
--- src/Containers/Operation/CommunityAllocation/MyAddress/index.js
+++ src/Containers/Operation/CommunityAllocation/MyAddress/index.js
@@ -16,6 +16,7 @@ import {
   TableRowColumn,
   TableFooter,
   Dialog,
+  Grid,
   Section,
   Row,
   Col,
@@ -32,8 +33,8 @@ import {
 
 import './index.less'
 import State from './State';
-import NewCommunityList from './NewCommunityList';
-import EditCommunityList from './EditCommunityList';
+import NewAddress from './NewAddress';
+import EditAddress from './EditAddress';
 import SearchUpperForm from './SearchUpperForm';
 import WatchCommunityList from './WatchCommunityList';
 //todo:城市组件值不清空，后期补上
@@ -50,140 +51,18 @@ class CommunityList  extends React.Component{
   }
 
   componentDidMount(){
-    State.searchDataHere();
-
+    
   }
 
-   //新建社区开关
-   openAddCommunity=()=>{
-      cityDataState.setCity("请选择");
-      State.searchDataHere();
-      State.switchNewCommunityList();
+   //新建开关
+   openNewCreate=()=>{
+      State.switchNewAddress();
    }
-   //新建社区关闭
+   //新建关闭
    cancelAddCommunity=()=>{
-      State.switchNewCommunityList();
+      State.switchNewAddress();
    }
-   //新建社区提交
-   onNewCommunitySubmit=(value)=>{
-        value = Object.assign({},value);
-        //亮点开始
-        var brightsStr=[];
-        if(value.bright_basic){
-         value.bright_basic.map((item)=>{
-              if(!item){
-                return ;
-            }
-             let {type,brightPoints} = item;
-             let bright = Object.assign({},{type,brightPoints})
-             brightsStr.push(bright);
-         });
-       }
-
-        if(value.bright_service){
-           value.bright_service.map((item)=>{
-          if(!item){
-             return ;
-          }
-          let {type,brightPoints} = item;
-             let bright = Object.assign({},{type,brightPoints})
-             brightsStr.push(bright)
-         })
-        }
-
-        if(value.bright_special){
-          value.bright_special.map((item,index)=>{
-         if(!item){
-            return ;
-          }
-          let {type,brightPoints} = item;
-             let bright = Object.assign({},{type,brightPoints})
-             brightsStr.push(bright)
-         })
-        }
-
-        if(value.bright_bright){
-          value.bright_bright.map((item,index)=>{
-          if(!item){
-             return ;
-           }
-             let {type,brightPoints} = item;
-             let bright = Object.assign({},{type,brightPoints})
-             brightsStr.push(bright)
-         })
-        }
-
-        if(value.brightPorts){
-          brightsStr.push({type:'TRANSPORTATION',brightPoints:value.brightPorts.brightPoints});
-        }
-        if(value.brightRound){
-          brightsStr.push({type:'PERIMETER',brightPoints:value.brightRound.brightPoints});
-        }
-        if(brightsStr.length){
-           value.brightsStr=JSON.stringify(brightsStr);
-        }
-        //亮点结束
-
-
-
-        //楼层开始
-        value.wherefloorsStr=JSON.stringify(value.wherefloors);
-        //楼层结束
-
-
-
-        //工位开始
-        if(value.porTypes){
-           value.porTypesStr=JSON.stringify(value.porTypes);
-        }
-        //工位结束
-
-
-       //图片开始
-       var photosStr=[];
-       if(value.photosStr_first){
-          value.photosStr_first.map((item,index)=>{
-            let images = Object.assign({},{type:'THEFIRST',photoId:item.photoId,first:(index?false:true)})
-            photosStr.push(images)
-          })
-        }
-
-        if(value.photosStr_list){
-          value.photosStr_list.map((item,index)=>{
-            let images = Object.assign({},{type:'LIST',photoId:item.photoId,first:(index?false:true)})
-             photosStr.push(images)
-          })
-         }
-
-         if(value.photosStr_detail){
-            value.photosStr_detail.map((item,index)=>{
-            let images = Object.assign({},{type:'DETAILS',photoId:item.photoId,first:(index?false:true)})
-            photosStr.push(images)
-          })
-         }
-
-         value.photosStr=JSON.stringify(photosStr);
-
-         delete value.photosStr_first;
-         delete value.photosStr_list;
-         delete value.photosStr_detail;
-         delete value.bright_basic;
-         delete value.bright_service;
-         delete value.bright_special;
-         delete value.bright_bright;
-         delete value.brightPorts;
-         delete value.brightRound;
-         delete value.brights;
-         delete value.wherefloors;
-         delete value.porTypes;
-         delete value.photoVOs;
-
-         //图片结束
-
-
-         State.onNewCommunitySubmit(value);
-
-   }
+  
    //查询
   onSearchSubmit=(params)=>{
       let obj = {
@@ -201,33 +80,28 @@ class CommunityList  extends React.Component{
    //查看或编辑
    onOperation=(type,itemDetail)=>{
       var id=itemDetail.id;
-      if(type=='watch'){
-         State.getEditList(id)
-         State.switchWatchList();
-         return ;
+      if(type=='edit'){
+        State.switchEditAddress();
       }
-       if(type=='edit'){
-         this.setState({
-          id
-         });
-          State.searchDataHere();
-          State.switchEditList();
+      if(type=='delete'){
+         State.isOpenDeleteDialog();
       }
    }
 
-   //查看取消
-   onSwitchCancelWatchList=()=>{
-     State.switchWatchList();
-   }
    //编辑取消
    switchEditList=()=>{
-       State.switchEditList();
+       State.switchEditAddress();
+   }
+
+   openDeleteDialog=()=>{
+    State.openDeleteDialog();
    }
 
 
   whiteClose=()=>{
     State.closeAllDialog();
   }
+
 
   render(){
 
@@ -237,6 +111,7 @@ class CommunityList  extends React.Component{
         value:'NAME'
       },
     ]
+    
 
     return(
 
@@ -251,7 +126,7 @@ class CommunityList  extends React.Component{
                   <Button
                       label="新建地点"
                       type='button'
-                      onTouchTap={this.openAddCommunity}
+                      onTouchTap={this.openNewCreate}
                   />
             </Col>
 
@@ -293,74 +168,70 @@ class CommunityList  extends React.Component{
                       <TableRowColumn name="portalShow" options={[{label:'显示',value:'true'},{label:'不显示',value:'false'}]}></TableRowColumn>
                       <TableRowColumn type="operation">
                           <Button label="编辑"  type="operation"  operation="edit" />
-                          <Button label="查看"  type="operation"  operation="watch" />
+                          <Button label="删除"  type="operation"  operation="delete" />
                       </TableRowColumn>
                      </TableRow>
               </TableBody>
               <TableFooter></TableFooter>
             </Table>
 
-                   {/*新建*/}
+          {/*新建*/}
           <Drawer
-                open={State.openNewCommunity}
+                open={State.openNewAddress}
                 width={750}
                 openSecondary={true}
                 containerStyle={{top:60,paddingBottom:48,zIndex:20}}
                 onClose={this.whiteClose}
               >
-            <NewCommunityList
+            <NewAddress
                 onSubmit={this.onNewCommunitySubmit}
                 onCancel={this.cancelAddCommunity}
-                open={State.openNewCommunity}
+                open={State.openNewAddress}
             />
 
-                </Drawer>
+          </Drawer>
 
-                 {/*编辑*/}
+          {/*编辑*/}
           <Drawer
-                open={State.openEditCommunity}
+                open={State.openEditAddress}
                 width={750}
                 openSecondary={true}
                 onClose={this.whiteClose}
                 containerStyle={{top:60,paddingBottom:48,zIndex:21}}
               >
-            <EditCommunityList
-                onSubmit={this.onNewCommunitySubmit}
-                onCancel={this.switchEditList}
-                open={State.openEditCommunity}
-                id={this.state.id}
+            <EditAddress
+                // onSubmit={this.onNewCommunitySubmit}
+                // onCancel={this.switchEditList}
+                // open={State.openEditAddress}
+                // id={this.state.id}
             />
 
                 </Drawer>
 
-                    {/*高级查询*/}
-                    <Dialog
-            title="高级查询"
-            onClose={this.openSearchUpperDialog}
-            open={State.openSearchUpper}
-            contentStyle ={{ width: '666px',height:'458px'}}
+        {/*高级查询*/}
+          <Dialog
+          title="提示"
+          open={State.openDeleteDialog}
+          onClose={State.isOpenDeleteDialog}
+          contentStyle={{width:443,height:236}}
           >
-            <SearchUpperForm
-                onCancel={this.openSearchUpperDialog}
-                onSubmit={this.onSearchUpperSubmit}
-                open={State.openSearchUpper}
-            />
-            </Dialog>
+          <div style={{marginTop:45}}>
+            <p style={{textAlign:"center",color:"#333333",fontSize:14}}>确定删除吗？</p>
+            <Grid style={{marginTop:60,marginBottom:'4px'}}>
+                  <Row>
+                    <ListGroup>
+                      <ListGroupItem style={{width:175,textAlign:'right',padding:0,paddingRight:15}}>
+                        <Button  label="确定" type="submit" onClick={State.confirmDelete} />
+                      </ListGroupItem>
+                      <ListGroupItem style={{width:175,textAlign:'left',padding:0,paddingLeft:15}}>
+                        <Button  label="取消" type="button"  cancle={true} onTouchTap={State.isOpenDeleteDialog} />
+                      </ListGroupItem>
+                    </ListGroup>
+                  </Row>
+                </Grid>
+          </div>
+        </Dialog>
 
-
-                     {/*查看*/}
-          <Drawer
-                open={State.openWatchCommunity}
-                width={750}
-                onClose={this.whiteClose}
-                openSecondary={true}
-                containerStyle={{top:60,paddingBottom:48,zIndex:20}}
-              >
-            <WatchCommunityList
-                onCancel={this.onSwitchCancelWatchList}
-            />
-
-                </Drawer>
 
        </Section>
 
