2d7aec85e4fdafe00872bf6739d573457b208845
马超越 <machaoyue@36kr.com>
2017-05-02 11:27:06 +0800

    fix

--- src/Containers/Operation/CustomerManage/AgreementList/Join/Edit/NewCreateForm.js
+++ src/Containers/Operation/CustomerManage/AgreementList/Join/Edit/NewCreateForm.js
@@ -201,7 +201,7 @@ class NewCreateForm extends React.Component {
 
 	onChangeSearchPersonel(personel) {
 		Store.dispatch(change('joinEditForm', 'lessorContacttel', personel.mobile));
-		Store.dispatch(change('joinEditForm', 'lessorContactName', personel.lastname));
+		Store.dispatch(change('joinEditForm', 'lessorContactName', personel.lastname || '请选择'));
 	}
 
 
