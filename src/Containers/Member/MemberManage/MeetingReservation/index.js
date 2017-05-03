ddeb44c914548bd77526fb368b7deab165692ff8
刘毅豪 <liuyihao@krspace.cn>
2017-05-03 14:08:50 +0800

    feature/目录搭建

---
+++ src/Containers/Member/MemberManage/MeetingReservation/index.js
@@ -0,0 +1,55 @@
+
+import React from 'react';
+import {initialize} from 'redux-form';
+
+import {Actions,Store} from 'kr/Redux';
+import {Http} from 'kr/Utils';
+
+import {
+	Table,
+	TableBody,
+	TableHeader,
+	TableHeaderColumn,
+	TableRow,
+	TableRowColumn,
+	TableFooter,
+	Button,
+	Section,
+	Grid,
+	Row,
+	Col,
+	Dialog,
+	SearchForms,
+	ListGroup,
+	ListGroupItem,
+	Message,
+	Title,
+	SnackTip
+
+} from 'kr-ui';
+
+
+import './index.less';
+
+export default class List extends React.Component {
+
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			
+		}
+	}
+	
+	
+	 
+    render(){
+        
+        return(
+            <div className="switchhover">
+            
+            </div>
+        );
+    }
+
+
+}
