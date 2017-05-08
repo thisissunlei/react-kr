d901a6d986985781e85eb70a97433f0cb27fd1cd
张屈 <joneyphair@qq.com>
2017-05-08 16:51:24 +0800

    fix:导航

---
+++ src/Containers/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+
+	constructor(props,context){
+		super(props, context);
+	}
+
+	render() {
+
+		return (
+				<div style={{marginBottom:"50px"}}>
+					导航
+				</div>
+	   );
+	}
+}
