d78efd743e868a925db2ff76a12435b5723ba1ed
dongfanai <dongfanai@krspace.cn>
2017-05-10 15:50:21 +0800

    feat：

--- src/Containers/Header/SidebarNav/index.js
+++ src/Containers/Header/SidebarNav/index.js
@@ -14,9 +14,10 @@ export default class SidebarNav extends React.Component {
 	renderMenuItems=(menuItems)=>{
 
 		return menuItems.map((item,index)=>{
+			var path=item.originUrl?item.originUrl:`.#${item.router}`
 			console.log('item------',item)
 			return(
-					<a href={`.#${item.router}`}  className={item.isActive?'u-sidebar-nav-active':''} key={index}>{item.primaryText}</a>
+					<a href={path}  className={item.isActive?'u-sidebar-nav-active':''} key={index}>{item.primaryText}</a>
 				)
 		})
 	}
@@ -25,23 +26,25 @@ export default class SidebarNav extends React.Component {
 
 		const {NavModel,SidebarNavDate} = this.props;
 		console.log('sidebarNavs',SidebarNavDate)
-		return (
-			<div className="g-sidebar-nav">
-				{SidebarNavDate.map((item,index)=>{
-					return(
-							<div className="m-sidebar-nav" key={index}>
-								<div className="u-sidebar-nav-title">
-									<span className={item.iconName} style={{color:`${item.iconColor}`}}></span>
-									{item.primaryText}
+			return (
+				<div className="g-sidebar-nav">
+					{SidebarNavDate.map((item,index)=>{
+						return(
+								<div className="m-sidebar-nav" key={index}>
+									<div className="u-sidebar-nav-title">
+										<span className={item.iconName} style={{color:`${item.iconColor}`}}></span>
+										{item.primaryText}
+									</div>
+									<div className="u-sidebar-navlist">
+										{this.renderMenuItems(item.menuItems)}
+									</div>
 								</div>
-								<div className="u-sidebar-navlist">
-									{this.renderMenuItems(item.menuItems)}
-								</div>
-							</div>
-						)
-				})}
-				
-			</div>
-	   );
+							)
+					})}
+					
+				</div>
+		   );
+
+		
 	}
 }
