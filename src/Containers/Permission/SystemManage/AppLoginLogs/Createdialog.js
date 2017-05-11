78113d8d00bf2e638396ee473743b2ce6e905669
张屈 <joneyphair@qq.com>
2017-05-11 14:37:09 +0800

    fix

--- src/Containers/Permission/SystemManage/AppLoginLogs/Createdialog.js
+++ src/Containers/Permission/SystemManage/AppLoginLogs/Createdialog.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {Actions, Store} from 'kr/Redux';
 import {
