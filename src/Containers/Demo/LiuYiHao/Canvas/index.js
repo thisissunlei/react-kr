634d0ee572220a70e1410fa2afbb5ef5362b65a8
刘毅豪 <liuyihao@krspace.cn>
2017-05-11 16:30:45 +0800

    fix

--- src/Containers/Demo/LiuYiHao/Canvas/index.js
+++ src/Containers/Demo/LiuYiHao/Canvas/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+
+import {
+	getOffset,
+	getCurrent
+} from './utils';
 export default class Canvas extends React.Component {
 
 	constructor(props, context) {
 		super(props, context);
 
 		this.state = {
-			
+
 		}
 
-	} 
+	}
+
 
 
 
@@ -17,11 +23,15 @@ export default class Canvas extends React.Component {
 
 	canvasClick = (event) =>{
 		//event.pageX获取鼠标相对页面的位置
-		console.log(event);
-		console.log(event.pageX,event.clientX,event.screenX,event.target.offsetLeft,">>>>>")
+		// console.log(event);
+		// console.log(event.pageX,event.clientX,event.screenX,event.target.offsetLeft,">>>>>")
+		//当前的元素
+		getCurrent(event)
+		console.log(elemY,elemX,);
 
 	}
 	getOffsetTop = (obj) =>{
+
 		 var tmp = obj.offsetTop;
 		 var val = obj.offsetParent;
 		 while(val != null){
@@ -33,9 +43,7 @@ export default class Canvas extends React.Component {
 
 	render() {
 		return (
-			<div style = {{width:1000,height:1000,background:"#ccc",position:"relative"}} onMouseDown= {this.canvasClick}>
-
-			</div>
+			<canvas width = "1000" height = "1000" style = {{background:"#ccc",position:"relative"}} onMouseDown= {this.canvasClick} />
 
 		);
 	}
