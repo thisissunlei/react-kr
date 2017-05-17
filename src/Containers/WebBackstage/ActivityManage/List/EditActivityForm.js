fe6ab5a60b6dcaa0b3a593cba5acda29aa67da87
马超越 <machaoyue@36kr.com>
2017-05-17 12:24:38 +0800

    合同

--- src/Containers/WebBackstage/ActivityManage/List/EditActivityForm.js
+++ src/Containers/WebBackstage/ActivityManage/List/EditActivityForm.js
@@ -336,6 +336,7 @@ class EditActivityForm extends React.Component{
 		State.pcCoverPicDefaultValue = '';
 	}
 	render(){
+		console.log('--------');
 		const {handleSubmit} = this.props;
 		let {timeStart,timeEnd,communitys} = this.state;
 		// 对应功能选项
