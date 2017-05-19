17e4560ea413c8ef9081a543ca3e87c465e4094d
张弛 <zhangchi@krspace.cn>
2017-05-19 13:40:11 +0800

    sso

--- src/Configs/Apis/PermissionApi/index.js
+++ src/Configs/Apis/PermissionApi/index.js
@@ -5,42 +5,42 @@ module.exports = {
 
     //个人中心-获取个人信息
     'PersonalCenterData': {
-        url: '/api-old/sys/sysOwn/getPersonalInfo',
+        url: '/api/krspace-sso-web/sso/sysOwn/getPersonalInfo',
         method: 'get'
     },
     //个人中心-身份验证手机
     'PersonalCenterVerifyIdByMobile': {
-        url: '/api-old/sys/sysOwn/verifyMobile?verifyCode={verifyCode}',
+        url: '/api/krspace-sso-web/sso/sysOwn/verifyMobile?verifyCode={verifyCode}',
         method: 'get'
     },
     //个人中心-身份验证邮箱
     'PersonalCenterVerifyIdByMail': {
-        url: '/api-old/sys/sysOwn/verifyEmail?verifyCode={verifyCode}',
+        url: '/api/krspace-sso-web/sso/sysOwn/verifyEmail?verifyCode={verifyCode}',
         method: 'get'
     },
     //个人中心-获取手机验证码
     'PersonalCenterGetMobileVerificationCode': {
-        url: '/api-old/sys/sysOwn/getVerifyCodeByMobile',
+        url: '/api/krspace-sso-web/sso/sysOwn/getVerifyCodeByMobile',
         method: 'get'
     },
     //个人中心-获取邮箱验证码
     'PersonalCenterGetMailVerificationCode': {
-        url: '/api-old/sys/sysOwn/getVerifyCodeByEmail',
+        url: '/api/krspace-sso-web/sso/sysOwn/getVerifyCodeByEmail',
         method: 'get'
     },
     //个人中心-修改手机号时新手机号获取验证码
     'PersonalCenterGetNewMobileVerificationCode': {
-        url: '/api-old/sys/sysOwn/getVerifyCodeByNewMobile?mobile={mobile}',
+        url: '/api/krspace-sso-web/sso/sysOwn/getVerifyCodeByNewMobile?mobile={mobile}',
         method: 'get'
     },
     //个人中心-验证修改手机号验证码
     'PersonalCenterVerifyReviseMobileVerificationCode': {
-        url: '/api-old/sys/sysOwn/editMobile',
+        url: '/api/krspace-sso-web/sso/sysOwn/editMobile',
         method: 'post'
     },
     //个人中心-修改密码
     'PersonalCenterVerifyRevisePwd': {
-        url: '/api-old/sys/sysOwn/editPassword',
+        url: '/api/krspace-sso-web/sso/sysOwn/editPassword',
         method: 'post'
     },
     //权限--登录
