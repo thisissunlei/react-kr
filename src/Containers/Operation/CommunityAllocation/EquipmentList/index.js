4e5cddced0c90137517dfd20fa18db56d92c27c2
刘毅豪 <liuyihao@krspace.cn>
2017-06-01 16:56:56 +0800

    fix:合同提示

--- src/Containers/Operation/CommunityAllocation/EquipmentList/index.js
+++ src/Containers/Operation/CommunityAllocation/EquipmentList/index.js
@@ -64,8 +64,11 @@ class EquipmentList  extends React.Component{
 
 	componentDidMount(){
 
-
+    window.addEventListener("scroll",this.scroll,false)
 	}
+  scroll = () =>{
+
+  }
    //搜索列表
    onSearchSubmit = (value) =>{
    	let {searchParams} = this.state;
