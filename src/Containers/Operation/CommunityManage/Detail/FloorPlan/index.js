534ed710c7dff4e62b91d11903e1ae5120dbe4cb
wusuling <wusuling@krspace.cn>
2017-06-02 11:47:02 +0800

    feat:连接平面图开始

--- src/Containers/Operation/CommunityManage/Detail/FloorPlan/index.js
+++ src/Containers/Operation/CommunityManage/Detail/FloorPlan/index.js
@@ -69,7 +69,49 @@ export default class FloorPlan extends React.Component {
 		data.pageSize=searchParams.pageSize;
 		var _this=this;
 		Http.request('getControlGraph',data).then(function(response) {
-			
+
+			var stationsDataOrigin = response.figures;
+            var stations = [];
+            stations = stationsDataOrigin.map(function (item, index) {
+
+                if (!item) {
+                    return;
+                }
+                var obj = {};
+                var x = item.cellCoordX;
+                var y = item.cellCoordY;
+
+                obj.x = Number(x);
+                obj.y = Number(y);
+                obj.width = Number(item.cellWidth);
+                obj.height = Number(item.cellHeight);
+                obj.name = item.cellName;
+                obj.belongType = item.belongType;
+                obj.belongId = Number(item.belongId);
+                obj.id = Number(item.id);
+                obj.canFigureId = item.canFigureId;
+                obj.type=obj.belongType;
+                return obj;
+            });
+            
+            var initializeConfigs = {
+                stations: stations,
+                backgroundImageUrl:'http://optest.krspace.cn'+response.graphFilePath,
+                translateX:0,
+                translateY:0,
+                mode:'view'
+            }
+
+            _this.setState({
+                initializeConfigs,
+            });
+            
+            _this.mapComponent.newMap(initializeConfigs);
+            _this.mapComponent.ready(function(data){
+                  _this.setState({
+                      loading:false
+                  })
+            });
 			console.log('vvvv',response);
 		}).catch(function(err) {
 			Message.error(err.message);
@@ -192,7 +234,8 @@ export default class FloorPlan extends React.Component {
 			communityIdList,
 			communityInfoFloorList,
 			dateend,
-			date
+			date,
+			initializeConfigs
 		} = this.state;
 
 		let {
@@ -218,10 +261,10 @@ export default class FloorPlan extends React.Component {
 			</form>
 			<p style={{margin:10}}></p>
 			{/*<IframeContent src={url} onClose={this.getState} className="floorIframe" onLoad={this.onLoad} width={'100%'} height={800} scrolling="no"/>*/}
-			{/*<PlanMapAll
+			<PlanMapAll
 			   ref={(mapComponent) => this.mapComponent = mapComponent}
-			   //initializeConfigs={initializeConfigs}
-			/>*/}
+			   initializeConfigs={initializeConfigs}
+			/>
 
 		</div>
 		);
