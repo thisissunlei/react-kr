830a436d7680c472d90d825d377dc14083c81520
谭琳琳 <805148199@qq.com>
2017-06-02 11:16:15 +0800

    提交测试

--- src/Containers/Member/MemberManage/DoorManage/ImpowerEditMemberForm.js
+++ src/Containers/Member/MemberManage/DoorManage/ImpowerEditMemberForm.js
@@ -171,7 +171,7 @@ export default class ImpowerEditMemberForm extends React.Component {
 					<Grid style={{margin:'25px 0'}}>
 						<Row>
 							<ListGroup>
-								<ListGroupItem style={{width:'270px',textAlign:'right',padding:0,paddingRight:15}}><Button  label="确定" type="submit"   width={90} height={34}/></ListGroupItem>
+								<ListGroupItem style={{width:'260px',textAlign:'right',padding:0,paddingRight:15}}><Button  label="确定" type="submit"   width={90} height={34}/></ListGroupItem>
 								<ListGroupItem style={{width:'240px',textAlign:'left',padding:0,paddingLeft:15}}><Button  label="取消" type="button" cancle={true} onTouchTap={this.onCancel} width={90} height={34}/> </ListGroupItem>
 							</ListGroup>
 						  </Row>
