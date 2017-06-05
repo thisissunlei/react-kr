483880bbb0fa22e3315412d288d92cb9ec08fc67
谭琳琳 <805148199@qq.com>
2017-06-05 13:52:09 +0800

    save

--- src/Configs/Router/StatisticalRouter/index.js
+++ src/Configs/Router/StatisticalRouter/index.js
@@ -24,26 +24,12 @@ const Statistical_Home = (location, callback) => {
 }
 
 
-
-
-
-
-
-const Statistical_PaymentRemindTable = (location, callback) => {
-  require.ensure([], require => {
-    callback(null, require('kr/Containers/Statistical/PaymentRemindTable').default)
-  }, 'Statistical_PaymentRemindTable')
-}
-
 module.exports =()=>{
 
 	return (
 		<Route path="statistical" getComponent={Basic}>
             <Route path="index" getComponent={Statistical_Home}/>
                 <IndexRedirect to="index" />
-
-            <Route path="paymentremindtable" getComponent={Statistical_PaymentRemindTable}/>
-
         </Route>
 
 
