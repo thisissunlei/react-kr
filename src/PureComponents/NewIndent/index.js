0f7f7f41273e8ad5f8685b5b3224025438ed5fd8
刘毅豪 <liuyihao@krspace.cn>
2017-06-08 15:04:34 +0800

    fix:新建订单样式

--- src/PureComponents/NewIndent/index.js
+++ src/PureComponents/NewIndent/index.js
@@ -146,7 +146,7 @@ import {Http} from "kr/Utils";
 					<Row>
 						<Col md={12} align="center">
 							<ButtonGroup>
-							<div  className='ui-btn-center'><Button  label="确定" type="submit" /></div>
+							<div style = {{display:"inline-block",marginRight:30}}><Button  label="确定" type="submit" /></div>
 							<Button  label="取消" type="button" cancle={true} onTouchTap={this.onCancel} />
 							</ButtonGroup>
 						</Col>
