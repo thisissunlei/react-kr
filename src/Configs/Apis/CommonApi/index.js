2026b23a8cb5d120e7482aad779503831675997a
刘毅豪 <liuyihao@krspace.cn>
2017-06-13 14:05:04 +0800

    feature:统一借口

--- src/Configs/Apis/CommonApi/index.js
+++ src/Configs/Apis/CommonApi/index.js
@@ -1,8 +1,13 @@
 //公共的api
 module.exports = {
-  //获取全局导航
+  //获取社区借口
   'getTheCommunity': {
       url: '/mockjsdata/31/krspace-finance-web/cmt/community/select/use/type/list',
       method: 'get'
   },
+  //获取楼层接口
+  'getFloorByComunity': {
+        url: '/mockjsdata/31/krspace-finance-web/cmt/community/select/use/floor?communityId={communityId}',
+        method: 'get'
+    },
 }
--- src/Configs/Apis/OperationApi/index.js
+++ src/Configs/Apis/OperationApi/index.js
@@ -28,11 +28,6 @@ module.exports = {
         method: 'get'
     },
     //运营平台-基础配置-设备定义-获取楼层
-    'getFloorByComunity': {
-        // url: '/api/krspace-finance-web/community/sysDeviceDefinition/getWherefloor?communityId={communityId}',
-        url: '/mockjsdata/31/krspace-finance-web/cmt/community/select/use/floor?communityId={communityId}',
-        method: 'get'
-    },
     //运营平台-基础配置-设备定义-根据属性获取对应位置
     'getLocationByProperty': {
         url: '/api/krspace-finance-web/community/sysDeviceDefinition/community-boardroom?communityId={communityId}&whereFloor={whereFloor}&type={type}',
