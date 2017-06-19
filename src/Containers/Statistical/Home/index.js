6c07741ed8bec350317b84abc68ee81b46b9e53d
wusuling <wusuling@krspace.cn>
2017-06-19 15:05:23 +0800

    feat:解决爆红2

--- src/Containers/Statistical/Home/index.js
+++ src/Containers/Statistical/Home/index.js
@@ -84,12 +84,12 @@ export default class Home  extends React.Component{
 
 				    <Tab label='集团经营'  key={1} onActive={this.companyTable} style={companyStyle}>
 				               <div className='tabWrap_section'>
-				                {tab=='table'&&<CompanyPanel/>}
+				               <CompanyPanel/>
 				               </div>
 				    </Tab>
 					<Tab label='招商数据' key={2} onActive={this.bussinessTable} style={businessStyle}>
 				               <div className='tabWrap_section'>
-				                {tab=='bus'&&<BussinessPanel/>}
+				                <BussinessPanel/>
 				               </div>
 				    </Tab>
 
