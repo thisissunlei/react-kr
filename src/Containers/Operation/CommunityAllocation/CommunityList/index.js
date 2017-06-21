bfb7f102666d2454f4f552113aee744e6cf232d7
刘毅豪 <liuyihao@krspace.cn>
2017-06-21 14:08:57 +0800

    feature:社区列表

--- src/Containers/Operation/CommunityAllocation/CommunityList/index.js
+++ src/Containers/Operation/CommunityAllocation/CommunityList/index.js
@@ -79,6 +79,7 @@ class CommunityList  extends React.Component{
    //新建社区提交
    onNewCommunitySubmit=(value)=>{
    	    value = Object.assign({},value);
+        
         //亮点开始
    	    var brightsStr=[];
         if(value.bright_basic){
@@ -200,7 +201,7 @@ class CommunityList  extends React.Component{
 
          //图片结束
 
-         
+        
    	     State.onNewCommunitySubmit(value);
 
    }
@@ -382,6 +383,7 @@ class CommunityList  extends React.Component{
      onSearchUpperSubmit=(searchParams)=>{
      	searchParams = Object.assign({},State.searchParams, searchParams);
      	searchParams.time=+new Date();
+       
 		if(searchParams.openDateBegin&&searchParams.openDateEnd&&searchParams.openDateEnd<searchParams.openDateBegin){
 			 Message.error('开始时间不能大于结束时间');
 	         return ;
@@ -392,7 +394,8 @@ class CommunityList  extends React.Component{
 		if(searchParams.openDateBegin && !searchParams.openDateEnd){
 			searchParams.openDateEnd = searchParams.openDateBegin
 		}
-
+        
+        
       	State.setSearchParams(searchParams);
       	State.searchUpperCustomer();
      }
@@ -440,6 +443,7 @@ class CommunityList  extends React.Component{
       var searchParams={
         page:page
       }
+
       State.searchParams=Object.assign({},State.searchParams,searchParams);
     }
 
