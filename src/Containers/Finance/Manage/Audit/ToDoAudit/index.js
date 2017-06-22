955e1697eb6f534127269c7fa3c9619250e84453
dongfanai <dongfanai@krspace.cn>
2017-06-22 17:03:13 +0800

    feat:待审核刷新

--- src/Containers/Finance/Manage/Audit/ToDoAudit/index.js
+++ src/Containers/Finance/Manage/Audit/ToDoAudit/index.js
@@ -54,7 +54,8 @@ export default class ToDoAudit extends React.Component {
       Params: {
         page: 1,
         pageSize: 10,
-        verifyStatus: 'UNCHECKED'
+        verifyStatus: 'UNCHECKED',
+        time:new Date(),
       },
       openCreateCustomer: false,
       openCreateMainbill: false,
@@ -338,11 +339,20 @@ export default class ToDoAudit extends React.Component {
       if (!form.mainBillId) {
         return;
       }
+
       Http.request('save-flow-verify', {}, form).then(function(response) {
           Message.success('新建成功');
-          window.setTimeout(function(){
-            window.location.reload();
-          },800)
+          _this.setState({
+            Params:{
+              verifyStatus:'UNCHECKED',
+              time:new Date(),
+              pageSize:15,
+            }
+          })
+          _this.getParentCount({
+            verifyStatus: 'UNCHECKED'
+          })
+           _this.openAddCreate();
         }).catch(function(err) {
           Message.error(err.message);
         });
