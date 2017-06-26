6648c0182e00891f01f7c87220d9335dc1b953b4
马超越 <machaoyue@36kr.com>
2017-06-26 15:47:41 +0800

    修改办公室为独立空间

--- src/Containers/Operation/CustomerManage/Agreement/Join/Detail/index.js
+++ src/Containers/Operation/CustomerManage/Agreement/Join/Detail/index.js
@@ -136,7 +136,7 @@ export default class JoinDetail extends React.Component {
 					<KrField component="labelText" grid={1/2} label="签署日期：" value={DateFormat(basic.signdate,"yyyy-mm-dd")} defaultValue="0" requireBlue={true}/>
 
 					<KrField component="labelText" left={60} label="租赁工位：" grid={1/2}  value={basic.stationnum} requireBlue={true} defaultValue="0"/>
-					<KrField component="labelText" label="租赁办公室：" grid={1/2} value={basic.boardroomnum} requireBlue={true} defaultValue="0"/>
+					<KrField component="labelText" label="租赁独立空间：" grid={1/2} value={basic.boardroomnum} requireBlue={true} defaultValue="0"/>
 
 					<KrField component="labelText" grid={1/2}  left={60} label="租金总额：" value={basic.totalrent} defaultValue="0" requireBlue={true}/>
 					<KrField component="labelText" grid={1/2} label="押金总额：" value={basic.totaldeposit} defaultValue="0" requireBlue={true}/>
--- src/PureComponents/Agreement/Increase/Detail/index.js
+++ src/PureComponents/Agreement/Increase/Detail/index.js
@@ -245,7 +245,7 @@ export default class JoinDetail extends Component {
 					<KrField component="labelText" grid={1/2} label="押金总额：" value={basic.totaldeposit} defaultValue="0" requireBlue={true} toolTrue='true'/>
 					<KrField component="labelText" grid={1/2} label="签署日期：" value={dateFormat(basic.signdate,"yyyy-mm-dd")} defaultValue="无" requireBlue={true}/>
 					<KrField component="labelText" label="租赁工位：" grid={1/2}  value={basic.stationnum} requireBlue={true} defaultValue="0" requireBlue={true} toolTrue='true' />
-					<KrField component="labelText" label="租赁办公室：" grid={1/2} value={basic.boardroomnum} requireBlue={true} defaultValue="0" requireBlue={true} toolTrue='true'/>
+					<KrField component="labelText" label="租赁独立空间：" grid={1/2} value={basic.boardroomnum} requireBlue={true} defaultValue="0" requireBlue={true} toolTrue='true'/>
 
 
 
