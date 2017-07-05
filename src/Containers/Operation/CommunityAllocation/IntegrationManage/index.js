d34bb0428a31b4a2df118750d3660dee9b72e318
dongfanai <dongfanai@krspace.cn>
2017-07-05 11:24:41 +0800

    feat:积分

---
+++ src/Containers/Operation/CommunityAllocation/IntegrationManage/index.js
@@ -0,0 +1,206 @@
+import React from 'react';
+import {Http} from 'kr/Utils';
+import {
+	Title,
+	Section,
+	Table,
+	TableBody,
+	TableHeader,
+	TableHeaderColumn,
+	TableRow,
+	TableRowColumn,
+	TableFooter,
+	Button,
+	Drawer,
+	Dialog,
+	Tooltip,
+	KrDate,
+	Message
+} from 'kr-ui';
+import './index.less';
+export default class Integration extends React.Component {
+
+
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			searchParams:{
+				page:1,
+				pageSize:15
+			},
+			openNewCreat:false,
+			openView:false,
+			openDelete:false,
+		}
+
+	}
+
+	componentDidMount() {
+		var _this=this;
+		
+		
+	}
+	//操作相关
+  	onOperation = (type, itemDetail) => {
+      this.setState({
+        itemDetail
+      });
+      switch (type){
+        case  'view':{
+         this.openView();
+          break;
+        }
+        
+        case  'delete':{
+         this.openDelete(itemDetail);
+          break;
+        }
+      }
+    }
+    //删除
+	onDeleteData=()=>{
+		var _this=this;
+		const {itemDetail}=this.state;
+		Http.request('del-notice',{},{topicId:itemDetail.topicId}).then(function (response) {
+			_this.openDelete();
+			Message.success('删除成功！');
+			_this.setState({
+				searchParams:{
+					date:new Date()
+				}
+			})
+
+		}).catch(function (err) { 
+			Message.error(err.message)
+		});
+
+	}
+	openNewCreat=()=>{
+		this.setState({
+			openNewCreat:!this.state.openNewCreat
+		})
+
+	}
+	openView=()=>{
+		this.setState({
+			openView:!this.state.openView
+		})
+	}
+	openDelete=()=>{
+		this.setState({
+			openDelete:!this.state.openDelete
+		})
+	}
+	createSubmit=()=>{
+		this.setState({
+			searchParams:{
+					date:new Date(),
+					pageSize:15
+				}
+		})
+		this.openNewCreat();
+	}
+	
+	render() {
+		let {itemDetail}=this.state;
+		return (
+
+			<div className="g-notice" >
+			<Title value="公告管理"/>
+				<Section title="公告列表" description="" style={{marginBottom:-5,minHeight:910}}>
+					<div className="m-btn">
+						<Button
+								label="新建公告"
+								type='button'
+								onTouchTap={this.openNewCreat}
+							/>
+					</div>
+					<Table
+						  style={{marginTop:10}}
+		                  ajax={true}
+		                  ajaxUrlName='notice-list'
+		                  ajaxParams={this.state.searchParams}
+		                  onOperation={this.onOperation}
+		                 
+					  >
+				            <TableHeader>
+				              <TableHeaderColumn>群组名称</TableHeaderColumn>
+				              <TableHeaderColumn>社区名称</TableHeaderColumn>
+				              <TableHeaderColumn>发布内容</TableHeaderColumn>
+				              <TableHeaderColumn>发布时间</TableHeaderColumn>
+				              <TableHeaderColumn>发布人</TableHeaderColumn>
+				              <TableHeaderColumn>操作</TableHeaderColumn>
+				          	</TableHeader>
+
+					        <TableBody >
+					              <TableRow>
+					                <TableRowColumn name="clusterName" ></TableRowColumn>
+					                <TableRowColumn name="cmtName"></TableRowColumn>
+					                <TableRowColumn 
+					                		name="topicContent" 
+					                		component={(value,oldValue)=>{
+												var TooltipStyle=""
+												if(value.length==""){
+													TooltipStyle="none"
+
+												}else{
+													TooltipStyle="inline-block";
+												}
+												return (
+													<div style={{display:TooltipStyle,paddingTop:5}} className='financeDetail-hover'>
+													<span className='tableOver' style={{maxWidth:200,display:"inline-block",overflowX:"hidden",textOverflow:" ellipsis",whiteSpace:" nowrap",paddingTop: '6px'}}>{value}</span>
+												 		<Tooltip offsetTop={5} place='top'>{value}</Tooltip>
+												 	</div>
+												)
+											}} 
+									></TableRowColumn>
+					                <TableRowColumn 
+					                	name="topicDate" 
+					                	component={(value) => {
+					                          return (<KrDate value={value} format="yyyy-mm-dd hh:MM:ss"/>)
+					                    }}
+					                ></TableRowColumn>
+					                <TableRowColumn name="authorName" ></TableRowColumn>
+					                <TableRowColumn>
+					                	<Button label="查看"  type="operation"  operation="view"/>
+									  	<Button label="删除"  type="operation"  operation="delete"/>
+					                </TableRowColumn>
+					               </TableRow>
+					        </TableBody>
+			        		<TableFooter></TableFooter>
+            		</Table>
+				</Section>
+				
+	           <Drawer
+	             modal={true}
+	             width={750}
+	             open={this.state.openView}
+	             onClose={this.openView}
+	             openSecondary={true}
+	             containerStyle={{paddingRight:43,paddingTop:40,paddingLeft:48,paddingBottom:48,zIndex:20}}
+	           >
+	             	{/*<ViewNotice 
+	             			onCancel={this.openView} 
+	             			detail={itemDetail}
+	             	 />*/}
+	           </Drawer>
+	           <Dialog
+	              title="删除"
+	              modal={true}
+	              contentStyle ={{ width: '444',overflow:'visible'}}
+	              open={this.state.openDelete}
+	              onClose={this.openDelete}
+	            >
+	            <div className='u-list-delete'>
+	              	<p className='u-delete-title' style={{textAlign:'center',color:'#333'}}>确认要删除帖子吗？</p>
+					<div style={{textAlign:'center',marginBottom:10}}>
+	                      <div  className='ui-btn-center'>
+		                      <Button  label="确定" onClick={this.onDeleteData}/></div>
+		                      <Button  label="取消" type="button" cancle={true} onClick={this.openDelete} />
+	                      </div>
+	            	</div>
+	            </Dialog>
+			</div>
+		);
+	}
+}
