b207ec4fc6ce73030ca371f0f1eae7f645b4da43
刘毅豪 <liuyihao@krspace.cn>
2017-07-11 10:58:16 +0800

    feature:search

---
+++ src/Containers/Statistical/DataReport/SearchForm.js
@@ -0,0 +1,132 @@
+import React, {
+	PropTypes
+} from 'react';
+import {
+	reduxForm,
+	initialize,
+	change
+} from 'redux-form';
+
+import {Actions,Store} from 'kr/Redux';
+
+import {
+	KrField,
+	ListGroup,
+	ListGroupItem,
+	SearchForms,
+	Message
+} from 'kr-ui';
+import $ from 'jquery';
+import "./index.less";
+import {DateFormat} from 'kr/Utils';
+
+import State from './State';
+import {
+	observer
+} from 'mobx-react';
+@observer
+
+class SearchForm extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+	}
+	componentDidMount() {
+		
+	}
+
+	componentWillReceiveProps(nextProps){
+		
+	}
+
+
+	chooseCommunity=(item)=>{
+		
+		if(!item || !item.id){
+			State.searchParams.communityId = '';
+		}else{
+			State.searchParams.communityId = item.id;
+		}
+		State.searchParams.page = 1;
+		$(".table-box").eq(0).scrollTop(0);
+		State.getList();
+	}
+
+	chooseStartTime=(date)=>{
+		
+		
+		if(State.searchParams.endDate && this.getTimeFun(date)>this.getTimeFun(State.searchParams.endDate)){
+			Message.error("开始时间不能大于结束时间");
+			return;
+		}
+		State.searchParams.beginDate = date;
+		State.searchParams.page = 1;
+		$(".table-box").eq(0).scrollTop(0);
+		State.getList();
+
+	}
+
+	chooseEndTime=(date)=>{
+
+		if(State.searchParams.beginDate && this.getTimeFun(State.searchParams.beginDate)>this.getTimeFun(date)){
+			Message.error("开始时间不能大于结束时间");
+			return;
+		}
+		State.searchParams.endDate = date;
+		State.searchParams.page = 1;
+		$(".table-box").eq(0).scrollTop(0);
+		State.getList();
+
+	}
+	
+	inputCompanyName=(value)=>{
+		
+		State.searchParams.companyName = value.content;
+		State.searchParams.page = 1;
+		$(".table-box").eq(0).scrollTop(0);
+		State.getList();
+	}
+
+	// 转成时间戳
+	getTimeFun=(date)=>{
+
+		var timeDateArr = date.split(" ");
+		var timeDate = Date.parse(timeDateArr[0]);
+		return timeDate;
+	}
+
+	render() {
+
+		return (
+
+			<form>
+
+				<div className='search-payment-remind-form'>
+					<ListGroup>
+						<ListGroupItem>
+							<KrField  name="communityId" component="searchCommunityAll" label="社区名称：" inline={true} style={{width:244}} onChange={this.chooseCommunity}/>
+						</ListGroupItem>
+
+						<ListGroupItem>
+							<KrField label="催款日期：" name="startDate" component="date" inline={true} style={{width:244,marginTop:-3}} onChange={this.chooseStartTime}/>
+						</ListGroupItem>
+
+						<ListGroupItem>
+							<div className="search-form-endDate">
+								<KrField label="至" name="endDate" component="date" inline={true} style={{width:200,marginTop:-3}} onChange={this.chooseEndTime} />
+							</div>
+						</ListGroupItem>
+						
+					</ListGroup>
+
+				</div>
+			</form>
+		);
+	}
+}
+
+
+export default reduxForm({
+	form: 'SearchForm'
+})(SearchForm);
--- src/Containers/Statistical/DataReport/index.js
+++ src/Containers/Statistical/DataReport/index.js
@@ -3,15 +3,18 @@ import React from 'react';
 import {
 	CheckPermission,
 	Button,
-	Dialog
+	Dialog,
+	Title,
+	Section
 } from 'kr-ui';
 import './index.less';
 import ReportDetail from './ReportDetail';
 import State from './State';
-import {
-	observer,
-} from 'mobx-react';
-@observer
+// import {
+// 	observer,
+// } from 'mobx-react';
+import SearchForm from "./SearchForm";
+// @observer
 export default class DataReport extends React.Component {
 
 	constructor(props, context) {
@@ -33,15 +36,23 @@ export default class DataReport extends React.Component {
 		
 		return (
 			<div className="data-report">
-                <div onClick={this.detailClick}>button</div>
-                {/*高级查询*/}
-                <Dialog
-                    title="报表明细"
-                    onClose={this.detailClick}
-                    open={this.state.openReportDetail}
-                >
-                    <ReportDetail />
-                </Dialog>
+				<Title value="催款表"/>
+					<Section title="催款表">
+						<SearchForm/>
+
+							<div onClick={this.detailClick}>button</div>
+
+							{/*详情*/}
+							<Dialog
+								title="报表明细"
+								onClose={this.detailClick}
+								open={this.state.openReportDetail}
+							>
+
+								<ReportDetail />
+							</Dialog>
+					</Section>
+             
 			</div>
 
 		);
