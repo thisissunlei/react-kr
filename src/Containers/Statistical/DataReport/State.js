d4b0a6318086eec6e1d70c297abe7c8db8bbdcbf
刘毅豪 <liuyihao@krspace.cn>
2017-07-15 19:28:18 +0800

    feature:数据成功

--- src/Containers/Statistical/DataReport/State.js
+++ src/Containers/Statistical/DataReport/State.js
@@ -2,10 +2,20 @@ import mobx, {
 	observable,
 	action,
 } from 'mobx';
+import {DateFormat} from "kr/Utils";
 let State = observable({
 	//是否是签约
-	isAdd:"add" //add 和 sign
-    //点击传参
+	isAdd:"add", //add 和 sign
+    //参数
+	searchParams:{
+		cityId :'',
+		communityId:'',
+		// searchStartDate:DateFormat(new Date() ,"yyyy-mm-dd HH:MM:ss"),
+		// searchEndDate:DateFormat(new Date() ,"yyyy-mm-dd HH:MM:ss"),
+		searchStartDate:"",
+		searchEndDate:"",
+
+	}
 	
 });
 module.exports = State;
