e222bf2e64036d0fa450e723d2e4d9240c9035a3
wusuling <wusuling@krspace.cn>
2017-07-19 19:48:19 +0800

    feat:人员详情

--- src/Containers/OA/PeopleDetail/index.js
+++ src/Containers/OA/PeopleDetail/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {	
-	
+	TabC,
+	TabCs
 } from 'kr-ui';
 import BasicInfo from './BasicInfo';
 import PersonalInfo from './PersonalInfo';
@@ -17,14 +18,49 @@ export default class PeopleDetail  extends React.Component{
 
 	render(){
 
+		let initStyle={
+			color:'#666666',
+			border: '1px solid #E1E6EB',
+			borderRadius: '4px 4px 0 0',
+			borderBottom:'none',
+		}
+        
+		let activeStyle={
+			color:'#4990E2',
+			border: '1px solid #E1E6EB',
+			borderRadius: '4px 4px 0 0',
+			borderBottom:'none'
+		}
+
 		return(
 
 			<div className='people-detail'>
-			  <div className='detail-left'>left</div>
+			  <div className='detail-left'>
+				<div className='left-pic'>
+					pic
+				</div>
+				<div className='left-text'>
+					123
+				</div>
+			  </div>
 			  <div className='detail-right'>
-			     <BasicInfo />
-				 {/*<PersonalInfo />*/}
-                 {/*<WorkInfo />*/}
+				  <TabCs
+					initStyle={initStyle} 
+					activeStyle={activeStyle}
+			      >
+				  <TabC label='基本信息'> 
+					  <BasicInfo />
+				  </TabC> 
+				  
+				  <TabC label='个人信息'> 
+					  <PersonalInfo />
+				  </TabC> 
+
+				  <TabC label='工作信息'> 
+					  <WorkInfo />
+				  </TabC> 
+			  </TabCs>
+			    
 			  </div>
 			</div>
 		);
