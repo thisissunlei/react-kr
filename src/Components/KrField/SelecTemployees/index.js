8e1a534b1e2a888069bdf9ed8aa6e0a1ed904198
刘毅豪 <liuyihao@krspace.cn>
2017-07-21 17:38:12 +0800

    feature;

--- src/Components/KrField/SelecTemployees/index.js
+++ src/Components/KrField/SelecTemployees/index.js
@@ -3,8 +3,12 @@ import React from 'react';
 
 import SelectComponent from '../SelectComponent';
 
-import temployees from './temployees.json'
-import bloodType from './bloodType.json'
+import temployees from './temployees.json';
+import bloodType from './bloodType.json';
+import degree from './degree.json';
+import educationType from './educationType.json';
+import entryResource from './entryResource.json';
+
 
 export default class SelecTemployees extends React.Component {
 
@@ -17,14 +21,28 @@ render() {
 			...other
 		} = this.props;
 		let data = ""
-		switch(otherType)
-		{
+		switch(otherType){
+			//星座
 			case "temployees":
 				data=temployees;
 				break;
+			//血型
 			case "bloodType":
 				data=temployees;
 				break;
+			//学士学位
+			case "degree":
+				data=degree;
+				break;
+			//学历
+			case "educationType":
+				data=educationType;
+				break;
+			//入职来源
+			case "entryResource":
+				data=entryResource;
+				break;
+
 			default:
 				data=temployees;
 		}
