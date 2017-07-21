9903b63ff267f296055066a4763c61dc3f45494d
刘毅豪 <liuyihao@krspace.cn>
2017-07-21 08:46:30 +0800

    feature:身份证号算星座

--- src/Configs/Apis/OA/index.js
+++ src/Configs/Apis/OA/index.js
@@ -70,4 +70,11 @@ module.exports = {
       url: '/api/krspace-erp-web/hrm/job/add',
       method: 'post'
   },
+  /*====离职列表借口*/
+  //职务列表－新增
+  'postListAdd': {
+      url: '/api/krspace-erp-web/hrm/job/add',
+      method: 'post'
+  },
+
 }
