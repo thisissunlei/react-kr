a7afac8c649a86922172998eebb051f19f4d6f72
wusuling <wusuling@krspace.cn>
2017-08-03 09:45:01 +0800

    fix:百度

--- src/Containers/OA/OaBackground/NewOffice/index.js
+++ src/Containers/OA/OaBackground/NewOffice/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {
-	
+import {	
 	Section,
-
+	IframeContent
 } from 'kr-ui';
 
 export default class Initialize  extends React.Component{
@@ -12,14 +11,15 @@ export default class Initialize  extends React.Component{
 	}
 
 
-
 	render(){
+
+		let url='http://www.baidu.com';
 
 		return(
 
 			<div>
 					<Section title="订单账单列表" description="" >
-					  123
+					  <IframeContent src={url}  width={'100%'} height={800} scrolling="no"/>
 					</Section>
 			</div>
 		);
