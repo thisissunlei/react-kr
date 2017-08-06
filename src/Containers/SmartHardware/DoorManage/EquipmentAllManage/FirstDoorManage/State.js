11b8aa9db0c663f0a343726aa158dd4623ffb9c2
谭琳琳 <805148199@qq.com>
2017-08-06 15:37:04 +0800

    save

--- src/Containers/SmartHardware/DoorManage/EquipmentAllManage/FirstDoorManage/State.js
+++ src/Containers/SmartHardware/DoorManage/EquipmentAllManage/FirstDoorManage/State.js
@@ -1,41 +1,41 @@
-
-
 import mobx, {
 	observable,
 	action,
 } from 'mobx';
-
+import {Actions,Store} from 'kr/Redux';
+import {
+	reduxForm,
+	initialize,
+	change
+} from 'redux-form';
 import {Http} from 'kr/Utils';
 import {Message} from 'kr-ui';
 
 let State = observable({
-
-	// isLeft: true,
-	items : [],
-	endDate:'',
-	communityId:'',
-	loading: true,
-
+	makerOptions : []
 });
 
-
-
-State.getCollectList = action(function() {
-	let searchParams = {
-		endDate :State.endDate,
-		communityId : State.communityId
-	}
-	
-
-	Http.request('communityListAging',searchParams).then(function(response) {
-
-		State.items=response;
+State.getListDic = action(function() {
+	var _this = this;
+	Http.request('getListDic', {}).then(function(response) {
 		
+		var arrNew = []
+		for (var i=0;i<response.Maker.length;i++){
+			arrNew[i] = {
+						label:response.Maker[i].desc,
+						value:response.Maker[i].value
+					}
+		}
 		
+		State.makerOptions = arrNew;
+
 	}).catch(function(err) {
 		Message.error(err.message);
 	});
 
 });
+
+
+
 
 module.exports = State;
