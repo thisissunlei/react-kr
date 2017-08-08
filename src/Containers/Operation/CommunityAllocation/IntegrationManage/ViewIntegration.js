11b0ee14665c18df9a62a5517590925b965a4743
dongfanai <dongfanai@krspace.cn>
2017-08-08 19:00:34 +0800

    feat:

--- src/Containers/Operation/CommunityAllocation/IntegrationManage/ViewIntegration.js
+++ src/Containers/Operation/CommunityAllocation/IntegrationManage/ViewIntegration.js
@@ -43,11 +43,12 @@ export default class ViewIntegration extends React.Component {
 				infoList,
 				ifCity,
 			}=this.state;
+			const {detail}=this.props;
 			
 		return (
 			<div className="g-view-integration">
 				<div className="u-create-title">
-						<div className="title-text">积分消费记录</div>
+						<div className="title-text"><span className="u-view-customerName">{detail.customerName}</span>—积分消费记录</div>
 						<div className="u-create-close" onClick={this.onCancel}></div>
 				</div>
 				<div style={{paddingLeft:50}}>
@@ -60,35 +61,14 @@ export default class ViewIntegration extends React.Component {
 		                  displayCheckbox={false}
 					  >
 				            <TableHeader>
-				              <TableHeaderColumn>客户名称</TableHeaderColumn>
 				              <TableHeaderColumn>社区</TableHeaderColumn>
 				              <TableHeaderColumn>记录描述</TableHeaderColumn>
 				              <TableHeaderColumn>积分</TableHeaderColumn>
 				              <TableHeaderColumn>创建时间</TableHeaderColumn>
 				              <TableHeaderColumn>操作人</TableHeaderColumn>
 				          	</TableHeader>
-
 					        <TableBody >
 					              <TableRow>
-					                <TableRowColumn 
-					                	name="customerName"
-					                	component={(value,oldValue)=>{
-												var TooltipStyle=""
-												if(value.length==""){
-													TooltipStyle="none"
-
-												}else{
-													TooltipStyle="inline-block";
-												}
-												return (
-													<div style={{display:TooltipStyle,paddingTop:5}} className='financeDetail-hover'>
-													<span className='tableOver' style={{maxWidth:150,display:"inline-block",overflowX:"hidden",textOverflow:" ellipsis",whiteSpace:" nowrap",paddingTop: '6px'}}>{value}</span>
-												 		<Tooltip offsetTop={5} place='top'>{value}</Tooltip>
-												 	</div>
-												)
-											}} 
-					                >
-					                </TableRowColumn>
 					                <TableRowColumn 
 					                		name="cmtName"
 					                		component={(value,oldValue)=>{
