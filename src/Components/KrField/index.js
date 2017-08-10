e0afd983a752a4b88cc837bb7af36396a871e1f6
谭琳琳 <805148199@qq.com>
2017-08-10 15:16:02 +0800

    删除注释

--- src/Components/KrField/index.js
+++ src/Components/KrField/index.js
@@ -106,10 +106,6 @@ class FieldRevert extends React.Component {
 				return <MobxForm.Field {...this.props} />;
 			}
 
-
-			//console.log('--->>>>>>',this.props);
-
-
 			return <Field  {...this.props} />;
 
 		}
