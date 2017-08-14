5db030d10dea84dec5996a0769c6bafddfb38882
马超越 <machaoyue@36kr.com>
2017-08-14 16:09:08 +0800

    merge 新版

--- src/Containers/Operation/Home/LeftTable/NullTable.js
+++ src/Containers/Operation/Home/LeftTable/NullTable.js
@@ -39,13 +39,22 @@ class NullTable  extends React.Component{
 	}
 
 	render(){
+		let imgStyle= {
+			display:'block',
+			margin:'0 auto'
+		};
+		let fontStyle= {
+			display:'inline-block',
+			marginLeft:20,
+			marginTop:10
+		}
 		
 		
 		return(
 			<div style={{height:570}}>
 				<div style={{textAlign:'center',marginTop:120}}>
-					<img src={nothings} />
-					<div>暂时还没有数据呦~</div>
+					<img src={nothings} style={imgStyle}/>
+					<div style={fontStyle}>暂时还没有数据呦~</div>
 				</div>
 				
 
