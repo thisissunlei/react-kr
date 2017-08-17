e72f5313f1aa25a39635e2a6930fd32839e2b80a
张弛 <zhangchi@krspace.cn>
2017-08-17 18:02:26 +0800

    ddd周四下午

--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -34,6 +34,32 @@ export default class Home extends React.Component {
   componentDidMount() {
     const { NavModel } = this.props;
 	NavModel.setSidebar(false);
+	var width = (document.getElementsByClassName('g-home-bottom')[0].clientWidth+180)*0.84*0.056;
+	console.log(document.getElementsByClassName('g-home-bottom')[0].clientWidth);
+	window.setTimeout(function() {
+		var swiper = new Swiper('.swiper-container', {
+			slidesPerView: 6,
+			paginationClickable: true,
+			loop:false,
+			prevButton:'.oa-swiper-prev',
+			nextButton:'.oa-swiper-next',
+			spaceBetween: width,
+		});
+		var swiperone = new Swiper('.swiper-container-one', {
+			loop:true,
+			// prevButton:'.oa-one-swiper-prev',
+			// nextButton:'.oa-one-swiper-next',
+			// scrollbar: '.swiper-scrollbar',
+			slidesPerView: 1,
+			scrollbarHide: false,
+			slidesPerView: 'auto',
+			centeredSlides: true,
+			autoplay : 560,
+		});
+	}, 100);
+	
+		
+	
   }
   
   render() {
@@ -59,7 +85,7 @@ export default class Home extends React.Component {
 
 								</span>
 								<span className="bottom">
-									已办事宜
+									待办事宜
 								</span>
 							</div>
 						</div>
@@ -105,7 +131,19 @@ export default class Home extends React.Component {
         </div>
 				<div className="g-home-middle">
 						<div className="g-home-middle-item">
-							
+							<div className="swiper-container-one">
+								<div className="swiper-wrapper">
+									<div className="swiper-slide swiper-slide1">Slide 1
+										
+									</div>
+									<div className="swiper-slide swiper-slide2">Slide 2</div>
+									<div className="swiper-slide swiper-slide3">Slide 3</div>
+									<div className="swiper-slide swiper-slide4">Slide 4</div>
+								</div>
+								
+							</div>
+
+
 						</div>
 						<div className="g-home-middle-item">
 							<div className="title">
@@ -117,6 +155,16 @@ export default class Home extends React.Component {
 								</div>
 								<div className="right">
 									更多
+								</div>
+							</div>
+							<div className="main">
+								<div className="item">
+									<span className="circle">
+
+									</span>
+									<span className="item-text">
+										ddd
+									</span>
 								</div>
 							</div>
 						</div>
@@ -131,6 +179,37 @@ export default class Home extends React.Component {
 								
 							</div>
 						</div>
+				</div>
+				<div className="g-home-bottom">
+					<div className="title">
+						<div className="circle">
+							
+						</div>
+						<div className="left">
+							最近动态
+						</div>
+					</div>
+					<div className="home-swiper">
+						<div className="swiper-container">
+							<div className="swiper-wrapper">
+								<div className="swiper-slide1 swiper-slide">Slide 1</div>
+								<div className="swiper-slide2 swiper-slide">Slide 2</div>
+								<div className="swiper-slide3 swiper-slide">Slide 3</div>
+								<div className="swiper-slide1 swiper-slide">Slide 1</div>
+								<div className="swiper-slide2 swiper-slide">Slide 2</div>
+								<div className="swiper-slide3 swiper-slide">Slide 3</div>
+								<div className="swiper-slide1 swiper-slide">Slide 1</div>
+								<div className="swiper-slide2 swiper-slide">Slide 2</div>
+								<div className="swiper-slide3 swiper-slide">Slide 3</div>
+								<div className="swiper-slide1 swiper-slide">Slide 1</div>
+							</div>
+							
+						</div>
+						<div className="oa-swiper-prev"></div>
+    					<div className="oa-swiper-next"></div>
+					</div>
+					
+					
 				</div>
       </div>
     );
