f8a1f22ba156612e7657119c757c43c1d83bfd37
1234 <a1234@1234deMacBook-Air.local>
2017-08-21 20:12:28 -0700

    xiangqing

--- src/Containers/PublicPage/DynamicsDetail/index.js
+++ src/Containers/PublicPage/DynamicsDetail/index.js
@@ -26,7 +26,7 @@ export default class DynamicsDetail extends React.Component {
 
 		this.state = {
 			infoList:{},
-		
+			searchParams:{},
 		}
 
 	}
@@ -44,7 +44,7 @@ export default class DynamicsDetail extends React.Component {
 	}
 	
 	render() {
-		let {infoList} = this.state;
+		let {infoList,searchParams} = this.state;
 		
 		if(infoList.photoUrl){
 			var styles={};
@@ -60,12 +60,12 @@ export default class DynamicsDetail extends React.Component {
                     <div className='g-dynamics-detail-left'></div>
                     <div className='g-dynamics-detail-right'></div>
                     <div className='g-dynamics-detail-container'>
-                        <li className='g-dynamics-detail-title'>浅谈用户体验数据化</li>
-                        <li className='g-dynamics-detail-time'>2017/08/03</li>
+                        <li className='g-dynamics-detail-title'>{infoList.title}</li>
+                        <li className='g-dynamics-detail-time'>{infoList.time}</li>
                         {infoList.photoUrl && <li style={styles} className='g-dynamics-detail-img'></li>}
                     </div>
                     <div className='g-dynamics-detail-content'>
-						{this.state.infoList.photoUrl}
+						{this.state.infoList.content || '无'}
 					</div>
                 </div>
 			</div>
