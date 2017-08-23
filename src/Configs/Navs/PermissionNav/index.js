cc0623ddf5766ed52d0d8be916b81fed2c0fdc69
刘毅豪 <liuyihao@krspace.cn>
2017-08-23 11:13:39 +0800

    位置

--- src/Configs/Navs/PermissionNav/index.js
+++ src/Configs/Navs/PermissionNav/index.js
@@ -24,25 +24,7 @@ module.exports = [
 				},
 				]
 			},
-			{
-				primaryText: '首页配置',
-				iconName: 'icon-basis',
-				menuCode: 'hrmbasedataadmin',
-				iconColor: '#79859a',
-				menuItems: [
-					{
-						primaryText: '轮播图列表',
-						menuCode: 'hrm_dim_list',
-						router: '/permission/homePageSetting/swperList',
-					},
-					{
-						primaryText: '最新动态列表',
-						menuCode: 'hrm_dim_list',
-						router: '/permission/homePageSetting/dynamicsList',
-					},
-					
-				]
-			},
+			
 			{
 				primaryText: "权限管理",
 				iconName: 'icon-wendang',
@@ -159,7 +141,25 @@ module.exports = [
 
 				]
 			},
-
+			{
+				primaryText: '首页配置',
+				iconName: 'icon-basis',
+				menuCode: 'hrmbasedataadmin',
+				iconColor: '#79859a',
+				menuItems: [
+					{
+						primaryText: '轮播图列表',
+						menuCode: 'hrm_dim_list',
+						router: '/permission/homePageSetting/swperList',
+					},
+					{
+						primaryText: '最新动态列表',
+						menuCode: 'hrm_dim_list',
+						router: '/permission/homePageSetting/dynamicsList',
+					},
+					
+				]
+			},
 
 		]
 	}
