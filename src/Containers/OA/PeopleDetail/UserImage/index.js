f329e1ce1687009057190bc347e68550dca1f7e6
wusuling <wusuling@krspace.cn>
2017-08-28 13:21:31 +0800

    feat:上传图片

--- src/Containers/OA/PeopleDetail/UserImage/index.js
+++ src/Containers/OA/PeopleDetail/UserImage/index.js
@@ -37,7 +37,7 @@ class UserImageChange  extends React.Component{
                             requireLabel={false}
 														personId={personId}
 														url={url}
-														requestUrl='http://optest02.krspace.cn/api/krspace-erp-web/hrm/resource/upload-photo/type/avatar'
+														requestUrl='http://op.krspace.cn/api/krspace-erp-web/hrm/resource/upload-photo/type/avatar'
 						/>
          </form>
 			</div>
