e69582e62ec35883d74f40af8d329facfd4a3471
wusuling <wusuling@krspace.cn>
2017-08-29 14:52:27 +0800

    feat:树单列的处理

--- src/Containers/Permission/ProcessManage/Form/index.js
+++ src/Containers/Permission/ProcessManage/Form/index.js
@@ -21,16 +21,14 @@ export default class Form  extends React.Component{
 				page: 1,
 				pageSize: 15,
 				orgId: '1',
-				orgType: "ROOT",
 			},
       //tab
-      data:{
+      data:[{
         children:[],
-        orgId:1,
+        orgId:0,
         orgName:"全部类型",
-        pId:0,
-        treeType:"ROOT"
-      },
+        treeType:"top"
+      }],
       dimData: [],
       treeData: [],
       searchKey: '',
@@ -70,11 +68,14 @@ export default class Form  extends React.Component{
 
   //获取树数据
   getTreeData = () => {
-      let {tree}=this.state;
+      let {tree,data}=this.state;
+      data[0].children=tree;
 			this.setState({
-				treeData: this.fnTree(tree),
-        data:this.fnTree(tree)[0]
-			});
+				treeData: this.fnTree(data),
+        data:this.fnTree(data)[0]
+			},function(){
+        console.log('treeData',this.state.treeData);
+      });
 	}
 
   //递归
@@ -82,7 +83,11 @@ export default class Form  extends React.Component{
 		let key = 0;
 		var arr = data.map((item,index)=>{
 			var obj = Object.assign({},item);
-			if(obj.children.length!=0){
+      if(!obj.children){
+        obj.children=[];
+        obj.treeType='single';
+      }
+      if(obj.children.length!=0){
 				obj.children = this.fnTree(obj.children);
 			}
 			obj.isClick = true;
@@ -109,31 +114,10 @@ export default class Form  extends React.Component{
 				page: 1,
 				pageSize: 15,
 				orgId: data[0].orgId,
-				orgType: data[0].treeType,
 			},
       data:data[0]
-		},function(){
-			_this.getOrganizationDetail();
 		});
 
-	}
-
-  getOrganizationDetail = ()=>{
-
-		/*var searchParams = Object.assign({},this.state.searchParams);
-		const that = this;
-		Http.request('org-detail', searchParams).then(function (response) {
-			const dataName = {};
-			dataName.orgName = response.orgName;
-			dataName.status = response.status || '0';
-			dataName.orgId = response.orgId || '1';
-			dataName.treeType = response.orgType || 'ROOT';
-			that.setState({
-				dataName,
-			});
-		}).catch(function (err) {
-
-		});*/
 	}
 
 
@@ -161,7 +145,7 @@ export default class Form  extends React.Component{
 
 		return(
 
-      <div className="g-oa-labour">
+      <div className="g-oa-labour pessi-form-type">
 				<div className="left">
 					<div className="search">
 						<input type="text" onChange={this.change} placeholder="输入表单名称" ref="searchKey"/>
