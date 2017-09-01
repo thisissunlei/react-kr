a917f4b33c22073a50ca4da5d7e047273d6edaf8
wusuling <wusuling@krspace.cn>
2017-09-01 09:06:20 +0800

    feat:字段

--- src/Containers/Permission/ProcessManage/Form/FormList/WatchForm/TextInfo/index.js
+++ src/Containers/Permission/ProcessManage/Form/FormList/WatchForm/TextInfo/index.js
@@ -14,6 +14,7 @@ import {
 	reduxForm,
 	change
 } from 'redux-form';
+import './index.less';
 var tableData = [
 	{name:'1liu',age:12,other:'1什么鬼',date:1504108800,select:'true'},
 	{name:'2liu',age:13,other:'2什么鬼',date:1504108800,select:'true'},
@@ -41,11 +42,11 @@ class TextInfo  extends React.Component{
 
 		return(
 
-			<div>
+			<div className='watch-text'>
           <Row style={{marginBottom:11,position:'relative',zIndex:5,marginTop:20}}>
 
             <Col
-              style={{float:'left'}}
+              style={{float:'left',marginBottom:20}}
             >
               <Button
                 label="新建明细表"
@@ -54,25 +55,37 @@ class TextInfo  extends React.Component{
               />
             </Col>
 
-            <form onSubmit={handleSubmit(this.onSubmit)} >
+            <form onSubmit={handleSubmit(this.onSubmit)} style={{marginTop:50}}>
 
-    					<FdTabel
-    						name = "tableData"
-    						isFold = {false}
-                toolbar={true}
-                batchDel={true}
-                checkbox={true}
-    					>
-    						<FRow name = "age" label = "字段名称"/>
-    						<FRow name = "name" label = "字段显示名"/>
-    						<FRow name = "other" label = "表现形式"/>
-    						<FRow name = "select" label = "字段类型"/>
-    						<FRow label = "操作" type='operation' component={(item)=>{
-    							 return <div style={{color:'#499df1',cursor:'pointer'}}>编辑</div>
-    						}}/>
-    					</FdTabel>
+              <div className='main-form'>
 
+                  <div className='main-name'>
+                   <span>主表-</span>
+                   <span>123</span>
+                   <span>(456)</span>
+                  </div>
 
+                  <div className='add-wrap'>
+                    <span className='add-form'></span>
+                    <span style={{marginTop:-12,display:'inline-block',verticalAlign:'middle'}}>新增字段</span>
+                  </div>
+
+        					<FdTabel
+        						name = "tableData"
+        						isFold = {false}
+                    toolbar={true}
+                    batchDel={true}
+                    checkbox={true}
+        					>
+        						<FRow name = "age" label = "字段名称"/>
+        						<FRow name = "name" label = "字段显示名"/>
+        						<FRow name = "other" label = "表现形式"/>
+        						<FRow name = "select" label = "字段类型"/>
+        						<FRow label = "操作" type='operation' component={(item)=>{
+        							 return <div style={{color:'#499df1',cursor:'pointer'}}>编辑</div>
+        						}}/>
+        					</FdTabel>
+              </div>
 
     				</form>
 
