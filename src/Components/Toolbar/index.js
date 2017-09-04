9e5ca8a55dfabe62dfc71f9708583ffa4bb87af7
刘毅豪 <liuyihao@krspace.cn>
2017-09-04 11:57:22 +0800

    feature

--- src/Components/Toolbar/index.js
+++ src/Components/Toolbar/index.js
@@ -20,11 +20,14 @@ export default class Toolbar extends React.Component{
 
 	render(){
 
-        let {label,iconClass,style} = this.props;
+        let {label,iconClass,style,leftSpac,rightSpac} = this.props;
+		let left = {marginLeft:leftSpac||0},right = {marginRight:rightSpac||0};
+		let toolbarStyle = Object.assign({},style||{},left,right)
+		
         
 		return (
 			
-                <div className='ui-toolbar' onClick = {this.iconClick} style = {style||{}}>
+                <div className='ui-toolbar' onClick = {this.iconClick} style = {toolbarStyle||{}}>
                     <span className={iconClass}></span>
                     <span style={{marginTop:-12,display:'inline-block',verticalAlign:'middle'}}>{label}</span>
                 </div>
