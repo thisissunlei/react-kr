2888dd0579a247208620a94bf66630522f0cb249
刘毅豪 <liuyihao@krspace.cn>
2017-10-18 15:29:58 +0800

    ok

--- src/PureComponents/LocationChoice/LocationChoiceSearch.js
+++ src/PureComponents/LocationChoice/LocationChoiceSearch.js
@@ -17,7 +17,7 @@ var type = [
     {value:"SPACE",label:'独立空间'}
 ]
 
-class LocationChoiceSearch extends Component {
+export default class LocationChoiceSearch extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
@@ -58,7 +58,7 @@ class LocationChoiceSearch extends Component {
         return(
             
             <div className='m-type-post'>
-				 <form onSubmit={handleSubmit(this.onSubmit)}>
+				 
                         <KrField grid={1/2}
                             style={{width:262,marginLeft:34,marginBottom:5}}
                             name="floor"
@@ -86,9 +86,6 @@ class LocationChoiceSearch extends Component {
                         <div style = {{display:"inline-block",top:36,left:45,position:"relative"}}>
                             <Button  label="查询" type="submit"/>
                         </div>
-                        
-                       
-                 </form>
 			</div>
         )
         
@@ -98,16 +95,3 @@ class LocationChoiceSearch extends Component {
 
 
 }
-const validate = values =>{
-	const errors = {};
-    if(!values.floor && values.floor != 0){
-        errors.floor = "楼层为必选项";
-    }
-    if(!values.detailType){
-        errors.detailType = "类型为必选项";
-    }
-    
-	return errors
-}
-
-export default reduxForm({ form: 'LocationChoiceSearch',validate})(LocationChoiceSearch);
