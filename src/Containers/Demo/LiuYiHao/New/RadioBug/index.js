0280271398034b194b713d7d47e613b00489aab7
刘毅豪 <liuyihao@krspace.cn>
2017-10-24 15:22:55 +0800

    feature:

--- src/Containers/Demo/LiuYiHao/New/RadioBug/index.js
+++ src/Containers/Demo/LiuYiHao/New/RadioBug/index.js
@@ -21,7 +21,18 @@ import {
     ButtonGroup,
     Button
 } from 'kr-ui';
-
+var data = {
+	name:"yihao",
+	age:18,
+	sex:"男",
+	motto:"宠辱不惊，看庭前花开花落；去留无意，望天空云卷云舒。"
+}
+/*
+姓名：{{name}}
+年龄：{{age}}
+性别：{{sex}}
+座右铭：{{motto}}
+*/
 class RadioBug extends React.Component {
 
 
@@ -46,7 +57,30 @@ class RadioBug extends React.Component {
 	componentDidMount() {
 		Store.dispatch(change('RadioBug','wsenabled',false));
 	}
-
+	attachTemplateToData = (template, data) => {
+        var i = 0,
+            len = data.length,
+            fragment = '';
+ 
+        // 遍历数据集合里的每一个项，做相应的替换
+        function replace(obj) {
+            var t, key, reg;
+ 　　　　　　
+　　　　　　　//遍历该数据项下所有的属性，将该属性作为key值来查找标签，然后替换
+            for (key in obj) {
+                reg = new RegExp('{{' + key + '}}', 'ig');
+                t = (t || template).replace(reg, obj[key]);
+            }
+ 
+            return t;
+        }
+ 
+        for (; i < len; i++) {
+            fragment += replace(data[i]);
+        }
+ 
+        return fragment;
+    };
 
 
 	render() {
@@ -56,10 +90,8 @@ class RadioBug extends React.Component {
 
 		return (
 			<form onSubmit={handleSubmit(this.onSubmit)} >
-				<KrField grid={1/2} style={{width:262,marginLeft:'30px'}} name="wsenabled" component="group" label="多文件上传 " requireLabel={true}>
-                    <KrField name="wsenabled" label="允许" type="radio" value={true} />
-                    <KrField name="wsenabled" label="禁止" type="radio" value={false} />
-                </KrField>
+				
+				<KrField component="editor" name="summary" label="活动介绍" defaultValue=''/>
                  <ButtonGroup>
                     <Button  label="确定" type="submit"/>
                     <Button  label="取消" type="button" cancle={true} onTouchTap={this.onCancel} />
