dfabcf6188ef16f0a8ed89d5a65df2030c2223bf
刘毅豪 <liuyihao@krspace.cn>
2017-10-30 19:19:01 +0800

    pull

--- src/Containers/PublicPage/PrintOther/publicFun.js
+++ src/Containers/PublicPage/PrintOther/publicFun.js
@@ -1,3 +1,6 @@
+var paperHeight = 1120,//整张纸的高
+    markHeight = 160,//章的高
+    markWidth = 160;//章的宽
 
 //字段替换
 function codeParse(template, data){
@@ -13,30 +16,102 @@ function codeParse(template, data){
 
 //标记替换
 function templateParse(template){
-    var t;
     var imgReg = new RegExp('`img`', 'ig');
     //分页标签
     var pageReg = new RegExp('`pagination`','ig');
+    //二维码
     var qrImgReg = new RegExp('`qrCode`','ig'); 
+    //区域划分组件开始
     var includeStart = new RegExp('`includeStart`','ig')
+    //区域划分组件结束
     var includeEnd = new RegExp('`includeEnd`','ig')
-    t = template.replace(imgReg, '<span class="print-other-chapter"><img src = "http://krspace-upload.oss-cn-qingdao.aliyuncs.com/activity_unzip/201707/Y/131233886_443.png"></span>');
-    t = template.replace(pageReg,'<div class = "print-pagination"><div>');
-    t = template.replace(qrImgReg,'<span class="print-qr-code"><img src = "http://krspace-upload.oss-cn-qingdao.aliyuncs.com/activity_unzip/201707/Y/131233886_443.png"></span>');
-    t = template.replace(includeStart,'<div class="print-include-start"></div>');
-    t = template.replace(includeEnd,'<div class="print-include-end"></div>');
-    return t;
+    template= template.replace(imgReg, '<span class="print-other-chapter" style="position: relative;"><img style="position:absolute;display:inline-block;width:160px;height:160px;left:-80px;top:-80px" src = "http://krspace-upload.oss-cn-qingdao.aliyuncs.com/activity_unzip/201707/Y/131233886_443.png"></span>');
+    template= template.replace(pageReg,'<div class = "print-pagination"></div>');
+    template= template.replace(qrImgReg,'<span class="print-qr-code"><img src = "http://krspace-upload.oss-cn-qingdao.aliyuncs.com/activity_unzip/201707/Y/131233886_443.png"></span>');
+    template = template.replace(includeStart,'<div class="print-include-start"></div>');
+    template = template.replace(includeEnd,'<div class="print-include-end"></div>');
+    return template;
 }
 
-function getClassName(data){
-    for(let key in data){
-        let allList = key.split("_");
-        
+//所有分页的标记渲染
+function paginations(){
+    let elems = getNode(".print-pagination");
+    for(let i=0;i<elems.length;i++){
+        everyPagination(elems[i]);
+    }
+}
+//每一个分页标记的渲染
+function everyPagination(elem){
+    var detail = elem.getBoundingClientRect(),
+        top = detail.top-60,
+        pageNum = Math.ceil(top/paperHeight),
+        diffValue = pageNum * paperHeight - top,
+        height = diffValue > 0 ? diffValue : 0;
+    
+        elem.style.height = height + "px";
+}
+
+// 所有包围标记的渲染
+function includes(){
+    var elemsStart = getNode('.print-include-start');
+    var elemsEnd = getNode('.print-include-end');
+    for(let i=0;i<elemsStart.length;i++){
+        everyInclude(elemsStart[i],elemsEnd[i]);
     }
 
 }
+//每一个包围标记的渲染
+function everyInclude(startElem,endElem){
+    var startDetail = startElem.getBoundingClientRect(),
+        endDetail = endElem.getBoundingClientRect(),
+        startTop = endDetail.top - 60,
+        endTop = endDetail.top - 60,
+        startPageNum =  Math.ceil(startTop/paperHeight),
+        endPageNum = Math.ceil(endTop/paperHeight);
+        if(endPageNum>startPageNum){
+            var diffValue = startPageNum * paperHeight - startTop,
+                height = diffValue > 0 ? diffValue : 0;
+                startElem.style.height = height + "px";
+        }      
+}
+
+//获取节点
+function getNode(elem){
+    return document.querySelectorAll(elem);
+}
+
+//齐缝章
+function checkMark(mainElem){
+    var mainDetil = mainElem.getBoundingClientRect(),
+        mainHeight = mainDetil.height,
+        pageNum = Math.ceil(mainHeight/paperHeight),
+        markElem = '';
+    if(pageNum>1){
+        for(let i = 0; i<pageNum;i++){
+            markElem += everyCheckMark(i,pageNum);
+        }
+        
+    }
+    mainElem.innerHTML = mainElem.innerHTML + markElem;
+    mainElem.style.height = pageNum * paperHeight + 'px';
+}
+
+function everyCheckMark(num,pageNum){
+   var boxWidth = Math.ceil(markWidth/pageNum*1000)/1000;
+   var top = num*paperHeight+paperHeight/2-markHeight/2;
+   var elemImg = '<div style="height:'+markHeight+'px;width:'+boxWidth+'px;overflow:hidden;position:absolute;right:0px;top:'+top+'px;">'+
+                    '<img style="width:'+markWidth+'px;height:'+markHeight+'px;display:inline-block;left:'+(-num*boxWidth)+'px;position:absolute;" src = "http://krspace-upload.oss-cn-qingdao.aliyuncs.com/activity_unzip/201707/Y/131233886_443.png"/>'+
+                 '</div>';
+   
+   return elemImg;
+}
+
+
+
 module.exports = {
 	templateParse,
     codeParse,
-    getClassName,
+    paginations,
+    includes,
+    checkMark
 }   
