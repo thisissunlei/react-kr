dd307110a6ad88200a8afc27514f29775f2498ae
张屈 <joneyphair@qq.com>
2017-11-10 16:30:58 +0800

    feat:cdn

--- webpack/webpack-production.config.js
+++ webpack/webpack-production.config.js
@@ -47,7 +47,7 @@ const config = {
 	},
 	// 出口文件配置
 	output: {
-		publicPath: "/new",
+		publicPath: "//sta.krpsace.cn/op/new",
 		path: buildPath,
 		filename: 'scripts/[name].[hash].js',
 		chunkFilename: '/scripts/[name].[chunkhash:5].js',
