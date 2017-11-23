1678dd0ebe0d5d104f4a8e5ba3df4f2930b69a09
马超越 <machaoyue@36kr.com>
2017-11-23 14:13:22 +0800

    fix

--- src/Containers/Operation/CommunityAllocation/Activitys/EditActivity.js
+++ src/Containers/Operation/CommunityAllocation/Activitys/EditActivity.js
@@ -171,7 +171,7 @@ class EditActivity extends React.Component {
 		
 		return (
 			<div className="g-create-activity">
-				<div className="u-create-title">
+				<div className="u-create-title"  style={{padding:'0 83px'}}>
 					<DrawerTitle title ='编辑活动' onCancel = {this.onCancel}/>
 				</div>
 				<form ref="form" onSubmit={handleSubmit(this.onSubmit)} >
