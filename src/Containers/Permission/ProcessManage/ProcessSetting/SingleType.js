ffa48a00aa4153366940eced46e900a20a0c1ce6
wusuling <wusuling@krspace.cn>
2017-11-30 11:31:37 +0800

    feat:合同类型名

--- src/Containers/Permission/ProcessManage/ProcessSetting/SingleType.js
+++ src/Containers/Permission/ProcessManage/ProcessSetting/SingleType.js
@@ -314,7 +314,7 @@ export default class SingleType extends React.Component {
                                                 }
 		 										return (<div  className='tooltipParent'><span className='tableOver'>{value}</span><Tooltip offsetTop={8} place='top'>{oldValue}</Tooltip></div>)
 		 								 }} ></TableRowColumn>
-                                    <TableRowColumn name="wfTypeName" 
+                                    <TableRowColumn name="typeName" 
 										component={(value,oldValue)=>{
                                                         var maxWidth=10;
                                                         if(value){
