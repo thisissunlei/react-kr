682453ae2d96d9d79b66f8f02d7de57f236c7a10
dongfanai <dongfanai@krspace.cn>
2017-12-13 15:38:21 +0800

    fix

--- src/Configs/Navs/BillNav/index.js
+++ src/Configs/Navs/BillNav/index.js
@@ -13,22 +13,22 @@ module.exports = [
 					{
 						primaryText: '已出账单管理',
 						menuCode: 'pay_created_bill_page',
-						router: 'http://optest01.krspace.cn/bill/list',
+						originUrl: 'http://optest01.krspace.cn/bill/list',
                     },
                     {
 						primaryText: '回款管理',
 						menuCode: 'pay_payment_page',
-						router: 'http://optest01.krspace.cn/bill/payment',
+						originUrl: 'http://optest01.krspace.cn/bill/payment',
                     },
                     {
 						primaryText: '收入管理',
 						menuCode: 'pay_income_page',
-						router: 'http://optest01.krspace.cn/bill/income',
+						originUrl: 'http://optest01.krspace.cn/bill/income',
                     },
                     {
 						primaryText: '交易流水',
 						menuCode: 'pay_deal_flow_page',
-						router: 'http://optest01.krspace.cn/bill/payrecord',
+						originUrl: 'http://optest01.krspace.cn/bill/payrecord',
 					},
 				]	
 			}, 
--- src/Configs/Navs/OrderNav/index.js
+++ src/Configs/Navs/OrderNav/index.js
@@ -13,7 +13,7 @@ module.exports = [
 					{
 						primaryText: '会议室订单管理',
 						menuCode: 'fina_meeting_order_page',
-						router: 'http://optest01.krspace.cn/order/list',
+						originUrl: 'http://optest01.krspace.cn/order/list',
                     },
                     
 				]	
