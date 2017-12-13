0821bf88d837cfa20e0fb997e33393d987719c63
dongfanai <dongfanai@krspace.cn>
2017-12-13 15:28:29 +0800

    feat:nav

---
+++ src/Configs/Navs/BillNav/index.js
@@ -0,0 +1,38 @@
+
+module.exports = [
+	{
+		primaryText: "账单中心",
+		router: 'office',
+		menuItems: [
+			{
+				primaryText: '账单中心',
+				iconName: 'icon-newthing',
+				router: 'office',
+				iconColor: '#79859a',
+				menuItems: [
+					{
+						primaryText: '已出账单管理',
+						menuCode: 'pay_created_bill_page',
+						originUrl: 'http://optest01.krspace.cn/bill/list',
+                    },
+                    {
+						primaryText: '回款管理',
+						menuCode: 'pay_payment_page',
+						originUrl: 'http://optest01.krspace.cn/bill/payment',
+                    },
+                    {
+						primaryText: '收入管理',
+						menuCode: 'pay_income_page',
+						originUrl: 'http://optest01.krspace.cn/bill/income',
+                    },
+                    {
+						primaryText: '交易流水',
+						menuCode: 'pay_deal_flow_page',
+						originUrl: 'http://optest01.krspace.cn/bill/payrecord',
+					},
+				]	
+			}, 
+			 
+		]
+	}
+]
---
+++ src/Configs/Navs/OrderNav/index.js
@@ -0,0 +1,24 @@
+
+module.exports = [
+	{
+		primaryText: "订单中心",
+		router: 'office',
+		menuItems: [
+			{
+				primaryText: '订单合同',
+				iconName: 'icon-newthing',
+				router: 'office',
+				iconColor: '#79859a',
+				menuItems: [
+					{
+						primaryText: '会议室订单管理',
+						menuCode: 'fina_meeting_order_page',
+						originUrl: 'http://optest01.krspace.cn/order/list',
+                    },
+                    
+				]	
+			}, 
+			 
+		]
+	}
+]
