b461b0e3f179caaa2a5fbc276e223a0aa712d23d
谭琳琳 <805148199@qq.com>
2018-01-07 18:10:35 +0800

    子设备列表搜索

--- src/Containers/SmartHardware/CenterControlManage/SonEquipmentManage/EquipmentManageBox/EquipmentSearchForm.js
+++ src/Containers/SmartHardware/CenterControlManage/SonEquipmentManage/EquipmentManageBox/EquipmentSearchForm.js
@@ -32,7 +32,6 @@ class EquipmentAdvancedQueryForm extends React.Component{
 			searchForm: false,
 			floorsOptions:[{label:"",value:""}],
 			propertyOption:[],
-			makerOptions : []
 		}
 	}
 	componentWillReceiveProps(){
@@ -40,43 +39,8 @@ class EquipmentAdvancedQueryForm extends React.Component{
 	componentDidMount(){
 
 		let _this = this;
-		_this.getDicList();
 		Store.dispatch(change('EquipmentAdvancedQueryForm','type',this.props.filter));
 		Store.dispatch(change('EquipmentAdvancedQueryForm','value',this.props.content));
-	}
-
-	// 获取通用字典
-	getDicList =()=>{
-		let _this =this;
-		Http.request('getWarningType', {}).then(function(response) {
-				
-			var arrNewDoorType = [],arrMakerOptions=[]
-			for(var i=0;i<response.DoorType.length;i++){
-				arrNewDoorType[i] = {
-					label:response.DoorType[i].desc,
-					value:response.DoorType[i].value,
-					code : response.DoorType[i].code
-				}
-				
-			}
-			for(var i=0;i<response.Maker.length;i++){
-				
-				arrMakerOptions[i]={
-					label:response.Maker[i].desc,
-					value:response.Maker[i].value,
-					code : response.Maker[i].code
-				}
-			}
-
-			_this.setState({
-				propertyOption :arrNewDoorType,
-				makerOptions : arrMakerOptions
-			})
-			
-
-		}).catch(function(err) {
-			Message.error(err.message);
-		});
 	}
 	
 
@@ -84,12 +48,8 @@ class EquipmentAdvancedQueryForm extends React.Component{
 	onSubmit=(values)=>{
 		//console.log("values",values);
 	}
-	// 关闭窗口
-	onCancle=()=>{
-		State.openSearchDialog = false;
-	}
-	
-	// 查询楼层
+
+
 	onChangeCommunity=(community)=>{
 
 		let _this = this;
@@ -105,7 +65,8 @@ class EquipmentAdvancedQueryForm extends React.Component{
 
 		}else{
 			communityIdReal = community.id;
-			floorReal = State.equipmentSecondParams.floor
+
+			floorReal = State.equipmentSearchParams.floor
 
 			let CommunityId = {
 				communityId : community.id
@@ -115,6 +76,7 @@ class EquipmentAdvancedQueryForm extends React.Component{
 	    		for (var i=0;i<response.floors.length;i++){
 	    			arrNew[i] = {label:response.floors[i],value:response.floors[i]}
 	    		}
+	    		console.log("arrNew",arrNew);
 	    		_this.setState({
 	    			floorsOptions : arrNew
 	    		})
@@ -130,7 +92,7 @@ class EquipmentAdvancedQueryForm extends React.Component{
 			page:1
 		}
 		State.realPage =1;
-		State.equipmentSecondParams = Object.assign({},State.equipmentSecondParams,newObj);
+		State.equipmentSearchParams = Object.assign({},State.equipmentSearchParams,newObj);
 
 	}
 
@@ -147,109 +109,78 @@ class EquipmentAdvancedQueryForm extends React.Component{
 			page :1
 		}
 		State.realPage = 1;
-		State.equipmentSecondParams = Object.assign({},State.equipmentSecondParams,newObj);
+		State.equipmentSearchParams = Object.assign({},State.equipmentSearchParams,newObj);
 	}
 
-	onchangeDoorType=(doorType)=>{
-		var doorTypeReal
-		if(doorType){
-			doorTypeReal = doorType.value;
+	onchangeControlEquipmentType=(controlEquipmentType)=>{
+		var controlEquipmentTypeReal
+		if(controlEquipmentType){
+			controlEquipmentTypeReal = controlEquipmentType.value;
 		}else{
-			doorTypeReal = '';
+			controlEquipmentTypeReal = '';
 		}
 		State.realPage =1;
 		var newData = {
-			doorType : doorTypeReal,
+			deviceType : controlEquipmentTypeReal,
 			date: new Date(),
 			page : 1
 		}
-		State.equipmentSecondParams = Object.assign({},State.equipmentSecondParams,newData);
+		State.equipmentSearchParams = Object.assign({},State.equipmentSearchParams,newData);
 		
 	}
 
-	onchangeMaker=(makerItem)=>{
-		
-		var makerItemReal
-		if(makerItem){
-			makerItemReal = makerItem.value;
-		}else{
-			makerItemReal = '';
-		}
-		
-		State.realPage =1;
-		var newData={
-			maker : makerItemReal,
-			page : 1,
-			date: new Date()
-		}
-		State.equipmentSecondParams = Object.assign({},State.equipmentSecondParams,newData);
-
-	}
 
 
 	onSearchSubmit=(value)=>{
-		
+		console.log("value",value);
 		var newObj;
-		if(value.filter == "doorCode"){
+		if(value.filter == "deviceId"){
 
 			
 		    newObj ={
-				    	doorCode: value.content,
-				    	deviceId: '',
-				    	title : '',
+				    	localNo: value.content,
+				    	name: '',
 				    	page:1
 				    }
 
-		}else if(value.filter == "title"){
+		}else if(value.filter == "name"){
 
 			
-		    newObj ={
-				    	title: value.content,
-				    	deviceId: '',
-				    	doorCode: '',
+		     newObj ={
+				    	name: value.content,
+				    	localNo: '',
 				    	page:1
 				    }
 
-		}else{
-		    
-		    
-		    newObj ={
-				    	deviceId: value.content,
-				    	doorCode: '',
-				    	title :'',
-				    	page:1
-				    }
-	    }
+		}
 
-	    var objNewT = Object.assign({},State.equipmentSecondParams);
-	    State.realPage =1;
-	    State.equipmentSecondParams = Object.assign({},objNewT,newObj);
+	    var objNewT = Object.assign({},State.equipmentSearchParams);
+	    State.realPage =1; 
+	    State.equipmentSearchParams = Object.assign({},objNewT,newObj);
 
 	}
 
 	onchangeConnect=(connectStatus)=>{
 		
 		var TimeObj={logined:connectStatus.value||''};
-		var objNewT = Object.assign({},State.equipmentSecondParams);
-	    State.equipmentSecondParams = Object.assign({},objNewT,TimeObj);
+		var objNewT = Object.assign({},State.equipmentSearchParams);
+	    State.equipmentSearchParams = Object.assign({},objNewT,TimeObj);
 
 	}
 
 	render(){
-		let {floorsOptions,propertyOption,makerOptions}=this.state;
+		let {floorsOptions,propertyOption}=this.state;
 		const { error, handleSubmit,content,filter} = this.props;
-		let options=[{
-		      label:"智能硬件ID",
-		      value:"deviceId"
-		    }]
-		var itemsDrop=["重启APP","重启系统","断开重连","刷新屏幕"]
-		let connectOptions=[{
-			label:"已连接",
-			value:"true"
-		  },{
-			label:"未连接",
-			value:"false"
-		  }]
+		let options=[{label:"设备ID",value:"deviceId"},
+						{label:"名称",value:"name"}]
+		let connectOptions=[{label:"已连接",value:"true"},
+							{label:"未连接",value:"false"}]
+	let controlEquipmentOption=[{label:"灯",value:"LAMP"},
+						  		{label:"雾化膜",value:"ATOMIZATION_MEMBRANE"},
+						  		{label:"空调",value:"AIR_CONDITION"},
+						  		{label:"空气质量仪",value:"AIR_SENSOR"},
+						  		{label:"温湿度计",value:"HUMITURE_SENSOR"},
+						  		{label:"人体感应器",value:"BODY_SENSOR"}]
 		return (
 			<form onSubmit={handleSubmit(this.onSubmit)} style={{width:"100%",marginTop:20,position:"relative"}} className="second-equipment-search">
 				<span className="fir-span">
@@ -274,23 +205,14 @@ class EquipmentAdvancedQueryForm extends React.Component{
 				<span className="thir-span">
 					<KrField name="propertyId"
 						component="select"
-						label="类型: "
-						onChange = {this.onchangeDoorType}
-						options={propertyOption}
-						style={{width:'150px'}}
+						label="控制器类型: "
+						onChange = {this.onchangeControlEquipmentType}
+						options={controlEquipmentOption}
+						style={{width:'200px'}}
 						inline={true}
 					/>
 				</span>
-				<span className="fouth-span">
-					<KrField name="maker"
-						component="select"
-						label="厂商: "
-						onChange = {this.onchangeMaker}
-						options={makerOptions}
-						style={{width:'150px'}}
-						inline={true}
-					/>
-				</span>
+				
 				<KrField name="connectStatus"
 						component="select"
 						label="连接状态："
