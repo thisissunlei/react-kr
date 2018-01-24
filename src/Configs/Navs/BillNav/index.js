1606e7fe40583389ff7bcd278b29011f0de3d7e9
wusuling <wusuling@krspace.cn>
2018-01-24 17:03:59 +0800

    feat: 拉取

--- src/Configs/Navs/BillNav/index.js
+++ src/Configs/Navs/BillNav/index.js
@@ -11,26 +11,22 @@ module.exports = [
 				menuItems: [
 					{
 						primaryText: '已出账单管理',
-						menuCode: 'order_seat_list',
-						//menuCode: 'pay_created_bill_page',
+						menuCode: 'pay_created_bill_page',
 						originUrl: '/bill/list',
                     },
                     {
 						primaryText: '回款管理',
-						menuCode: 'order_seat_list',
-						//menuCode: 'pay_payment_page',
+						menuCode: 'pay_payment_page',
 						originUrl: '/bill/payment',
                     },
                     {
 						primaryText: '应收管理',
-						menuCode: 'order_seat_list',
-						//menuCode: 'pay_income_page',
+						menuCode: 'pay_income_page',
 						originUrl: '/bill/income',
                     },
                     {
 						primaryText: '交易流水',
-						menuCode: 'order_seat_list',
-						//menuCode: 'pay_deal_flow_page',
+						menuCode: 'pay_deal_flow_page',
 						originUrl: '/bill/payrecord',
 					},
 				]	
