54287c4cdda61e933af3f8c903810eb6ded0d3bc
谭琳琳 <805148199@qq.com>
2018-03-26 11:42:45 +0800

    Save

--- src/Configs/Apis/DoorPermissionApi/index.js
+++ src/Configs/Apis/DoorPermissionApi/index.js
@@ -5,52 +5,52 @@ module.exports = {
 
 	//门禁权限列表
    'getDoorPermissionList':{
-      url: '/mockjsdata/53/iot-platform/auth/user-group/list?pageSize={pageSize}&name={name}&page={page}&communityId={communityId}&customerId={customerId}&groupLevel={groupLevel}',
+      url: '/api/iot-platform/auth/user-group/list?pageSize={pageSize}&name={name}&page={page}&communityId={communityId}&customerId={customerId}&groupLevel={groupLevel}',
       method: 'get'
    },
    //新增门禁组
    'newCreateDoorGroup':{
-        url: '/mockjsdata/53/iot-platform/auth/user-group/add',
+        url: '/api/iot-platform/auth/user-group/add',
         method: 'post'
     },
     //删除门禁组
    'deleteDoorGroup':{
-        url: '/mockjsdata/53/iot-platform/auth/user-group/delete?id={id}',
+        url: '/api/iot-platform/auth/user-group/delete?id={id}',
         method: 'delete'
     },
     //获取组成员列表
     'getDoorGroupMemberList':{
-        url: '/mockjsdata/53/iot-platform/auth/user-group/user/list?name={name}&communityId={communityId}&groupId={groupId}&customerId={customerId}&phone={phone}&date={date}',
+        url: '/pai/iot-platform/auth/user-group/user/list?name={name}&communityId={communityId}&groupId={groupId}&customerId={customerId}&phone={phone}&date={date}',
         method: 'get'
     },
     //删除组成员
     'deleteGroupMemberApi':{
-        url: '/mockjsdata/53/iot-platform/auth/user-group-mapping/delete?ids={ids}',
+        url: '/api/iot-platform/auth/user-group-mapping/delete?ids={ids}',
         method: 'delete'
     },
     //添加组成员
     'addGroupMemberApi':{
-        url: '/mockjsdata/53/iot-platform/auth/user-group-mapping/add',
+        url: '/api/iot-platform/auth/user-group-mapping/add',
         method: 'post'
     },
     //个人/门禁权限组已授权设备
     'getGroupAuthorizeEquipmentApi':{
-        url: '/mockjsdata/53/iot-platform/auth/device-grant/list?communityId={communityId}&deviceId={deviceId}&doorCode={doorCode}&doorType={doorType}&floor={floor}&title={title}&granteeId={granteeId}&granteeType={granteeType}',
+        url: '/api/iot-platform/auth/device-grant/list?communityId={communityId}&deviceId={deviceId}&doorCode={doorCode}&doorType={doorType}&floor={floor}&title={title}&granteeId={granteeId}&granteeType={granteeType}',
         method: 'get'
     },
     //从门禁组/个人已授权设备中删除设备
     'deleteEquipmentFromGroupApi':{
-        url: '/mockjsdata/53/iot-platform/auth/device-grant/delete?ids={ids}',
+        url: '/api/iot-platform/auth/device-grant/delete?ids={ids}',
         method: 'delete'
     },
     //门禁组所有可选择设备
     'doorGroupAllEquipmentApi':{
-        url: '/mockjsdata/53/iot-platform/door-device/select-items?communityId={communityId}&deviceId={deviceId}&doorType={doorType}&floor={floor}&title={title}&doorCode={doorCode}',
+        url: '/api/iot-platform/door-device/select-items?communityId={communityId}&deviceId={deviceId}&doorType={doorType}&floor={floor}&title={title}&doorCode={doorCode}',
         method: 'get'
     },
     //增加设备到门禁授权组
     'addEquipmentToGroupApi':{
-        url: '/mockjsdata/53/iot-platform/auth/device-grant/add',
+        url: '/api/iot-platform/auth/device-grant/add',
         method: 'post'
     },
     //获取所有成员列表数据以供添加成员到组
@@ -60,24 +60,24 @@ module.exports = {
     },
     //获取所有成员列表数据以供添加成员到组
     'getDoorGroupDetailApi':{
-        url: '/mockjsdata/53/iot-platform/auth/user-group/detail?id={id}',
+        url: '/api/iot-platform/auth/user-group/detail?id={id}',
         method: 'get'
     },
 
     // 编辑门禁组
     'editDoorGroupApi':{
-        url: '/mockjsdata/53/iot-platform/auth/user-group/edit',
+        url: '/api/iot-platform/auth/user-group/edit',
         method: 'put'
     },
 
     // 用户所属门禁组列表
     'getGroupContainMember':{
-        url: '/mockjsdata/53/iot-platform/auth/user/belong-to-group/list?uid={uid}',
+        url: '/api/iot-platform/auth/user/belong-to-group/list?uid={uid}',
         method: 'get'
     },
     //个人权限页将个人移出权限组
     'personPageDropOutGroup':{
-        url: '/mockjsdata/53/iot-platform/auth/user-group/user/delete?uid={uid}&groupId={groupId}',
+        url: '/api/iot-platform/auth/user-group/user/delete?uid={uid}&groupId={groupId}',
         method: 'delete'
     },
 
