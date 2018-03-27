25b23bcfadb1c6d0ce9fd3abc83b1122dedde3d2
谭琳琳 <805148199@qq.com>
2018-03-27 19:33:34 +0800

    save

--- src/Containers/DoorPermission/DoorGroupManage/ChangeEquipment/index.js
+++ src/Containers/DoorPermission/DoorGroupManage/ChangeEquipment/index.js
@@ -64,19 +64,24 @@ export default class ChangeMember extends React.Component{
 		let {itemDetail}  = this.props;
 		let {freshGroupEquipment} = this.state;
 		let doorTypeOptions=State.doorTypeOptions;
+		console.log("itemDetail",itemDetail);
+		let titleText =  "组授权设备 | 组名称："+itemDetail.name 
 		return (
 			<div className="change-member">
 				<div style={{width:"100%",height:30}}>
 					<img src={closeImg} style={{float:"right",width:30,cursor:"pointer"}} onClick={this.closeChangeMember}/>
 				</div>
 				<div style={{width:"100%"}}>
-
-					<div className="change-member-item">
-						<AllEquipmentListSearch memberDetailInfo={itemDetail} granteeType="USER_GROUP" refreshPage={this.freshGroupEquipment} doorTypeOptions={doorTypeOptions} refreshAuthoriazationEquipmentList={this.refreshAuthoriazationEquipmentList}/>
-					</div>
+					{
+						itemDetail.groupLevel &&  itemDetail.groupLevel=="NORMAL" &&
+						<div className="change-member-item">
+							<AllEquipmentListSearch memberDetailInfo={itemDetail} granteeType="USER_GROUP" refreshPage={this.freshGroupEquipment} doorTypeOptions={doorTypeOptions} refreshAuthoriazationEquipmentList={this.refreshAuthoriazationEquipmentList}/>
+						</div>
+					}
+					
 					
 					<div className="change-member-item">
-						<Section title={`组授权设备`} description="" >
+						<Section title={titleText} description="" >
 							<AuthorizationEquipment memberDetailInfo={itemDetail} doorTypeOptions={doorTypeOptions} granteeId={itemDetail.id} granteeType="USER_GROUP" noShowAddNew={true} freshGroupEquipment={freshGroupEquipment}/> 
 						</Section>
 					</div>
