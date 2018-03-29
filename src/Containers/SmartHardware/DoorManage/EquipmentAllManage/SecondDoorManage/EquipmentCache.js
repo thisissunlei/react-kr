a357b2ee655c5a7d89c97d3991f85c06baf70268
谭琳琳 <805148199@qq.com>
2018-03-29 22:37:24 +0800

    save

--- src/Containers/SmartHardware/DoorManage/EquipmentAllManage/SecondDoorManage/EquipmentCache.js
+++ src/Containers/SmartHardware/DoorManage/EquipmentAllManage/SecondDoorManage/EquipmentCache.js
@@ -40,10 +40,7 @@ export default class EquipmentSearch extends React.Component{
 		let _this =this;
 		var DomOuter = document.getElementsByClassName("table-body")[0];
 		var DomInner = document.getElementsByClassName("table-body-box")[0];
-		console.log("DomOuter",DomOuter);
 		DomOuter.onscroll = function(){
-			console.log("DomInner.offsetHeight",DomInner.offsetHeight);
-			console.log("this.scrollTop",this.scrollTop,"this.offsetHeight",this.offsetHeight)
 			if(this.scrollTop+this.offsetHeight+20>=DomInner.offsetHeight){
 				
 				_this.getEquipmentCache();
