1e96591e4fa1a5c2f34c127c0ea7f2e67319807a
yihaoliu <liuyihao@krspace.cn>
2018-06-26 20:41:44 +0800

    feat: 菜单修改

--- src/Configs/Navs/DevTool/index.js
+++ src/Configs/Navs/DevTool/index.js
@@ -1,25 +1,25 @@
-// return  {
-//     primaryText: "开发工具",
-//     router: 'dev',
-//     type: 'vue',
-//     menuItems: [
-//         {
-//             primaryText: '后端工具',
-//             iconName: 'icon-money',
-//             iconColor: '#79859a',
-//             type: 'vue',
-//             menuItems: [
-//                 {
-                    primaryText: "数据监控",
-                    router: 'management-tool/data-monitoring',
-                    type: 'vue',
-                    menuCode: 'pm_manage_list'
-//                 }
-//             ]
-//         }
+// // return  {
+// //     primaryText: "开发工具",
+// //     router: 'dev',
+// //     type: 'vue',
+// //     menuItems: [
+// //         {
+// //             primaryText: '后端工具',
+// //             iconName: 'icon-money',
+// //             iconColor: '#79859a',
+// //             type: 'vue',
+// //             menuItems: [
+// //                 {
+//                     primaryText: "数据监控",
+//                     router: 'management-tool/data-monitoring',
+//                     type: 'vue',
+//                     menuCode: 'pm_manage_list'
+// //                 }
+// //             ]
+// //         }
 
-//     ]
-// }
+// //     ]
+// // }
 module.exports = [
 
 	{
