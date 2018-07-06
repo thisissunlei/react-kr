a7365d46302cd1c9b7f5aeac876a5e092b9bfeba
yihaoliu <liuyihao@krspace.cn>
2018-07-06 12:26:44 +0800

    feat: 配置

--- deploy_test.sh
+++ deploy_test.sh
@@ -12,6 +12,7 @@ test01_ip=ali-docker-test01
 test02_ip=ali-docker-test02
 test03_ip=ali-docker-test03
 test04_ip=ali-docker-test04
+dev01_ip=10.4.15.241
 
 case $1 in
   
@@ -40,6 +41,10 @@ case $1 in
     target_site_port=22
 
     npm run build:test04
+  ;;
+  dev01)
+    target_site="www@${dev01_ip}"
+    target_site_port=22
   ;;
   *)
   target_site=www@10.1.60.201
