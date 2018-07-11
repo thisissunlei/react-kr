c79873be3e6af111b413872c5a1e7d2fdf7bb1cc
yihaoliu <liuyihao@krspace.cn>
2018-07-11 18:24:00 +0800

    feat: 资金转移菜单吟唱

--- src/Configs/Navs/FinanceNav/index.js
+++ src/Configs/Navs/FinanceNav/index.js
@@ -74,12 +74,12 @@ module.exports = [
                         type: 'vue',
                         menuCode: 'qualification'
                     },
-                    {
-                        primaryText: "客户资金转移",
-                        type: 'vue',
-                        originUrl: '/order-center/apply-manage/_transferOperate',
-                        menuCode: 'wallet_transfer',
-                    }
+                    // {
+                    //     primaryText: "客户资金转移",
+                    //     type: 'vue',
+                    //     originUrl: '/order-center/apply-manage/_transferOperate',
+                    //     menuCode: 'wallet_transfer',
+                    // }
                     ]
                 },
 		]
