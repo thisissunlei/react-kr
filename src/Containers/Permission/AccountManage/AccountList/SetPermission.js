48494e44b70b3aaa1f2fb769c1841cfaceadd9af
sunlei <sunlei@MacBook-Pro-10.local>
2018-09-29 14:21:23 +0800

    feat: 授予弹框

--- src/Containers/Permission/AccountManage/AccountList/SetPermission.js
+++ src/Containers/Permission/AccountManage/AccountList/SetPermission.js
@@ -146,8 +146,8 @@ export default class SetPermission extends React.Component {
     render() {
       let {roleList}=this.state;
       return(
-        <div className="g-SetPermission" style={{maxHeight:'300px',overflow:'scroll'}}>
-            <div style={{textAlign:'left',marginTop:20}}>
+        <div className="g-SetPermission">
+            <div style={{textAlign:'left',marginTop:20,maxHeight:'300px',overflow:'auto'}}>
               <Checkbox label="全选" style={{display:'block',color:'#333'}} onCheck={this.allSelect} checked={this.state.allCheck}/>
               {roleList.map((item,index)=>{return this.renderData(item,index)})}
             </div>
