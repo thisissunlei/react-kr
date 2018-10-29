f663c9b3e4fb4c8e4502cf67f166d9e05152ec98
sunlei <sunlei@MacBook-Pro-10.local>
2018-10-29 15:32:23 +0800

    feat: 角色反查与业务代码合并

--- src/Containers/Permission/User/Createdialog/index.js
+++ src/Containers/Permission/User/Createdialog/index.js
@@ -22,10 +22,11 @@ class Createdialog extends React.Component {
 		this.state = {
 			moduleDetail: [],
 			resourceIds: [],
+			businessCodeIds: [],
 			errorTip: false,
-			roleList:[]
+			roleList: []
 		}
-		
+
 	}
 	componentDidMount() {
 		this.getInfo();
@@ -37,35 +38,47 @@ class Createdialog extends React.Component {
 		} = this.props;
 		onCancel && onCancel();
 	}
-	onSelect = (item)=>{
-		let idList=[]; 
+	onSelect = (item) => {
+		//	let idList=[]; 
+		let resourceIds = [];
+		let businessCodeIds = [];
 		Http.request('getRoleData', {
 			id: item.id
-		}).then( (response) =>{
-			response.moduleAndResources.map((item)=>{
-				item.check=false;
-					item.resources.length && item.resources.map((items)=>{
-						if(items.ownFlag==1 && idList.indexOf(items.id)==-1 ){
-								idList.push(items.id)					
+		}).then((response) => {
+			response.moduleAndResources.map((item) => {
+				item.check = false;
+				item.resources.length && item.resources.map((items) => {
+					// 业务代码
+					if (items.ownFlag == 1 && items.operationType === 'BUSINESS_CODE') {
+						if (businessCodeIds.indexOf(items.id) == -1) {
+							businessCodeIds.push(items.id)
 						}
-					})
+					} else if (items.ownFlag == 1 && (items.operationType === 'MENU' || items.operationType === 'OPERATION')) {
+						// 操作项和业务代码
+						if (resourceIds.indexOf(items.id) == -1) {
+							resourceIds.push(items.id)
+						}
+					}
+				})
 			})
 			this.setState({
 				moduleDetail: response.moduleAndResources,
-				resourceIds:idList
+				resourceIds,
+				businessCodeIds
 			})
-		}).catch(function(err) {
+		}).catch(function (err) {
 
 		});
 	}
 	onSubmit = (form) => {
 		let {
-			resourceIds
+			resourceIds,
+			businessCodeIds
 		} = this.state;
 
-		if (resourceIds.length > 0) {
+		if (resourceIds.length > 0 || businessCodeIds.length > 0) {
 			form.resourceIds = resourceIds;
-
+			form.businessCodeIds = businessCodeIds;
 			let {
 				onSubmit
 			} = this.props;
@@ -75,139 +88,154 @@ class Createdialog extends React.Component {
 			this.setState({
 				errorTip: true
 			})
+
 		}
 
 	}
-	getRoleList = () =>{
-		Http.request('getRoleListData').then( (response) =>{
-			let roleList =[];
-			response.reduce((pre,v,i,self)=>{
-				pre.push({id:v.id,label:v.name,value:v.id,name:v.name})
+	getRoleList = () => {
+		Http.request('getRoleListData').then((response) => {
+			let roleList = [];
+			response.reduce((pre, v, i, self) => {
+				pre.push({ id: v.id, label: v.name, value: v.id, name: v.name })
 				return pre;
-			},roleList)
+			}, roleList)
 			this.setState({
 				roleList
 			})
-		}).catch(function(err) {
+		}).catch(function (err) {
 
 		});
 	}
 	getInfo = () => {
-		Http.request('getModuleData').then( (response)=> {
+		Http.request('getModuleData').then((response) => {
 			this.setState({
 				moduleDetail: response.moduleAndResources
 			})
-		}).catch(function(err) {
+		}).catch(function (err) {
 
 		});
 	}
+	// 第二级选择切换 
 	getValue = (item) => {
 
 		var check = item.ownFlag
 		var id = item.id;
-		var idList = this.state.resourceIds;
+		// var idList = this.state.resourceIds;
 		let {
-			moduleDetail
+			moduleDetail,
+			resourceIds,
+			businessCodeIds
 		} = this.state;
 		if (check == 1) {
 			item.ownFlag = 0;
-			var index = idList.indexOf(id);
-			if (index > -1) {
-				idList.splice(index, 1);
+			// 业务代码 
+			if (item.operationType === 'BUSINESS_CODE') {
+				let Bindex = businessCodeIds.indexOf(id);
+				if (Bindex > -1) {
+					businessCodeIds.splice(Bindex, 1);
+				} // 操作项
+			} else if (item.operationType === 'MENU' || item.operationType === 'OPERATION') {
+				let Rindex = resourceIds.indexOf(id);
+				if (Rindex > -1) {
+					resourceIds.splice(Rindex, 1);
+				}
 			}
-			this.setState({
-				resourceIds: idList
-			})
-
 		} else {
 			item.ownFlag = 1;
-			var index = idList.indexOf(id);
-			if (index == -1) {
-				idList.push(id);
+			if (item.operationType === 'BUSINESS_CODE') {
+				let Bindex = businessCodeIds.indexOf(id);
+				if (Bindex == -1) {
+					businessCodeIds.push(id);
+				} // 操作项
+			} else if (item.operationType === 'MENU' || item.operationType === 'OPERATION') {
+				let Rindex = resourceIds.indexOf(id);
+				if (Rindex == -1) {
+					resourceIds.push(id);
+				}
 			}
-			this.setState({
-				resourceIds: idList
-			})
-
-
 		}
-		
-		if (this.state.resourceIds.length > 0) {
-			this.setState({
-				errorTip: false
-			})
-		}
+		this.setState({ resourceIds, businessCodeIds })
 	}
-	getAllValue=(value)=>{
-		var idList = this.state.resourceIds;
-		if(!value.check){
-			value.check=true;
-			value.resources.map((item)=>{
-				item.ownFlag=1;
-				if(idList.indexOf(item.id)==-1){
-					idList.push(item.id)
+	// 第一级全选 
+	getAllValue = (value) => {
+		let { resourceIds, businessCodeIds } = this.state;
+
+		if (!value.check) {
+			value.check = true;
+			value.resources.map((item) => {
+				item.ownFlag = 1;
+				if (resourceIds.indexOf(item.id) == -1 && (item.operationType === 'MENU' || item.operationType === 'OPERATION')) {
+					resourceIds.push(item.id)
+				} else if (businessCodeIds.indexOf(item.id) == -1 && item.operationType === 'BUSINESS_CODE') {
+					businessCodeIds.push(item.id)
 				}
 			})
-		}else{
-			value.check=false;
-			value.resources.map((item)=>{
-				item.ownFlag=0;
-				var index = idList.indexOf(item.id);
-				if(index>-1){
-					idList.splice(index, 1);
+		} else {
+			value.check = false;
+			value.resources.map((item) => {
+				item.ownFlag = 0;
+				let Rindex = resourceIds.indexOf(item.id);
+				let Bindex = businessCodeIds.indexOf(item.id);
+				if (Rindex > -1) {
+					resourceIds.splice(Rindex, 1);
+				} else if (Bindex > -1) {
+					businessCodeIds.splice(Bindex, 1)
 				}
 			})
 		}
 		this.setState({
-				resourceIds: idList
-			})
-		
+			resourceIds,
+			businessCodeIds
+		})
+
 	}
-	renderResources=(resources)=>{
-		return resources.map((item,index)=>{
-			return(
+	// 第二级渲染
+	renderResources = (resources) => {
+		return resources.map((item, index) => {
+			return (
 				<div className="u-resources-list" key={index}>
-					<input 
-						  type="checkbox"  
-						  checked={item.ownFlag==1?'checked':''} 
-						  value={item.id} 
-						  onChange={this.getValue.bind(this,item)}
+					<input
+						type="checkbox"
+						checked={item.ownFlag == 1 ? 'checked' : ''}
+						value={item.id}
+						onChange={this.getValue.bind(this, item)}
 					/>{item.name}
 				</div>
-				)
+			)
 		})
 	}
+	// 渲染所有的操作项 第一级 
 	renderOperation = (moduleDetail) => {
 		var _this = this;
-		
-		return	moduleDetail.map((item,index)=>{
-				if(item.resources.length>0){
-					return(
-						<div className="u-clearfix u-module" key={index} >
-							<div className="u-module-list">{item.name}</div>
-							<div className="u-resources-list">
-								<input 
-									  type="checkbox" 
-									  onChange={this.getAllValue.bind(this,item)}
-								/>全选
-							</div>	
-							{this.renderResources(item.resources)}
-						</div>
-					)
-				}else{
-					
-					return(
-						<div key={index}>
-							{item.name}
-						</div>
-					)	
-				}
-				
-				
-			})	
+
+		return moduleDetail.map((item, index) => {
+			if (item.resources.length > 0) {
+				return (
+					<div className="u-clearfix u-module" key={index} >
+						<div className="u-module-list">{item.name}</div>
+						<div className="u-resources-list">
+							<input
+								type="checkbox"
+								onChange={this.getAllValue.bind(this, item)}
+							/>全选
+							</div>
+						{this.renderResources(item.resources)}
+					</div>
+				)
+			} else {
+
+				return (
+					<div key={index}>
+						{item.name}
+					</div>
+				)
+			}
+
+
+		})
 	}
-	
-	
+
+
 	render() {
 		let {
 			handleSubmit
@@ -215,78 +243,73 @@ class Createdialog extends React.Component {
 		let {
 			moduleDetail,
 			resourceIds,
+			businessCodeIds,
 			errorTip,
 			roleList
 		} = this.state;
 		return (
 			<div className="g-create">
 				<div className="u-create-title">
-			     	<DrawerTitle title ="新建角色" onCancel = {this.onCancel}/>
-			     </div>
+					<DrawerTitle title="新建角色" onCancel={this.onCancel} />
+				</div>
 
 				<form onSubmit={handleSubmit(this.onSubmit)} >
 					<KrField
-							style={{width:252,marginLeft:96,marginBottom:16}}
-							name="name" type="text"
-							component="input" label="名称"
-							requireLabel={true}
-							requiredValue={true}
-							inline={false}
+						style={{ width: 252, marginLeft: 96, marginBottom: 16 }}
+						name="name" type="text"
+						component="input" label="名称"
+						requireLabel={true}
+						requiredValue={true}
+						inline={false}
 					/>
 					<KrField
-							style={{width:252,marginLeft:50,marginBottom:16}}
-							name="code" type="text"
-							component="input" label="编码"
-							requireLabel={true}
-							requiredValue={true}
-							inline={false}
+						style={{ width: 252, marginLeft: 50, marginBottom: 16 }}
+						name="code" type="text"
+						component="input" label="编码"
+						requireLabel={true}
+						requiredValue={true}
+						inline={false}
 					/>
-					<div style={{margin:'0 0 10px 95px',fontWeight:'500'}}>
-								<KrField
-								name="module"
-								component="select"
-								label="复制其他的操作项角色当前配置"
-								options={roleList}
-								inline={true}
-								onChange={this.onSelect}
+					<div style={{ margin: '0 0 10px 95px', fontWeight: '500' }}>
+						<KrField
+							name="module"
+							component="select"
+							label="复制其他的操作项角色当前配置"
+							options={roleList}
+							inline={true}
+							onChange={this.onSelect}
 						/>
-							</div>
+					</div>
 					<div className="u-operation">
 						<div className="u-operation-label">
 							<span className="u-require">*</span>操作项：
 						</div>
 						<div className="u-operation-content">
-							{moduleDetail.length>0?this.renderOperation(moduleDetail):''}
-							{errorTip?<div className="u-error-tip">请选择操作项</div>:''}
+							{moduleDetail.length > 0 ? this.renderOperation(moduleDetail) : ''}
+							{errorTip ? <div className="u-error-tip">请选择操作项</div> : ''}
 						</div>
 
 						<KrField
-								 type="hidden"
-								 name="resourceIds"
-								 values={resourceIds}
+							type="hidden"
+							name="resourceIds"
+							values={resourceIds}
 						/>
-
-							<div style={{margin:'0 0 20px 40px',fontWeight:'500'}}>
-								<KrField
-								name="module"
-								component="select"
-								label="复制其他的操作项角色当前配置"
-								options={roleList}
-								inline={true}
-								onChange={this.onSelect}
+						<KrField
+							type="hidden"
+							name="businessCodeIds"
+							values={businessCodeIds}
 						/>
-							</div>
 					</div>
-	
-					<Row style={{marginTop:10,marginBottom:15}}>
-					<Col md={12} align="center"> 
-						<ButtonGroup>
-							<div  className='ui-btn-center'><Button  label="确定" type="button"  type="submit"  height={34} width={90}/></div>
-							<Button  label="取消" type="button"  onTouchTap={this.onCancel} cancle={true} height={33} width={90}/>
-						</ButtonGroup>
-					 </Col>
-					 </Row>
-					
+
+					<Row style={{ marginTop: 10, marginBottom: 15 }}>
+						<Col md={12} align="center">
+							<ButtonGroup>
+								<div className='ui-btn-center'><Button label="确定" type="button" type="submit" height={34} width={90} /></div>
+								<Button label="取消" type="button" onTouchTap={this.onCancel} cancle={true} height={33} width={90} />
+							</ButtonGroup>
+						</Col>
+					</Row>
+
 				</form>
 
 			</div>
